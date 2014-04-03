1bfa9c06c9efa73bb5888b6cd3ad09b2f3bbae43
Jordan Hatch <jordan@jordanh.net>
2014-04-03 14:57:20 +0100

    Add jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -x
+export DISPLAY=:99
+export GOVUK_APP_DOMAIN=test.gov.uk
+export GOVUK_ASSET_ROOT=http://static.test.gov.uk
+export REPO_NAME="alphagov/collections"
+env
+
+function github_status {
+  REPO_NAME="$1"
+  STATUS="$2"
+  MESSAGE="$3"
+  gh-status "$REPO_NAME" "$GIT_COMMIT" "$STATUS" -d "Build #${BUILD_NUMBER} ${MESSAGE}" -u "$BUILD_URL" >/dev/null
+}
+
+function error_handler {
+  trap - ERR # disable error trap to avoid recursion
+  local parent_lineno="$1"
+  local message="$2"
+  local code="${3:-1}"
+  if [[ -n "$message" ]] ; then
+    echo "Error on or near line ${parent_lineno}: ${message}; exiting with status ${code}"
+  else
+    echo "Error on or near line ${parent_lineno}; exiting with status ${code}"
+  fi
+  github_status "$REPO_NAME" failure "failed on Jenkins"
+  exit "${code}"
+}
+
+trap "error_handler ${LINENO}" ERR
+github_status "$REPO_NAME" pending "is running on Jenkins"
+
+bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
+RAILS_ENV=test bundle exec rake
+bundle exec rake assets:precompile
+
+export EXIT_STATUS=$?
+
+if [ "$EXIT_STATUS" == "0" ]; then
+  github_status "$REPO_NAME" success "succeeded on Jenkins"
+else
+  github_status "$REPO_NAME" failure "failed on Jenkins"
+fi
+
+exit $EXIT_STATUS
