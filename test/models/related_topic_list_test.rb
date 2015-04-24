58f2ca971221c62ebc507c47338df0e46ecd0383
tijmen <tijmen@gmail.com>
2015-04-24 14:54:16 +0100

    Use content schema examples in unit tests
    We now use the content schema examples directly, so that when the
    schema/examples change, these tests will start failing as well.

--- test/models/related_topic_list_test.rb
+++ test/models/related_topic_list_test.rb
@@ -1,18 +1,21 @@
 require 'test_helper'
 
 describe RelatedTopicList do
+  include ContentSchemaHelpers
+
   describe "#related_topics_for" do
     it "returns related topics for a subsection" do
-      content_store = content_store_with_response(links: {related_topics: [{ title: 'From Content Store', web_url: 'http://example.org/whatever'}]})
+      content_store = content_store_with_response(content_schema_example(:mainstream_browse_page, :level_2_page_with_related_topics))
 
       topic_list = RelatedTopicList.new(content_store, nil)
+      found_topic = topic_list.related_topics_for('/browse/whatever').first
 
-      assert_equal topic_list.related_topics_for('/browse/whatever'),
-        [OpenStruct.new(title: 'From Content Store', web_url: 'http://example.org/whatever')]
+      assert_equal found_topic.title, 'Universal credit'
+      assert_equal found_topic.web_url, 'https://www.gov.uk/benefits/universal-credit'
     end
 
     it "returns guidance categories from whitehall when there are no related topics" do
-      content_store = content_store_with_response(links: {related_topics: []})
+      content_store = content_store_with_response(content_schema_example(:mainstream_browse_page, :level_2_page))
       whitehall = whitehall_with_response([{title: 'From Whitehall', content_with_tag: { web_url: 'http://example.org/whatever'}}])
 
       topic_list = RelatedTopicList.new(content_store, whitehall)
--- test/support/content_schema_helpers.rb
+++ test/support/content_schema_helpers.rb
@@ -3,4 +3,9 @@ module ContentSchemaHelpers
     examples = GovukContentSchemaTestHelpers::Examples.new.get_all_for_format(format)
     examples.map { |json_example| JSON.parse(json_example) }
   end
+
+  def content_schema_example(format, example_name)
+    json_example = GovukContentSchemaTestHelpers::Examples.new.get(format, example_name)
+    JSON.parse(json_example)
+  end
 end
