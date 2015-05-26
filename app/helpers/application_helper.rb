0b7bef03ad9c5332577a7ebef04153a780b7a52f
tijmen <tijmen@gmail.com>
2015-05-26 19:26:45 +0100

    Extract root section list to partial
    The views for the root sections and sections are almost identical
    across the pages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def browsing_in_root_section?(root_section)
+    params[:section].present? && params[:section] == root_section.slug
+  end
+
+  def browsing_in_section?(section)
+    params[:sub_section].present? && full_slug == section.slug
+  end
+
+private
+
+  def full_slug
+    [params[:section], params[:sub_section]].join('/')
+  end
 end
