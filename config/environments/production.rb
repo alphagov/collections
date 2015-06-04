e40e61c9d00996f59148b366af8efb479153e4f4
tijmen <tijmen@gmail.com>
2015-06-04 08:53:46 +0100

    Remove ActionMailer
    Collections doesn't send emails and probably never will. Removing
    ActionMailer will speed up boot time.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,10 +68,6 @@ Collections::Application.configure do
   config.assets.prefix = "/collections"
   config.action_controller.asset_host = ENV['GOVUK_ASSET_HOST']
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
