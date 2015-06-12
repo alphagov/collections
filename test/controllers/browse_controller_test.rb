187159b56a291f3ba422d94dc4c89c07a0f37ba1
tijmen <tijmen@gmail.com>
2015-06-12 08:57:14 +0100

    Remove view-testing controller tests
    Controller tests - if they should exist at all - should not test the
    views. That's more easily done in integration tests (which is already
    being done). This commit removes view-tests controller tests for
    browsecontroller.
    
    The tests now only verify HTTP headers and response codes.

--- test/controllers/browse_controller_test.rb
+++ test/controllers/browse_controller_test.rb
@@ -2,14 +2,9 @@ require "test_helper"
 
 describe BrowseController do
   describe "GET index" do
-    it "list all categories" do
-      content_api_has_root_sections(["crime-and-justice"])
-      get :index
-      assert_select "a[href='/browse/crime-and-justice']", "Crime and justice"
-    end
-
     it "set slimmer format of browse" do
       content_api_has_root_sections(["crime-and-justice"])
+
       get :index
 
       assert_equal "browse",  response.headers["X-Slimmer-Format"]
@@ -17,6 +12,7 @@ describe BrowseController do
 
     it "set correct expiry headers" do
       content_api_has_root_sections(["crime-and-justice"])
+
       get :index
 
       assert_equal "max-age=1800, public",  response.headers["Cache-Control"]
@@ -28,35 +24,27 @@ describe BrowseController do
       content_api_has_root_sections(["crime-and-justice"])
     end
 
-    it "list the sub sections" do
-      content_api_has_root_sections(["crime-and-justice"])
-      content_api_has_section("crime-and-justice")
-      content_api_has_subsections("crime-and-justice", ["alpha"])
-      get :section, section: "crime-and-justice"
-
-      assert_select "h1", "Crime and justice"
-      assert_select "a[href='/browse/alpha']"
-    end
-
     it "404 if the section does not exist" do
       api_returns_404_for("/tags/banana.json")
       api_returns_404_for("/tags.json?parent_id=banana&type=section")
 
       get :section, section: "banana"
+
       assert_response 404
     end
 
     it "return a cacheable 404 without calling content_api if the section slug is invalid" do
       get :section, section: "this & that"
+
       assert_equal "404", response.code
       assert_equal "max-age=600, public",  response.headers["Cache-Control"]
-
       assert_not_requested(:get, %r{\A#{GdsApi::TestHelpers::ContentApi::CONTENT_API_ENDPOINT}})
     end
 
     it "set slimmer format of browse" do
       content_api_has_section("crime-and-justice")
       content_api_has_subsections("crime-and-justice", ["alpha"])
+
       get :section, section: "crime-and-justice"
 
       assert_equal "browse",  response.headers["X-Slimmer-Format"]
@@ -65,6 +53,7 @@ describe BrowseController do
     it "set correct expiry headers" do
       content_api_has_section("crime-and-justice")
       content_api_has_subsections("crime-and-justice", ["alpha"])
+
       get :section, section: "crime-and-justice"
 
       assert_equal "max-age=1800, public",  response.headers["Cache-Control"]
@@ -83,36 +72,12 @@ describe BrowseController do
       content_api_has_artefacts_with_a_tag("section", "crime-and-justice/judges", ["judge-dredd"])
     end
 
-    it "list the content in the sub section" do
-      get :sub_section, section: "crime-and-justice", sub_section: "judges"
-
-      assert_select "h1", "Judges"
-      assert_select "a", "Judge dredd"
-    end
-
-    it "lists related topics in the subsection" do
-      RelatedTopicList.any_instance.expects(:related_topics_for).with("/browse/crime-and-justice/judges").returns(
-        [OpenStruct.new(title: 'A Related Topic', web_url: 'https://www.gov.uk/benefits/related')]
-      )
-
-      get :sub_section, section: "crime-and-justice", sub_section: "judges"
-
-      assert_select '.detailed-guidance' do
-        assert_select "li a[href='https://www.gov.uk/benefits/related']", text: 'A Related Topic'
-      end
-    end
-
     it "ignores query parameters when requesting related topics" do
       RelatedTopicList.any_instance.expects(:related_topics_for).with("/browse/crime-and-justice/judges").returns(
         [OpenStruct.new(title: 'A Related Topic', web_url: 'https://www.gov.uk/benefits/related')]
       )
 
       get :sub_section, section: "crime-and-justice", sub_section: "judges", :foo => "bar"
-
-      assert_select '.detailed-guidance' do
-        assert_select "li a[href='https://www.gov.uk/benefits/related']", text: 'A Related Topic'
-      end
-
     end
 
     it "404 if the section does not exist" do
@@ -120,14 +85,15 @@ describe BrowseController do
       api_returns_404_for("/tags/crime-and-justice.json")
 
       get :sub_section, section: "crime-and-justice", sub_section: "frume"
+
       assert_response 404
     end
 
     it "return a cacheable 404 without calling content_api if the section slug is invalid" do
       get :sub_section, section: "this & that", sub_section: "foo"
+
       assert_equal "404", response.code
       assert_equal "max-age=600, public",  response.headers["Cache-Control"]
-
       assert_not_requested(:get, %r{\A#{GdsApi::TestHelpers::ContentApi::CONTENT_API_ENDPOINT}})
     end
 
@@ -135,14 +101,15 @@ describe BrowseController do
       api_returns_404_for("/tags/crime-and-justice%2Ffrume.json")
 
       get :sub_section, section: "crime-and-justice", sub_section: "frume"
+
       assert_response 404
     end
 
     it "return a cacheable 404 without calling content_api if the sub section slug is invalid" do
       get :sub_section, section: "foo", sub_section: "this & that"
+
       assert_equal "404", response.code
       assert_equal "max-age=600, public",  response.headers["Cache-Control"]
-
       assert_not_requested(:get, %r{\A#{GdsApi::TestHelpers::ContentApi::CONTENT_API_ENDPOINT}})
     end
 
--- test/integration/example_test.rb
+++
@@ -1,9 +0,0 @@
-require 'integration_test_helper'
-
-class ExampleTest < ActionDispatch::IntegrationTest
-
-  it 'asserts the truth' do
-    assert true
-  end
-  
-end
