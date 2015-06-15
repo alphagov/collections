89385d14c8f721aba75797172fc333de3f3f74dc
tijmen <tijmen@gmail.com>
2015-06-15 10:56:18 +0100

    Use poltergeist for javascript integration tests
    Turns out webkit didn't actually test the javascript correctly when
    running the entire test suite. Using poltergeist like other projects
    also allows us to remove some code.

--- features/support/slimmer.rb
+++
@@ -1,7 +0,0 @@
-class ActionController::Base
-  before_filter proc {
-    unless ENV["USE_SLIMMER"] || Capybara.current_driver == Capybara.javascript_driver
-      response.headers[Slimmer::Headers::SKIP_HEADER] = "true"
-    end
-  }
-end
--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -x
-export DISPLAY=:99
 export REPO_NAME=${REPO_NAME:-"alphagov/collections"}
 export CONTEXT_MESSAGE=${CONTEXT_MESSAGE:-"default"}
 export GH_STATUS_GIT_COMMIT=${SCHEMA_GIT_COMMIT:-${GIT_COMMIT}}
