b887d921a0c939b7a110c9866de59987721341c9
tijmen <tijmen@gmail.com>
2015-06-18 14:09:57 +0100

    Add namespaced routes for topics
    Topics will be moving to the `/topic` namespace. To keep the content
    available when this app has been deployed, but collections-publisher
    has not, we'll serve topics from both the namespaced and non-namespaced
    URLs.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,15 @@ Collections::Application.routes.draw do
   get "/browse/:top_level_slug", to: "browse#top_level_browse_page"
   get "/browse/:top_level_slug/:second_level_slug", to: "browse#second_level_browse_page"
 
+  # Make the topics temporarily available under both /topic and their old route.
+  # When all Topics have been updated to use the namespace in the content-store,
+  # we can remove the duplication.
+  get "/topic/:topic_slug/:subtopic_slug/latest", to: "subtopics#latest_changes"
+  get "/topic/:topic_slug/:subtopic_slug", to: "subtopics#show"
+  get "/topic/:topic_slug", to: "topics#show"
+  get "/topic/:topic_slug/:subtopic_slug/email-signup", to: "email_signups#new"
+  post "/topic/:topic_slug/:subtopic_slug/email-signup", to: "email_signups#create"
+
   # Note that this app only receives requests for routes registered with the
   # content-store (by collections-publisher) - whenever the routes below
   # change, also change the routes claimed by collections-publisher.
