6d215d2d9b6e95fcc6d35710c1a7a3de0ae44f2d
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-06-23 13:31:25 +0000

    Disable sessions
    This app doesn't use sessions, and therefore has no need to set a
    session cookie.  Disabling the session_store prevents the cookie from
    being set.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Collections::Application.config.session_store :cookie_store, key: '_collections_session'
+Collections::Application.config.session_store :disabled
