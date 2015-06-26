12ae3a4e2b1beecf30c39669923bb7a69ef60d49
tijmen <tijmen@gmail.com>
2015-06-26 10:05:16 +0100

    Clean up /browse route
    ` => "browse#index"` is superflous because we're already specifying the
    controller/method in `to:`

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Collections::Application.routes.draw do
-
-  mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
-  get "/browse.json" => redirect("/api/content/browse")
-  get "/browse" => "browse#index", to: "browse#index"
-  get "/browse/:top_level_slug", to: "browse#top_level_browse_page"
-  get "/browse/:top_level_slug/:second_level_slug", to: "browse#second_level_browse_page"
-
   # Note that this app only receives requests for routes registered with the
   # content-store (by collections-publisher) - whenever the routes below
   # change, also change the routes claimed by collections-publisher.
+
+  mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
+
+  get "/browse.json" => redirect("/api/content/browse")
+  get "/browse", to: "browse#index"
+  get "/browse/:top_level_slug", to: "browse#top_level_browse_page"
+  get "/browse/:top_level_slug/:second_level_slug", to: "browse#second_level_browse_page"
 
   get "/topic/:topic_slug/:subtopic_slug/latest", to: "subtopics#latest_changes", as: :latest_changes
   get "/topic/:topic_slug/:subtopic_slug", to: "subtopics#show", as: :subtopic
