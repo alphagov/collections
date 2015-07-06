fb4a6cb6743840d410116c642270e216e95c5e1e
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-07-06 10:42:02 +0000

    Remove slug validation code.
    All routes to this application are now created as exact routes in the
    router. This means that requests for pages with invalid or malformed
    slugs will never make it to this application because the router will
    return a 404 for them.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -55,23 +55,9 @@ class ApplicationController < ActionController::Base
     render status: status_code, text: "#{status_code} error"
   end
 
-  def cacheable_404
-    set_expiry(10.minutes)
-    error 404
-  end
-
   def set_expiry(duration = 30.minutes)
     unless Rails.env.development?
       expires_in(duration, :public => true)
     end
-  end
-
-  def validate_slug_param(param_name = :slug)
-    param_to_use = params[param_name]
-    if param_to_use.parameterize != param_to_use
-      cacheable_404
-    end
-  rescue StandardError # Triggered by trying to parameterize malformed UTF-8
-    cacheable_404
   end
 end
