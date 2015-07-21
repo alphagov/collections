a51d7813d2b2adae91d8c87f449e7eba124e9ccb
Kushal Pisavadia <kushi.p@gmail.com>
2015-07-21 14:44:10 +0100

    Remove duplication by shareing test helpers
    These blocks of code are duplicated in the `RummagerHelpers`
    module. Include the corresponding module to reduce this duplication.

--- features/support/topic_helper.rb
+++ features/support/topic_helper.rb
@@ -1,9 +1,12 @@
 require 'gds_api/test_helpers/content_api'
 require 'gds_api/test_helpers/rummager'
 
+require_relative '../../test/support/rummager_helpers'
+
 module TopicHelper
   include GdsApi::TestHelpers::ContentApi
   include GdsApi::TestHelpers::Rummager
+  include RummagerHelpers
 
   def stub_topic_lookups
     @organisations = %w{
@@ -55,50 +58,6 @@ module TopicHelper
     })
 
     stub_topic_organisations("oil-and-gas/fields-and-wells")
-  end
-
-  def stub_topic_organisations(slug)
-    Collections::Application.config.search_client.stubs(:unified_search).with(
-      count: "0",
-      filter_specialist_sectors: [slug],
-      facet_organisations: "1000",
-    ).returns(
-      rummager_has_specialist_sector_organisations(slug)
-    )
-  end
-
-  def rummager_has_documents_for_subtopic(subtopic_slug, document_slugs, format = "guide", page_size: 50)
-    results = document_slugs.map.with_index do |slug, i|
-      rummager_document_for_slug(slug, (i + 1).hours.ago, format)
-    end
-
-    results.each_slice(page_size).with_index do |results_page, page|
-      start = page * page_size
-      Collections::Application.config.search_client.stubs(:unified_search).with(
-        has_entries(
-          start: start,
-          count: page_size,
-          filter_specialist_sectors: [subtopic_slug],
-        )
-      ).returns({
-        "results" => results_page,
-        "start" => start,
-        "total" => results.size,
-      })
-    end
-  end
-
-  def rummager_document_for_slug(slug, updated_at = 1.hour.ago, format = "guide")
-    {
-      "format" => "#{format}",
-      "latest_change_note" => "This has changed",
-      "public_timestamp" => updated_at.iso8601,
-      "title" => "#{slug.titleize.humanize}",
-      "link" => "/#{slug}",
-      "index" => "/",
-      "_id" => "/#{slug}",
-      "document_type" => "edition"
-    }
   end
 end
 
