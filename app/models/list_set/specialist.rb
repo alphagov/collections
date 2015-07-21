fcb2f2a65d4d7af400144ce2bb5a81ccbc654111
Kushal Pisavadia <kushi.p@gmail.com>
2015-07-21 11:53:13 +0100

    ListSet::Specialist creates lists for specialist content
    This new class is very similar to `ListSet`. Instead of calling the
    Content API, it calls Rummager for the tagged content.

---
+++ app/models/list_set/specialist.rb
@@ -0,0 +1,51 @@
+class ListSet::Specialist
+  include Enumerable
+
+  List = Struct.new(:title, :contents)
+
+  def initialize(slug, groups)
+    @slug = slug
+    @groups = groups || []
+  end
+
+  def each
+    lists.each do |l|
+      yield l
+    end
+  end
+
+  private
+
+  def lists
+    if @groups.any?
+      curated_list
+    else
+      a_to_z_list
+    end
+  end
+
+  def a_to_z_list
+    [List.new(
+      "A to Z",
+      content_tagged_to_topic.reject do |content|
+        ListSet::FORMATS_TO_EXCLUDE.include? content.format
+      end.sort_by(&:title)
+    )]
+  end
+
+  def curated_list
+    @groups.map do |group|
+      contents = group["contents"].map do |api_url|
+        content_tagged_to_topic.find do |content|
+          content.base_path + ".json" == URI.parse(api_url).path
+        end
+      end.compact
+
+      List.new(group["name"], contents) if contents.any?
+    end.compact
+  end
+
+  def content_tagged_to_topic
+    Topic::ContentTaggedToTopic.new(@slug)
+  end
+end
