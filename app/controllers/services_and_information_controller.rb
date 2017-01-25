eb177ac22b48bdbe9d3c0e0f0a561fb86f4d5abb
Carlos Vilhena <carlosvilhena@gmail.com>
2017-01-25 17:05:43 +0000

    Render services and information with locals

--- app/controllers/services_and_information_controller.rb
+++ app/controllers/services_and_information_controller.rb
@@ -1,12 +1,34 @@
 class ServicesAndInformationController < ApplicationController
   def index
-    base_path = "/government/organisations/#{params[:organisation_id]}/services-information"
-    @content_item = Services.content_store.content_item!(base_path).to_hash
-    links_grouper = ServicesAndInformationLinksGrouper.new(params[:organisation_id])
-    @navigation_helpers = GovukNavigationHelpers::NavigationHelper.new(@content_item)
-    @organisation = @content_item.dig("links", "parent", 0)
-    @grouped_links = links_grouper.parsed_grouped_links.reject do |group|
+    setup_navigation_helpers(content_item)
+
+    render :index, locals: {
+      content_item: content_item,
+      organisation: organisation,
+      grouped_links: grouped_links
+    }
+  end
+
+private
+
+  def base_path
+    "/government/organisations/#{params[:organisation_id]}/services-information"
+  end
+
+  def content_item
+    Services.content_store.content_item!(base_path)
+  end
+
+  def grouped_links
+    links_grouper =
+      ServicesAndInformationLinksGrouper.new(params[:organisation_id])
+
+    links_grouper.parsed_grouped_links.reject do |group|
       group["title"].nil?
     end
+  end
+
+  def organisation
+    content_item.to_hash.dig("links", "parent", 0)
   end
 end
