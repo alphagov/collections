7e88838a5fc3004a89ec5ec41db9f6e78d869535
Carlos Vilhena <carlosvilhena@gmail.com>
2017-01-25 16:46:16 +0000

    Render locals from topics controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,11 +1,15 @@
 class TopicsController < ApplicationController
   def index
-    @topic = Topic.find(request.path)
-    setup_content_item_and_navigation_helpers(@topic)
+    topic = Topic.find(request.path)
+    setup_navigation_helpers(topic)
+
+    render :index, locals: { topic: topic }
   end
 
   def show
-    @topic = Topic.find(request.path)
-    setup_content_item_and_navigation_helpers(@topic)
+    topic = Topic.find(request.path)
+    setup_navigation_helpers(topic)
+
+    render :index, locals: { topic: topic }
   end
 end
