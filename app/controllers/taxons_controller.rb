b2546b5eb60d9ac747f2a42dcb24835303c67de6
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2017-02-08 13:35:35 +0000

    Add A/B test `meta` tag to taxon navigation pages
    The `meta` tag powers Google Analytics (to record which variant a user
    has seen), and the Chrome extension.

--- app/controllers/taxons_controller.rb
+++ app/controllers/taxons_controller.rb
@@ -4,7 +4,14 @@ class TaxonsController < ApplicationController
   def show
     setup_content_item_and_navigation_helpers(taxon)
 
-    render :show, locals: { taxon: taxon }
+    ab_variant = GovukAbTesting::AbTest.new("EducationNavigation").requested_variant(request)
+
+    ab_variant.configure_response(response)
+
+    # Show the taxon page regardless of which variant is requested, because
+    # there is no straighforward mapping of taxons back to original navigation
+    # pages.
+    render :show, locals: { taxon: taxon, ab_variant: ab_variant }
   end
 
 private
