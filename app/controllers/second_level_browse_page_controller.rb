2aeb2a1958e08ce3d3c1f90089d49147da4461b0
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2017-02-13 15:53:21 +0000

    Redirect from second-level browse pages in the education nav A/B test
    Configure redirects from all education second-level browse pages, such
    as /browse/education/school-life, to the equivalent taxon page.

--- app/controllers/second_level_browse_page_controller.rb
+++ app/controllers/second_level_browse_page_controller.rb
@@ -6,15 +6,30 @@ class SecondLevelBrowsePageController < ApplicationController
 
     respond_to do |f|
       f.html do
-
         dimension = Rails.application.config.navigation_ab_test_dimension
         ab_test = GovukAbTesting::AbTest.new("EducationNavigation", dimension: dimension)
         ab_variant = ab_test.requested_variant(request)
-        
+
+        is_page_under_ab_test = false
+
+        if new_navigation_enabled? && params[:top_level_slug] == "education"
+          ab_variant.configure_response(response)
+          is_page_under_ab_test = true
+
+          if ab_variant.variant_b?
+            taxon = redirects["education"][params[:second_level_slug]]
+
+            return redirect_to controller: "taxons",
+              action: "show",
+              taxon_base_path: taxon
+          end
+        end
+
         render :show, locals: {
           page: page,
           meta_section: meta_section,
-          ab_variant: ab_variant
+          ab_variant: ab_variant,
+          is_page_under_ab_test: is_page_under_ab_test
         }
       end
       f.json do
@@ -36,5 +51,9 @@ private
     MainstreamBrowsePage.find(
       "/browse/#{params[:top_level_slug]}/#{params[:second_level_slug]}"
     )
+  end
+
+  def redirects
+    Rails.application.config_for(:navigation_redirects)["second_level_browse_pages"]
   end
 end
