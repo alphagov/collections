87e70239f58504b1017fa282bcd29d4924b1c167
Carlos Vilhena <carlosvilhena@gmail.com>
2017-04-19 16:01:25 +0100

    Enable Heroku review apps
    This commit allows us to deploy the application to Heroku and therefore
    enable review apps for each PR.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,10 @@ Collections::Application.configure do
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
 
+  if ENV['RAILS_SERVE_STATIC_ASSETS'] == 'yes'
+    config.serve_static_assets = true
+  end
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
