4612bab7513761f836e5a027e87b830cbffcc6b6
Carlos Vilhena <carlosvilhena@gmail.com>
2017-06-07 15:25:03 +0000

    Refactor how taxonomy pages are rendered
    - Move logic into the presenter and use it across the views;
    - One partial per page type: grid, accordion and leaf.

--- app/controllers/taxons_controller.rb
+++ app/controllers/taxons_controller.rb
@@ -1,14 +1,22 @@
 class TaxonsController < ApplicationController
-  helper_method :taxon_overview_and_child_taxons
-
   def show
     setup_content_item_and_navigation_helpers(taxon)
+
+    taxon_template =
+      case presented_taxon.rendering_type
+      when TaxonPresenter::GRID
+        :grid
+      when TaxonPresenter::ACCORDION
+        :accordion
+      else
+        :leaf
+      end
 
     # Show the taxon page regardless of which variant is requested, because
     # there is no straighforward mapping of taxons back to original navigation
     # pages.
-    render :show, locals: {
-      taxon: taxon,
+    render taxon_template, locals: {
+      presented_taxon: presented_taxon,
       blue_box_ab_test: blue_box_ab_test,
       education_ab_test: education_ab_test
     }
@@ -30,30 +38,5 @@ private
 
   def presented_taxon
     @presented_taxon ||= TaxonPresenter.new(taxon)
-  end
-
-  def taxon_overview_and_child_taxons(taxon)
-    accordion_items = taxon.child_taxons
-    return [] if taxon.child_taxons.empty?
-
-    current_taxon_title = 'General information and guidance'
-
-    if taxon.tagged_content.count > 0
-      general_information = Taxon.new(
-        ContentItem.new(
-          'content_id' => taxon.content_id,
-          'title' => current_taxon_title,
-          'base_path' => current_taxon_title.downcase.tr(' ', '-'),
-          'description' => ''
-        )
-      )
-      general_information.can_subscribe = false
-
-      accordion_items.unshift(
-        general_information
-      )
-    end
-
-    accordion_items
   end
 end
--- app/presenters/taxon_presenter.rb
+++ app/presenters/taxon_presenter.rb
@@ -1,10 +1,21 @@
 class TaxonPresenter
-  GRID = 1
-  ACCORDION = 2
-  LEAF = 3
+  GRID = :grid
+  ACCORDION = :accordion
+  LEAF = :leaf
 
   attr_reader :taxon
-  delegate :tagged_content, to: :taxon
+  delegate(
+    :content_id,
+    :title,
+    :description,
+    :base_path,
+    :tagged_content,
+    :grandchildren?,
+    :child_taxons,
+    :most_popular_content,
+    :can_subscribe?,
+    to: :taxon
+  )
 
   def initialize(taxon)
     @taxon = taxon
@@ -26,5 +37,30 @@ class TaxonPresenter
 
   def renders_as_grid?
     rendering_type == GRID
+  end
+
+  def accordion_content
+    return [] unless renders_as_accordion?
+    accordion_items = taxon.child_taxons.map { |taxon| TaxonPresenter.new(taxon) }
+
+    general_information_title = 'General information and guidance'
+
+    if tagged_content.count > 0
+      general_information = Taxon.new(
+        ContentItem.new(
+          'content_id' => taxon.content_id,
+          'title' => general_information_title,
+          'base_path' => general_information_title.downcase.tr(' ', '-'),
+          'description' => ''
+        )
+      )
+      general_information.can_subscribe = false
+
+      accordion_items.unshift(
+        TaxonPresenter.new(general_information)
+      )
+    end
+
+    accordion_items
   end
 end
