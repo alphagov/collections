1bdbe94a1234c35583848b7549484881daeefa98
David Basalla <davidbasalla@gmail.com>
2017-06-28 13:11:44 +0100

    Set specific order for world taxonomy accordion content
    For the new world taxon pages (eg /world/germany), we require the child taxons to be displayed in a specific order. This commit adds short-term solution. Ideally we would like to be determine the order somewhere else in the publishing platform, but given the time constraints, this might be the best solution at the moment.
    
    We rely on the title of the taxons so that's quite brittle, but not sure if there's anything better?

---
+++ test/lib/world_taxonomy_sorter_test.rb
@@ -0,0 +1,70 @@
+require 'test_helper'
+
+class FakeTaxon
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+  end
+end
+
+describe WorldTaxonomySorter do
+  setup do
+    child_taxon_titles = [
+     "Birth, death and marriage abroad",
+     "Brexit",
+     "British embassy or high commission",
+     "Coming to the UK",
+     "Emergency help for British nationals",
+     "News and events",
+     "Passports and emergency travel documents",
+     "Living in",
+     "Tax, benefits, pensions and working abroad",
+     "Trade and invest",
+     "Travelling to",
+    ]
+    @child_taxons = child_taxon_titles.map { |title| FakeTaxon.new(title) }
+
+    @expected_ordered_taxon_titles = [
+      "Emergency help for British nationals",
+      "Brexit",
+      "Passports and emergency travel documents",
+      "Travelling to",
+      "Coming to the UK",
+      "Living in",
+      "Tax, benefits, pensions and working abroad",
+      "Birth, death and marriage abroad",
+      "News and events",
+      "Trade and invest",
+      "British embassy or high commission",
+    ]
+  end
+
+  describe '#call' do
+    it 'sorts the given child taxons by the specified sorting order' do
+      sorted_child_taxon_titles = WorldTaxonomySorter.call(@child_taxons).map(&:title)
+
+      assert_equal(@expected_ordered_taxon_titles, sorted_child_taxon_titles)
+    end
+
+    it 'appends any child taxons which did not match to a title in the specified sorting order' do
+      child_taxons = @child_taxons.concat(
+        [
+          FakeTaxon.new("Another taxon"),
+          FakeTaxon.new("And another taxon"),
+        ]
+      )
+
+      expected_ordered_taxon_titles = @expected_ordered_taxon_titles.concat(
+        [
+          "Another taxon",
+          "And another taxon",
+        ]
+      )
+
+      sorted_child_taxon_titles = WorldTaxonomySorter.call(child_taxons).map(&:title)
+
+      assert_equal(expected_ordered_taxon_titles, sorted_child_taxon_titles)
+    end
+  end
+end
