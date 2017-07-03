28063bee1fc8053e88f9f1244f39bfb450bd48a0
Tijmen Brommet <tijmen@gmail.com>
2017-07-03 09:33:25 +0100

    Stop using `govuk-content-schema-test-helpers`
    The `govuk-content-schema-test-helpers` is being deprecated in favour
    of `govuk_schemas`.

--- test/support/content_schema_helpers.rb
+++ test/support/content_schema_helpers.rb
@@ -1,12 +1,10 @@
 module ContentSchemaHelpers
-  def content_schema_examples_for(format)
-    examples = GovukContentSchemaTestHelpers::Examples.new.get_all_for_format(format)
+  def content_schema_examples_for(schema_name)
     # Shuffle the examples to ensure tests don't become order dependent
-    examples.map { |json_example| JSON.parse(json_example) }.shuffle
+    GovukSchemas::Example.find_all(schema_name).shuffle
   end
 
-  def content_schema_example(format, example_name)
-    json_example = GovukContentSchemaTestHelpers::Examples.new.get(format, example_name)
-    JSON.parse(json_example)
+  def content_schema_example(schema_name, example_name)
+    GovukSchemas::Example.find(schema_name, example_name: example_name)
   end
 end
