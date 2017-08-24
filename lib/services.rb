19021b51844fd67dcc6f6d4ee1746999362f0ef1
Tijmen Brommet <tijmen@gmail.com>
2017-08-24 09:46:25 +0100

    Use Sentry for error reporting
    https://trello.com/c/pcht845E

--- lib/services.rb
+++ lib/services.rb
@@ -1,4 +1,3 @@
-require 'statsd'
 require 'gds_api/email_alert_api'
 require 'gds_api/content_store'
 require 'gds_api/rummager'
@@ -14,13 +13,5 @@ module Services
 
   def self.rummager
     @rummager ||= GdsApi::Rummager.new(Plek.new.find("search"))
-  end
-
-  def self.statsd
-    @statsd ||= begin
-      statsd_client = Statsd.new("localhost")
-      statsd_client.namespace = ENV['GOVUK_STATSD_PREFIX'].to_s
-      statsd_client
-    end
   end
 end
