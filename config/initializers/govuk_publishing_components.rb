241941d4eeac90866261c2717685fc597bf90666
Paul Hayes <fofr@users.noreply.github.com>
2017-09-25 13:45:19 +0100

    Add component guide
    * Use govuk_publishing_components gem to generate guide
    * Mount at /component-guide and indicate presence at startup
    * Include minutest integration tests for automated accessibility testing

---
+++ config/initializers/govuk_publishing_components.rb
@@ -0,0 +1,13 @@
+if defined?(GovukPublishingComponents)
+  GovukPublishingComponents.configure do |c|
+    c.component_guide_title = "Collections Component Guide"
+  end
+
+  if Rails.env.development?
+    startup_message = "=> collections component guide available at: /component-guide"
+    color_blue = 36
+
+    # https://stackoverflow.com/questions/1489183/colorized-ruby-output
+    puts "\e[#{color_blue}m#{startup_message}\e[0m"
+  end
+end
--- test/integration_test_helper.rb
+++ test/integration_test_helper.rb
@@ -3,6 +3,10 @@ require_relative "test_helper"
 require 'capybara/rails'
 require 'slimmer/test'
 require 'slimmer/test_helpers/govuk_components'
+require 'capybara/poltergeist'
+require 'phantomjs/poltergeist'
+
+Capybara.javascript_driver = :poltergeist
 
 class ActionDispatch::IntegrationTest
   include Capybara::DSL
