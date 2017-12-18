b501a7ee2b271dbb1a8d90f496dbef9d44bb1009
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2017-12-18 14:20:33 +0000

    Add print styles
    - add mechanism for displaying print styles
    - add print css, includes only print styles for task list

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,7 @@ Rails.application.config.assets.precompile += %w(
   application-ie6.css
   application-ie7.css
   application-ie8.css
+  print.css
 )
 
 Rails.application.config.assets.prefix = "/collections"
