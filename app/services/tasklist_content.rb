dfa72fb6850577d07b6204e6c463dbec1f3aea28
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2018-01-29 17:33:03 +0000

    Refactor tasklist_controller
    Change the show action to cater for all hard coded step by step navigation
    pages rather than having an action per page.
    
    This gives us a head start on rendering pages from the content store and is
    the first part of https://trello.com/c/3pQPJDmj/440-allow-collections-to-render-task-lists-from-the-publishing-api

--- app/services/tasklist_content.rb
+++ app/services/tasklist_content.rb
@@ -1,16 +1,4 @@
 class TasklistContent
-  def self.learn_to_drive_config
-    @learn_to_drive_config ||= find_file("learn-to-drive-a-car")
-  end
-
-  def self.end_a_civil_partnership_config
-    @end_a_civil_partnership_config ||= find_file("end-a-civil-partnership")
-  end
-
-  def self.get_a_divorce_config
-    @get_a_divorce_config ||= find_file("get-a-divorce")
-  end
-
   def self.find_file(filename)
     JSON.parse(
       File.read(
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,8 @@ Rails.application.routes.draw do
   mount GovukPublishingComponents::Engine, at: "/component-guide" if defined?(GovukPublishingComponents)
 
   get "/learn-to-drive-a-car", to: 'tasklist#show'
-  get "/get-a-divorce", to: 'tasklist#show_get_a_divorce'
-
-  get "/end-a-civil-partnership", to: 'tasklist#show_end_a_civil_partnership'
+  get "/get-a-divorce", to: 'tasklist#show'
+  get "/end-a-civil-partnership", to: 'tasklist#show'
 
   get "/browse.json" => redirect("/api/content/browse")
 
