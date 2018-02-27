8f41f10dbb166971215d035e2b715a59a846e6f7
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2018-02-27 14:03:03 +0000

    Remove hard coded step nav
    We're no longer using this.

--- app/services/step_nav_content.rb
+++ app/services/step_nav_content.rb
@@ -1,14 +1,5 @@
 class StepNavContent
-  def self.find_file(filename)
-    JSON.parse(
-      File.read(
-        Rails.root.join("config", "step_nav", "#{filename}.json")
-      )
-    ).deep_symbolize_keys
-  end
-
   def self.from_content_item(content_item)
     content_item.details["step_by_step_nav"].deep_symbolize_keys
   end
-
 end
