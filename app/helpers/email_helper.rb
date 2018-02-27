81b1524e1e2b34c6b2b24a01e869495313850a55
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2018-02-27 16:29:18 +0000

    Add phase check for email alerts
    This commit reverts #516, which in turn reverted #505, #507 and #508.
    
    In preparation for publishing the taxonomy in phases of alpha, beta or live, we want to make sure that we only show email alert buttons for taxons that are live and that if a taxon is not live we stop it being indexed by search engines.

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -11,6 +11,10 @@ module EmailHelper
     whitehall_base_email_sign_up_url + whitehall_atom_url
   end
 
+  def taxon_is_live?(presented_taxon)
+    presented_taxon.live_taxon?
+  end
+
 private
 
   WHITEHALL_EMAIL_SIGNUP_PATH = "/government/email-signup/new?email_signup[feed]=".freeze
