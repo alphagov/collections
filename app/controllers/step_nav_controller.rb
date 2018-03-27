ed53359c1e5eab03a5cc37272b2d6b68599c50f5
Tijmen Brommet <tijmen@gmail.com>
2018-03-27 18:29:51 +0100

    Move all controller code into a single method
    This makes it easier to see what's going on and to refactor.

--- app/controllers/step_nav_controller.rb
+++ app/controllers/step_nav_controller.rb
@@ -1,27 +1,15 @@
 class StepNavController < ApplicationController
   def show
+    step_nav = StepNav.find!(request.path)
+
     setup_content_item_and_navigation_helpers(step_nav)
 
-    step_nav_content = StepNavContent.from_content_item(step_nav)
+    step_nav_content = step_nav.details["step_by_step_nav"].deep_symbolize_keys
 
     render :show, locals: {
       content_item: step_nav,
       navigation_helpers: @navigation_helpers,
       step_nav: step_nav_content
     }
-  end
-
-private
-
-  def base_path
-    request.path
-  end
-
-  def slug
-    base_path[1..-1]
-  end
-
-  def step_nav
-    StepNav.find!(base_path)
   end
 end
