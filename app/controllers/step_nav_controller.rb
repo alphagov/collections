5942c0ab5e01a384c1790c4a443c7224d684d1ed
Tijmen Brommet <tijmen@gmail.com>
2018-03-27 18:29:51 +0100

    Reformat controller

--- app/controllers/step_nav_controller.rb
+++ app/controllers/step_nav_controller.rb
@@ -1,11 +1,7 @@
 class StepNavController < ApplicationController
   def show
     step_by_step = StepNav.find!(request.path)
-
     setup_content_item_and_navigation_helpers(step_by_step)
-
-    render :show, locals: {
-      step_by_step: step_by_step,
-    }
+    render :show, locals: { step_by_step: step_by_step }
   end
 end
