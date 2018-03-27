4466e4054fe884002026fa987b09186ca2a90ce2
Tijmen Brommet <tijmen@gmail.com>
2018-03-27 18:29:51 +0100

    Move content-fetching method to StepNav class

--- app/controllers/step_nav_controller.rb
+++ app/controllers/step_nav_controller.rb
@@ -4,12 +4,10 @@ class StepNavController < ApplicationController
 
     setup_content_item_and_navigation_helpers(step_nav)
 
-    step_nav_content = step_nav.details["step_by_step_nav"].deep_symbolize_keys
-
     render :show, locals: {
       content_item: step_nav,
       navigation_helpers: @navigation_helpers,
-      step_nav: step_nav_content
+      step_nav: step_nav.content
     }
   end
 end
