5f1fc888d7d900a4d79938934447615fd09a4bf7
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2018-04-17 14:22:42 +0100

    Prioritise consultations on topic pages
    Show consultations first in the list of policy and engagement content

--- app/presenters/supergroups/policy_and_engagement.rb
+++ app/presenters/supergroups/policy_and_engagement.rb
@@ -8,6 +8,13 @@ module Supergroups
 
     def tagged_content(taxon_id)
       @content = MostRecentContent.fetch(content_id: taxon_id, filter_content_purpose_supergroup: @name)
+      @content.select { |content_item| consultation?(content_item.content_store_document_type) } | @content
+    end
+
+    def consultation?(document_type)
+      document_type == 'open_consultation' ||
+        document_type == 'consultation_outcome' ||
+        document_type == 'closed_consultation'
     end
   end
 end
