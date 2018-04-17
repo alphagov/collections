f090b67d3d40b3d846d0fd35761346af28d2a4ff
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2018-04-17 14:22:42 +0100

    Prioritise consultations on topic pages
    Show consultations first in the list of policy and engagement content

--- app/presenters/supergroups/policy_and_engagement.rb
+++ app/presenters/supergroups/policy_and_engagement.rb
@@ -8,6 +8,15 @@ module Supergroups
 
     def tagged_content(taxon_id)
       @content = MostRecentContent.fetch(content_id: taxon_id, filter_content_purpose_supergroup: @name)
+      @content.select do |content_item|
+        consultation?(content_item.content_store_document_type)
+      end | @content
+    end
+
+    def consultation?(document_type)
+      document_type == 'open_consultation' ||
+      document_type == 'consultation_outcome' ||
+      document_type == 'closed_consultation'
     end
   end
 end
--- test/presenters/supergroups/policy_and_engagement_test.rb
+++ test/presenters/supergroups/policy_and_engagement_test.rb
@@ -28,5 +28,78 @@ describe Supergroups::PolicyAndEngagement do
 
       assert_equal expected, policy_and_engagement_supergroup.document_list(taxon_id)
     end
+
+    it 'prioritises consultations over other content' do
+      tagged_content = []
+      tagged_content.push(*section_tagged_content_list('consultation_outcome'))
+      tagged_content.push(*section_tagged_content_list('case_study'))
+      tagged_content.push(*section_tagged_content_list('open_consultation'))
+      tagged_content.push(*section_tagged_content_list('case_study'))
+      tagged_content.push(*section_tagged_content_list('closed_consultation'))
+
+      MostRecentContent.any_instance
+        .stubs(:fetch)
+        .returns(tagged_content)
+
+      expected = [
+        {
+          link: {
+            text: 'Tagged Content Title',
+            path: '/government/tagged/content'
+          },
+          metadata: {
+            public_updated_at: '2018-02-28T08:01:00.000+00:00',
+            organisations: 'Tagged Content Organisation',
+            document_type: 'Consultation outcome'
+          }
+        },
+        {
+          link: {
+            text: 'Tagged Content Title',
+            path: '/government/tagged/content'
+          },
+          metadata: {
+            public_updated_at: '2018-02-28T08:01:00.000+00:00',
+            organisations: 'Tagged Content Organisation',
+            document_type: 'Open consultation'
+          }
+        },
+        {
+          link: {
+            text: 'Tagged Content Title',
+            path: '/government/tagged/content'
+          },
+          metadata: {
+            public_updated_at: '2018-02-28T08:01:00.000+00:00',
+            organisations: 'Tagged Content Organisation',
+            document_type: 'Closed consultation'
+          }
+        },
+        {
+          link: {
+            text: 'Tagged Content Title',
+            path: '/government/tagged/content'
+          },
+          metadata: {
+            public_updated_at: '2018-02-28T08:01:00.000+00:00',
+            organisations: 'Tagged Content Organisation',
+            document_type: 'Case study'
+          }
+        },
+        {
+          link: {
+            text: 'Tagged Content Title',
+            path: '/government/tagged/content'
+          },
+          metadata: {
+            public_updated_at: '2018-02-28T08:01:00.000+00:00',
+            organisations: 'Tagged Content Organisation',
+            document_type: 'Case study'
+          }
+        }
+      ]
+
+      assert_equal expected, policy_and_engagement_supergroup.document_list(taxon_id)
+    end
   end
 end
