1907a568165e8c8e734e6144c129d52ebda546bc
Tijmen Brommet <tijmen@gmail.com>
2018-05-02 16:28:31 +0100

    Remove special config for errors
    This might be interfering with the default error handling, which excludes `GdsApi::TimedOutException` errors raised by the application.

--- config/initializers/govuk_error.rb
+++
@@ -1,8 +0,0 @@
-GovukError.configure do |config|
-  # A couple of hundred timeouts occur each day. This can be caused by
-  # rummager or content-store being busy, or network issues. They generally
-  # aren't a big deal, since Fastly will show the mirror. We can safely
-  # ignore the errors because we have other monitoring in place that alerts
-  # us if the error rate reaches certain thresholds.
-  config.excluded_exceptions << "GdsApi::TimedOutException"
-end
