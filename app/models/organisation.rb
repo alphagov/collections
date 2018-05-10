ba694217a73729aee1c6ec545e3ec80fd3b48bc0
mahmudh <mahmud.hussain@digital.cabinet-office.gov.uk>
2018-05-10 17:04:56 +0100

    Create organisations index page - Display organisations - Display agencies and public bodies each organisation works with

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -4,13 +4,45 @@ class Organisation
   include ActiveModel::Model
 
   attr_accessor(
-    :title,
-    :content_id,
-    :link,
-    :slug,
-    :organisation_state,
-    :document_count
+      :title,
+      :content_id,
+      :link,
+      :slug,
+      :organisation_state,
+      :document_count
   )
+
+  attr_reader :content_item
+
+  def initialize(content_item)
+    @content_item = content_item
+  end
+
+  def self.find!(base_path)
+    content_item = ContentItem.find!(base_path)
+    new(content_item)
+  end
+
+  def organisations_count(department_type)
+    @content_item.content_item_data["details"][department_type].count
+  end
+
+  def organisations_list(department_type)
+    @content_item.content_item_data["details"][department_type]
+  end
+
+  def organisation_department_count(organisation)
+    organisation["works_with"].flatten.count
+  end
+
+  def get_name_of_organisation(organisation_id)
+    case organisation_id
+    when "ordered_ministrial_departments"
+      "Ministerial departments"
+    else
+      "Not yet named"
+    end
+  end
 
   def live?
     organisation_state == 'live'
