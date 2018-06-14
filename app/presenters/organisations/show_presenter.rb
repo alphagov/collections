1f7f0f53289d41fc8817391f0d91d622dce0d833
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2018-06-14 14:07:07 +0000

    Add 'our policies' section

--- app/presenters/organisations/show_presenter.rb
+++ app/presenters/organisations/show_presenter.rb
@@ -1,24 +1,22 @@
 module Organisations
   class ShowPresenter
-    attr_reader :org
-
     def initialize(organisation)
       @org = organisation
     end
 
     def prefixed_title
-      prefix = needs_definite_article?(org.title) ? "the " : ""
-      (prefix + org.title)
+      prefix = needs_definite_article?(@org.title) ? "the " : ""
+      (prefix + @org.title)
     end
 
     def has_latest_documents?
-      org.ordered_featured_documents.length.positive?
+      @org.ordered_featured_documents.length.positive?
     end
 
     def latest_documents
       documents = []
 
-      org.ordered_featured_documents.each do |story|
+      @org.ordered_featured_documents.each do |story|
         metadata = {}
         metadata[:document_type] = story["document_type"]
 
@@ -37,15 +35,44 @@ module Organisations
 
       {
         items: documents,
-        brand: org.brand
+        brand: @org.brand
       }
     end
 
     def subscription_links
       {
-        email_signup_link: "/government/email-signup/new?email_signup[feed]=#{org.web_url}.atom",
-        feed_link_box_value: "#{org.web_url}.atom",
-        brand: org.brand
+        email_signup_link: "/government/email-signup/new?email_signup[feed]=#{@org.web_url}.atom",
+        feed_link_box_value: "#{@org.web_url}.atom",
+        brand: @org.brand
+      }
+    end
+
+    def has_featured_policies?
+      true if @org.ordered_featured_policies && @org.ordered_featured_policies.length.positive?
+    end
+
+    def featured_policies
+      policies = []
+
+      @org.ordered_featured_policies.each do |policy|
+        policies << {
+          link: {
+            text: policy["title"],
+            path: policy["base_path"]
+          }
+        }
+      end
+
+      policies << {
+        link: {
+          text: "See all our policies",
+          path: "/government/policies?organisations[]=#{@org.slug}"
+        }
+      }
+
+      {
+        items: policies,
+        brand: @org.brand
       }
     end
 
