2afc0f4673c621ac3181a7cfbe7c04820ac6c9f1
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2018-06-18 13:48:03 +0100

    Get latest documents from rummager for organisations
    This commit fetches the latest documents, announcements, consultations, publications and statistics for an organisation from rummager.

--- app/presenters/organisations/show_presenter.rb
+++ app/presenters/organisations/show_presenter.rb
@@ -10,33 +10,77 @@ module Organisations
     end
 
     def has_latest_documents?
-      @org.ordered_featured_documents.length.positive?
+      latest_documents.length.positive?
     end
 
     def latest_documents
-      documents = []
+      @latest_documents ||= Services.rummager.search(
+        count: 3,
+        order: "-public_timestamp",
+        filter_organisations: @org.slug,
+        fields: %w[title link content_store_document_type public_timestamp]
+      )["results"]
+      search_results_to_documents(@latest_documents)
+    end
 
-      @org.ordered_featured_documents.each do |story|
-        metadata = {}
-        metadata[:document_type] = story["document_type"]
+    def has_latest_announcements?
+      latest_announcements.length.positive?
+    end
 
-        if story["public_updated_at"]
-          metadata[:public_updated_at] = Date.parse(story["public_updated_at"])
-        end
+    def latest_announcements
+      @latest_announcements ||= Services.rummager.search(
+        count: 2,
+        order: "-public_timestamp",
+        filter_organisations: @org.slug,
+        filter_email_document_supertype: "announcements",
+        fields: %w[title link content_store_document_type public_timestamp]
+      )["results"]
+      search_results_to_documents(@latest_announcements)
+    end
 
-        documents << {
-          link: {
-            text: story["title"],
-            path: story["href"]
-          },
-          metadata: metadata
-        }
-      end
+    def has_latest_consultations?
+      latest_consultations.length.positive?
+    end
 
-      {
-        items: documents,
-        brand: @org.brand
-      }
+    def latest_consultations
+      @latest_consultations ||= Services.rummager.search(
+        count: 2,
+        order: "-public_timestamp",
+        filter_organisations: @org.slug,
+        filter_government_document_supertype: "consultations",
+        fields: %w[title link content_store_document_type public_timestamp]
+      )["results"]
+      search_results_to_documents(@latest_consultations)
+    end
+
+    def has_latest_publications?
+      latest_publications.length.positive?
+    end
+
+    def latest_publications
+      @latest_publications ||= Services.rummager.search(
+        count: 2,
+        order: "-public_timestamp",
+        filter_organisations: @org.slug,
+        filter_email_document_supertype: "publications",
+        fields: %w[title link content_store_document_type public_timestamp]
+      )["results"]
+      search_results_to_documents(@latest_publications)
+    end
+
+    def has_latest_statistics?
+      latest_statistics.length.positive?
+    end
+
+    def latest_statistics
+      @latest_statistics ||= Services.rummager.search(
+        count: 2,
+        order: "-public_timestamp",
+        filter_organisations: @org.slug,
+        filter_government_document_supertype: "statistics",
+        fields: %w[title link content_store_document_type public_timestamp]
+      )["results"]
+      search_results_to_documents(@latest_statistics)
     end
 
     def subscription_links
@@ -77,6 +121,32 @@ module Organisations
     end
 
   private
+
+    def search_results_to_documents(search_results)
+      documents = []
+
+      search_results.each do |item|
+        metadata = {}
+        metadata[:document_type] = item["content_store_document_type"].capitalize.tr("_", " ")
+
+        if item["public_timestamp"]
+          metadata[:public_updated_at] = Date.parse(item["public_timestamp"])
+        end
+
+        documents << {
+          link: {
+            text: item["title"],
+            path: item["link"]
+          },
+          metadata: metadata
+        }
+      end
+
+      {
+        items: documents,
+        brand: @org.brand
+      }
+    end
 
     def needs_definite_article?(phrase)
       exceptions = [/civil service resourcing/, /^hm/, /ordnance survey/]
