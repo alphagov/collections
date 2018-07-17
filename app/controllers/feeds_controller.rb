d382bf7b1d6639ead0c405ad72ade4e085871fe8
Tijmen Brommet <tijmen@gmail.com>
2018-07-17 12:41:57 +0100

    Allow view to be reused for other feeds
    This allows us to pass in certain things that will differ between
    organisation feeds and the global feed.

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,5 +1,5 @@
 class FeedsController < ApplicationController
-  enable_request_formats organisation: :atom
+  enable_request_formats organisation: :atom, all: :atom
 
   before_action do
     # Allows ajax requests as per https://govuk.zendesk.com/agent/tickets/1935680
@@ -13,7 +13,9 @@ class FeedsController < ApplicationController
     @organisation = Organisation.find!(path)
 
     results = FeedContent.new(filter_organisations: organisation_name).results
-    @items = results.map { |result| FeedEntryPresenter.new(result) }
+    items = results.map { |result| FeedEntryPresenter.new(result) }
+
+    render :feed, locals: { items: items, root_url: @organisation.web_url, title: "#{@organisation.title} - Activity on GOV.UK" }
   end
 
 private
--- app/views/feeds/feed.atom.builder
+++ app/views/feeds/feed.atom.builder
@@ -1,11 +1,13 @@
-atom_feed(language: 'en-GB', root_url: @organisation.web_url) do |feed|
-  feed.title("#{@organisation.title} - Activity on GOV.UK")
+atom_feed(language: 'en-GB', root_url: root_url) do |feed|
+  feed.title(title)
+
   feed.author do |author|
     author.name('HM Government')
   end
-  feed.updated(@items.first.updated) if @items.any?
 
-  @items.each do |item|
+  feed.updated(items.first.updated) if items.any?
+
+  items.each do |item|
     feed.entry(item, id: item.id, url: item.url, updated: item.updated) do |entry|
       entry.title(item.title)
       if item.display_type
