6e9b5cac8be3b492aada88da6689c8725ad71ec5
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2018-09-05 15:45:32 +0000

    Show tagged taxons for documents

--- app/presenters/browse_presenter.rb
+++ app/presenters/browse_presenter.rb
@@ -1,4 +1,6 @@
 class BrowsePresenter
+  include ActionView::Helpers::UrlHelper
+
   def top_level_taxons
     # Retrieve the root taxon (GOV.UK homepage)
     top_level_taxons = Services.content_store.content_item('/').dig('links', 'level_one_taxons').map do |top_level_taxon|
@@ -14,6 +16,30 @@ class BrowsePresenter
 
   def all_supergroups
     %w(services guidance_and_regulation news_and_communications policy_and_engagement research_and_statistics transparency other)
+  end
+
+  def associated_taxons(base_path, parent_taxon_path)
+    associated_taxons = Services.content_store.content_item(base_path).dig('links', 'taxons')
+
+    associated_taxons.each do |taxon|
+      if parent_taxon_path.eql?("/business-and-industry")
+        associated_taxons.delete_if {|taxon| !taxon["base_path"].include?(parent_taxon_path) && !taxon["base_path"].include?("/business") }
+      elsif parent_taxon_path.eql?("/defence-and-armed-forces")
+        associated_taxons.delete_if {|taxon| !taxon["base_path"].include?(parent_taxon_path) && !taxon["base_path"].include?("/defence") }
+      elsif parent_taxon_path.eql?("/government/all")
+        associated_taxons.delete_if {|taxon| !taxon["base_path"].include?(parent_taxon_path) && !taxon["base_path"].include?("/government") }
+      elsif parent_taxon_path.eql?("/work")
+        associated_taxons.delete_if {|taxon| !taxon["base_path"].include?(parent_taxon_path) && !taxon["base_path"].include?("/employment") }
+      else
+        associated_taxons.delete_if {|taxon| !taxon["base_path"].include?(parent_taxon_path) && !taxon["base_path"].include?("/business") }
+      end
+    end
+
+    taxon_links = associated_taxons.map do |taxon|
+      link_to taxon["title"], taxon["base_path"]
+    end
+
+    taxon_links.to_sentence.html_safe
   end
 
 private
