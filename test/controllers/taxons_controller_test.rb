9b9b7e3f8b7858ec824803b2a90e31792ca0e942
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-12-05 14:59:46 +0000

    Consolidate the TaxonsController tests
    Previously, there were two files, one without any expectations, and
    one with some expectations, but a slightly odd name. This commit
    consolidates the two files in to one.

--- test/controllers/taxons_controller_test.rb
+++ test/controllers/taxons_controller_test.rb
@@ -2,14 +2,41 @@ require "test_helper"
 
 describe TaxonsController do
   include RummagerHelpers
+  include GovukAbTesting::MinitestHelpers
+  include TaxonHelpers
 
   describe "GET show" do
     before do
-      content_store_has_item("/education",
-                             content_id: "education-content-id",
-                             base_path: "/education",
-                             title: "Education")
-      stub_content_for_taxon("education-content-id", [])
+      content_store_has_item(taxon["base_path"], taxon)
+      stub_content_for_taxon(taxon["content_id"], [taxon])
+      stub_content_for_taxon(taxon["content_id"], generate_search_results(5))
+      stub_document_types_for_supergroup('guidance_and_regulation')
+      stub_most_popular_content_for_taxon(taxon["content_id"], tagged_content_for_guidance_and_regulation, filter_content_store_document_type: 'guidance_and_regulation')
+      stub_document_types_for_supergroup('services')
+      stub_most_popular_content_for_taxon(taxon["content_id"], tagged_content_for_services, filter_content_store_document_type: 'services')
+      stub_document_types_for_supergroup('news_and_communications')
+      stub_most_recent_content_for_taxon(taxon["content_id"], tagged_content_for_news_and_communications, filter_content_store_document_type: 'news_and_communications')
+      stub_document_types_for_supergroup('policy_and_engagement')
+      stub_most_recent_content_for_taxon(taxon["content_id"], tagged_content_for_policy_and_engagement, filter_content_store_document_type: 'policy_and_engagement')
+      stub_document_types_for_supergroup('transparency')
+      stub_most_recent_content_for_taxon(taxon["content_id"], tagged_content_for_transparency, filter_content_store_document_type: 'transparency')
+      stub_document_types_for_supergroup('research_and_statistics')
+      stub_most_recent_content_for_taxon(taxon["content_id"], tagged_content_for_research_and_statistics, filter_content_store_document_type: 'research_and_statistics')
+      stub_organisations_for_taxon(taxon["content_id"], tagged_organisations)
+    end
+
+    %w(A B C D E).each do |test_variant|
+      it "TopicPagesTest works correctly for each variant (variant: #{test_variant})" do
+        with_variant TopicPagesTest: test_variant do
+          get :show, params: { taxon_base_path: "alpha-taxonomy/a-level" }
+          assert_response 200
+          assert_template "show_#{test_variant.downcase}"
+
+          ab_test = @controller.send(:taxon_page_test)
+          requested = ab_test.requested_variant(request.headers)
+          assert requested.variant?(test_variant)
+        end
+      end
     end
   end
 end
