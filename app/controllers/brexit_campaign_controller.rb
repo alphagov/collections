8a5e86d760701c27dca9b88f67c7b8c4a8053c2c
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2018-12-05 15:57:00 +0000

    Add links to other campaigns on show_b

--- app/controllers/brexit_campaign_controller.rb
+++ app/controllers/brexit_campaign_controller.rb
@@ -66,6 +66,16 @@ class BrexitCampaignController < ApplicationController
 
     @documents = Organisations::DocumentsPresenter.new(@campaign)
 
+    @campaign_links = {
+      random: SecureRandom.hex(4),
+      section_title: "There's different guidance if you're:",
+      contents_list_links: [
+        { text: "Running a business", href: "https://tiger-team-5-campaign-business-ready.glitch.me/" },
+        { text: "A UK national living in the EU", href: "https://uk-nationals-in-eu.herokuapp.com" },
+        { text: "An EU national living in the UK", href: "https://draft-origin.publishing.service.gov.uk/continue-live-uk-after-leaves-eu?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjMWU0ZGJhNy1kMzU1LTQ1ZTQtYTk0OC01OTI3MmIzZWUzYWEifQ.13RMXDLpk-lg0d8j8UEXEZAR58tYgP6jkaFPGFNXs88" },
+      ]
+    }
+
     render :show_b, locals: {
       campaign: @campaign
     }
