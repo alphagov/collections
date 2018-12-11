b750d423a6463a95e2aa2028cc046d2208ea8232
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2018-12-11 09:41:08 +0000

    Update other campaign links

--- app/controllers/brexit_campaign_controller.rb
+++ app/controllers/brexit_campaign_controller.rb
@@ -20,9 +20,9 @@ private
     {
       section_title: "There's different guidance if you're:",
       contents_list_links: [
-        { text: "running a business", href: "https://tiger-team-5-campaign-business-ready.glitch.me/" },
-        { text: "a UK national living in the EU", href: "https://uk-nationals-in-eu.herokuapp.com" },
-        { text: "an EU national living in the UK", href: "https://draft-origin.publishing.service.gov.uk/continue-live-uk-after-leaves-eu?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjMWU0ZGJhNy1kMzU1LTQ1ZTQtYTk0OC01OTI3MmIzZWUzYWEifQ.13RMXDLpk-lg0d8j8UEXEZAR58tYgP6jkaFPGFNXs88" },
+        { text: "running a business", href: "https://euexitbusiness.campaign.gov.uk" },
+        { text: "a UK national living in the EU", href: "/uk-nationals-living-eu" },
+        { text: "an EU national living in the UK", href: "/staying-uk-eu-citizen" },
       ]
     }
   end
