5e06f4eba8689c38c4b3b33bf3e7593112427d97
Erin Raj-Staniland <erin.raj-staniland@digital.cabinet-office.gov.uk>
2019-03-29 12:16:16 +0000

    Actually use the GOV.UK test gem
    Use the GOV.UK test gem to configure Selenium and other test
    dependencies. Previous the gem was included but not configured.

--- test/integration_test_helper.rb
+++ test/integration_test_helper.rb
@@ -3,17 +3,7 @@ require_relative "test_helper"
 require 'capybara/rails'
 require 'slimmer/test'
 
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w(headless disable-gpu) }
-  )
-
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    desired_capabilities: capabilities
-  )
-end
+GovukTest.configure
 
 Capybara.javascript_driver = :headless_chrome
 
