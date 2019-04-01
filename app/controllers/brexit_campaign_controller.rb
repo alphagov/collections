1eeb8e9ceecce16e9f24f35785859c14034203e1
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2019-04-01 17:52:55 +0100

    Move campaign links to sidebar

--- app/controllers/brexit_campaign_controller.rb
+++ app/controllers/brexit_campaign_controller.rb
@@ -7,24 +7,8 @@ class BrexitCampaignController < ApplicationController
     @featured_links = presenter.featured_links
     @other_links = presenter.other_links
 
-    @campaign_links = campaign_links
-
     render :show, locals: {
       campaign: @campaign
-    }
-  end
-
-private
-
-  def campaign_links
-    {
-      section_title: "Other guidance",
-      description: "There's different guidance if you're:",
-      contents_list_links: [
-        { text: "running a business", href: "/business-uk-leaving-eu" },
-        { text: "a UK national living in the EU", href: "/uk-nationals-living-eu" },
-        { text: "an EU national and you want to continue living in the UK", href: "/staying-uk-eu-citizen" },
-      ]
     }
   end
 end
--- app/presenters/citizen_readiness/links_presenter.rb
+++ app/presenters/citizen_readiness/links_presenter.rb
@@ -27,11 +27,11 @@ module CitizenReadiness
     end
 
     def other_links
-      @other_links ||= level_one_taxons
-        .reject { |content_item| FEATURED_TAXONS.include?(content_item.base_path) }
-        .reject { |content_item| REJECTED_TAXONS.include?(content_item.base_path) }
-        .select { |content_item| taxon_ids_with_citizen_tagged_content.include?(content_item.content_id) }
-        .map { |content_item| TaxonLinkPresenter.new(content_item) }
+      @other_links ||= %w(
+        /business-uk-leaving-eu
+        /uk-nationals-living-eu
+        /staying-uk-eu-citizen
+      ).map { |path| LinkPresenter.new(path) }
     end
 
   private
@@ -51,27 +51,6 @@ module CitizenReadiness
 
     def level_one_taxons
       @level_one_taxons ||= ContentItem.find!('/').linked_items('level_one_taxons')
-    end
-
-    def taxon_ids_with_citizen_tagged_content
-      @taxon_ids_with_citizen_tagged_content ||= begin
-        params = {
-          count: 0,
-          facet_part_of_taxonomy_tree: "1000,examples:0,scope:all_filters",
-          filter_taxons: CITIZEN_TAXON_CONTENT_ID,
-          filter_content_store_document_type: document_types
-        }
-
-        search_result = RummagerSearch.new(params)
-
-        search_result.facets["part_of_taxonomy_tree"]["options"].map do |item|
-          item.dig("value", "slug")
-        end
-      end
-    end
-
-    def document_types
-      %w(travel_advice_index) + GovukDocumentTypes.supergroup_document_types('guidance_and_regulation', 'services')
     end
   end
 end
