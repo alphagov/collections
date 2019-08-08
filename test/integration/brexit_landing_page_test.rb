5632ed3529c743f8f099fa9e05c25a4dd9fc19dc
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2019-08-08 04:31:22 +0000

    Add tests

--- test/integration/brexit_landing_page_test.rb
+++ test/integration/brexit_landing_page_test.rb
@@ -8,21 +8,7 @@ class BrexitLandingPageTest < ActionDispatch::IntegrationTest
     given_there_is_a_brexit_taxon
     when_i_visit_the_brexit_landing_page
     then_i_can_see_the_title_section
-    then_i_can_see_navigation_to_brexit_pages
-    and_i_can_see_the_email_signup_link
-    and_i_can_see_the_services_section
-    and_i_can_see_the_guidance_and_regulation_section
-    and_i_can_see_the_news_and_communications_section
-    and_i_can_see_the_policy_papers_and_consulations_section
-    and_i_can_see_the_transparency_and_foi_releases_section
-    and_i_can_see_the_research_and_statistics_section
-    and_i_can_see_the_organisations_section
-  end
-
-  it 'renders an in-page nav' do
-    given_there_is_a_brexit_taxon
-    when_i_visit_the_brexit_landing_page
-    and_i_can_see_the_in_page_nav
+    and_i_can_see_the_explore_topics_section
   end
 
   it "has tracking on all links" do
