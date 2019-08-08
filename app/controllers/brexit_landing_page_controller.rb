9a36b50e9edc9332c184b37384a0cf6305705517
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2019-08-08 03:16:56 +0000

    Add explore the topic section

--- app/controllers/brexit_landing_page_controller.rb
+++ app/controllers/brexit_landing_page_controller.rb
@@ -15,7 +15,7 @@ private
   end
 
   def presented_taxon
-    @presented_taxon ||= TaxonPresenter.new(taxon)
+    @presented_taxon ||= BrexitLandingPagePresenter.new(taxon)
   end
 
   def presentable_section_items
@@ -24,12 +24,6 @@ private
           href: "##{section[:id]}",
           text: t(section[:id], scope: :content_purpose_supergroup, default: section[:title])
       }
-    end
-
-    @presentable_section_items << { href: "#organisations", text: t('taxons.organisations') }
-
-    if presented_taxon.show_subtopic_grid?
-      @presentable_section_items << { href: "#sub-topics", text: t('taxons.explore_sub_topics') }
     end
 
     @presentable_section_items
