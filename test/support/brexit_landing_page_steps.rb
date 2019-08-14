39c32962a90cc832c076671afca44ad9a5c9fd17
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2019-08-14 16:39:11 +0100

    Remove unused stubs and instance variables
    The Brexit landing page does not display any content from the search
    api, unlike the Taxon pages so these stubs can be removed.
    
    Also removed a useless memoization

--- test/support/brexit_landing_page_steps.rb
+++ test/support/brexit_landing_page_steps.rb
@@ -8,7 +8,6 @@ module BrexitLandingPageSteps
 
   def given_there_is_a_brexit_taxon
     content_store_has_item(brexit_taxon_path, content_item)
-    and_the_taxon_has_tagged_content
   end
 
   def brexit_taxon_path
@@ -20,38 +19,19 @@ module BrexitLandingPageSteps
   end
 
   def content_item
-    @content_item ||= begin
-      GovukSchemas::RandomExample.for_schema(frontend_schema: "taxon") do |item|
-        item.merge(
-          "base_path" => brexit_taxon_path,
-          "content_id" => content_id,
-          "title" => "Brexit",
-          "phase" => "live",
-          "links" => {},
-        )
-      end
+    GovukSchemas::RandomExample.for_schema(frontend_schema: "taxon") do |item|
+      item.merge(
+        "base_path" => brexit_taxon_path,
+        "content_id" => content_id,
+        "title" => "Brexit",
+        "phase" => "live",
+        "links" => {},
+      )
     end
   end
 
   def when_i_visit_the_brexit_landing_page
     visit brexit_taxon_path
-  end
-
-  def and_the_taxon_has_tagged_content
-    # We still need to stub tagged content because it is used by the sub-topic grid
-    stub_content_for_taxon(content_id, tagged_content)
-    stub_document_types_for_supergroup('guidance_and_regulation')
-    stub_most_popular_content_for_taxon(content_id, tagged_content_for_guidance_and_regulation, filter_content_store_document_type: 'guidance_and_regulation')
-    stub_document_types_for_supergroup('services')
-    stub_most_popular_content_for_taxon(content_id, tagged_content_for_services, filter_content_store_document_type: 'services')
-    stub_document_types_for_supergroup('news_and_communications')
-    stub_most_recent_content_for_taxon(content_id, tagged_content_for_news_and_communications, filter_content_store_document_type: 'news_and_communications')
-    stub_document_types_for_supergroup('policy_and_engagement')
-    stub_most_recent_content_for_taxon(content_id, tagged_content_for_policy_and_engagement, filter_content_store_document_type: 'policy_and_engagement')
-    stub_document_types_for_supergroup('transparency')
-    stub_most_recent_content_for_taxon(content_id, tagged_content_for_transparency, filter_content_store_document_type: 'transparency')
-    stub_document_types_for_supergroup('research_and_statistics')
-    stub_most_recent_content_for_taxon(content_id, tagged_content_for_research_and_statistics, filter_content_store_document_type: 'research_and_statistics')
   end
 
   def then_i_can_see_the_title_section
@@ -134,40 +114,5 @@ module BrexitLandingPageSteps
   def and_the_email_link_is_tracked
     assert page.has_css?("a[data-track-category='emailAlertLinkClicked']", text: "Subscribe to updates to this topic")
     assert page.has_css?("a[data-track-action=\"#{current_path}\"]", text: "Subscribe to updates to this topic")
-  end
-
-  def tagged_content
-    generate_search_results(5)
-  end
-
-  def tagged_content_for_services
-    @tagged_content_for_services ||= generate_search_results(2, "services")
-  end
-
-  def tagged_content_for_guidance_and_regulation
-    @tagged_content_for_guidance_and_regulation ||= generate_search_results(2, 'guidance_and_regulation')
-  end
-
-  def tagged_content_for_news_and_communications
-    @tagged_content_for_news_and_communications ||= generate_search_results(2, "news_and_communications")
-  end
-
-  def tagged_content_for_policy_and_engagement
-    @tagged_content_for_policy_and_engagement ||= generate_search_results(2, "policy_and_engagement")
-  end
-
-  def tagged_content_for_transparency
-    @tagged_content_for_transparency ||= generate_search_results(2, "transparency")
-  end
-
-  def tagged_content_for_research_and_statistics
-    @tagged_content_for_research_and_statistics ||= generate_search_results(2, "research_and_statistics")
-  end
-
-  def finder_query_string
-    {
-      parent: content_item['base_path'],
-      topic: content_item['content_id'],
-    }.to_query
   end
 end
