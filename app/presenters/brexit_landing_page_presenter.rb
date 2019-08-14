1466d1512f0d6eb608185c5a3b2cd68f9d03f368
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2019-08-14 16:43:47 +0100

    Move logic from a view to the presenter.

--- app/presenters/brexit_landing_page_presenter.rb
+++ app/presenters/brexit_landing_page_presenter.rb
@@ -30,12 +30,15 @@ private
   def fetch_buckets
     buckets = YAML.load_file('config/brexit_campaign_buckets.yml')
 
-    buckets.each do |bucket|
+    buckets.each_with_index do |bucket, index|
       bucket.fetch("items", []).each do |link|
         link.symbolize_keys!
         link[:description] = link[:description].html_safe
       end
       bucket["block"] = Govspeak::Document.new(bucket["block"]).to_html.html_safe unless bucket['block'].nil?
+      bucket["index"] = index
+      bucket["display_border"] = index > 1
+      bucket["display_mobile_border"] = index == 1
     end
   end
 end
