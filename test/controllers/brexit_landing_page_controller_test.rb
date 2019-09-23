a6f9913c0c6efa0048c7e61f7639c15fa55cb0ef
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2019-09-23 17:00:14 +0100

    Create A and B show for landing page
    This commit creates two variants of the landing page, A and you guessed
    it... B. Variant B is fairly bare but will be updated with the new
    landing page designs in a later PR.
    
    The action knows which view to render via a http header passed in, if
    this isn't present the default rendered view will be A. When a variant
    is rendered a html meta tag is also set for the response, this tells GA which
    version of the page was used.
    
    link to chevron-extend-hover header has been removed due to it being
    redundant as the code for this resides in govuk-publishing-components.

--- test/controllers/brexit_landing_page_controller_test.rb
+++ test/controllers/brexit_landing_page_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 describe BrexitLandingPageController do
   include TaxonHelpers
+  include GovukAbTesting::MinitestHelpers
 
   describe "GET show" do
     before do
@@ -14,6 +15,34 @@ describe BrexitLandingPageController do
       get :show
 
       assert_response :success
+    end
+
+    context "Brexit landing page AB tests" do
+      it "renders A variant" do
+        with_variant BrexitLandingPageTest: "A" do
+          get :show
+
+          assert_response :success
+          assert_equal("GOVUK-ABTest-BrexitLandingPageTest", response.headers["Vary"])
+          assert_includes(
+            response.body,
+            "<meta name=\"govuk:ab-test\" content=\"BrexitLandingPageTest:A\" data-analytics-dimension=\"68\" data-allowed-variants=\"A,B\">",
+          )
+        end
+      end
+
+      it "renders B variant" do
+        with_variant BrexitLandingPageTest: "B" do
+          get :show
+
+          assert_response :success
+          assert_equal("GOVUK-ABTest-BrexitLandingPageTest", response.headers["Vary"])
+          assert_includes(
+            response.body,
+            "<meta name=\"govuk:ab-test\" content=\"BrexitLandingPageTest:B\" data-analytics-dimension=\"68\" data-allowed-variants=\"A,B\">",
+          )
+        end
+      end
     end
   end
 end
