407671b8d60179ab5da6bcbf64d2381dce30d3a5
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2019-10-08 16:45:20 +0100

    Assign images to each step of the HowTo schema
    We should be using unique images for each step in the howTo schema.  As we do
    not, we surmise that we're not eligible for our preferred rich result
    type in Google.  Kate's kindly provided me with some images to use.
    
    We start numbering at 1 to match the component.
    
    If the step has a logic "and" or "or" then we choose the appropriate image
    and do not increment the step counter (because we want to use the same numbering
    style as the component).
    
    Otherwise, we choose the appropriate image from the list.
    
    We don't have any more than 12 steps at the moment.  In the case of
    having more, then we just use the placeholder text.

--- app/models/schemas/how_to.rb
+++ app/models/schemas/how_to.rb
@@ -9,9 +9,10 @@ class Schemas::HowTo
 
   def initialize(step_by_step)
     @step_by_step = step_by_step
+    @step_image_index = 1
   end
 
-  def structured_data(image_urls)
+  def structured_data
     step_items = details["step_by_step_nav"]["steps"].map.with_index(1) do |step, step_index|
       contents = step["contents"].map.with_index(1) do |content, item_index|
         direction_index = item_index
@@ -29,7 +30,7 @@ class Schemas::HowTo
 
       {
         "@type": "HowToStep",
-        "image": image_urls,
+        "image": step_image_url(step),
         "name": step["title"],
         "url": step_url(step["title"].parameterize),
         "position": step_index,
@@ -41,7 +42,7 @@ class Schemas::HowTo
       "@context": "http://schema.org",
       "@type": "HowTo",
       "description": description,
-      "image": image_urls,
+      "image": image_urls["placeholder"],
       "name": title,
       "step": step_items,
     }
@@ -49,7 +50,7 @@ class Schemas::HowTo
 
 private
 
-  attr_reader :step_by_step
+  attr_reader :step_by_step, :step_image_index
 
   def how_to_direction(content, index)
     {
@@ -77,5 +78,33 @@ private
 
   def step_url(step_slug)
     Plek.new.website_root + base_path + "#" + step_slug
+  end
+
+  def step_image_url(step)
+    if step["logic"] == "and"
+      image_urls["and"]
+    elsif step["logic"] == "or"
+      image_urls["or"]
+    elsif step_image_index <= 12
+      image_urls[@step_image_index.to_s].tap do
+        @step_image_index += 1
+      end
+    end || image_urls["placeholder"]
+  end
+
+  def image_url(image_file)
+    ActionController::Base.helpers.asset_url(image_file, type: :image)
+  end
+
+  def image_urls
+    @image_urls ||= begin
+      (1..12).each_with_object({}) { |index, image_urls|
+        image_urls[index.to_s] = image_url("step-#{index}.png")
+      }.merge(
+        "or" => image_url("step-or.png"),
+        "and" => image_url("step-and.png"),
+        "placeholder" => image_url("govuk_publishing_components/govuk-schema-placeholder-1x1.png"),
+      )
+    end
   end
 end
