94adcf157c0861c3b29e53e11a15ede4353a8616
Alex Jurubita <alexandru.jurubita@digital.cabinet-office.gov.uk>
2019-10-17 11:01:48 +0100

    Update JavaScript imports

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,9 @@
+//= require govuk_publishing_components/all_components
 //= require govuk/vendor/polyfills/Function/prototype/bind
 //
-//= require support
-//= require browse-columns
-//= require organisation-list-filter
+//= require modules/browse-columns
 //= require modules/current-location
 //= require modules/feeds.js
+//= require modules/organisation-list-filter
+//= require modules/support
 //= require components/accordion
-//= require govuk_publishing_components/all_components
