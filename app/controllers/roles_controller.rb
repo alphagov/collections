174e6efdef0dacd0609ec39252d771195668f88d
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-12-17 10:10:44 +0000

    Set locale in roles controller
    This matches the code in the organisations controller.

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,9 +1,15 @@
 class RolesController < ApplicationController
+  before_action :set_locale
+
   def show
     @role = Role.find!(request.path)
     setup_content_item_and_navigation_helpers(@role)
-    render :show, locals: {
-      role: @role,
-    }
+    render :show, locals: { role: @role }
+  end
+
+private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
