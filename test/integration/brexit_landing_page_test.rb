a5b8d85023b55ead227636203b10178b955e43dc
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2020-01-16 13:00:19 +0000

    Change brexit_landing page tests to reflect mid-jan 2020 pre-lords changes

--- test/integration/brexit_landing_page_test.rb
+++ test/integration/brexit_landing_page_test.rb
@@ -11,7 +11,7 @@ class BrexitLandingPageTest < ActionDispatch::IntegrationTest
       then_i_can_see_the_buckets_section
       then_i_can_see_the_title_section
       then_i_can_see_the_header_section
-      then_i_can_see_the_get_ready_section
+      then_i_can_see_the_what_happens_next_section
       then_i_can_see_the_share_links_section
       and_i_can_see_the_explore_topics_section
       and_i_can_see_an_email_subscription_link
@@ -21,7 +21,6 @@ class BrexitLandingPageTest < ActionDispatch::IntegrationTest
       given_there_is_a_brexit_taxon
       when_i_visit_the_brexit_landing_page_with_dynamic_list
       then_all_finder_links_have_tracking_data
-      and_the_start_button_is_tracked
       and_the_email_link_is_tracked
       and_ecommerce_tracking_is_setup
     end
