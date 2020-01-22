ee06cb6e888af21c5307ba6d7afd333bbe48c44d
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2020-01-22 16:08:56 +0000

    Add basic Person page render test
    This helps ensure that person pages don't break when govuk_publishing_components
    are bumped (for example).
    
    I've had to tweak a few bits of the person model to defend against parts of
    the random person schema that is generated.  These may be perfectly valid,
    so I don't see it as a bad thing.
    
    The next commit will be adding the schema.org Person schema, so having a
    naive working test based on the available govuk_content_schemas seems sensible.

---
+++ test/integration/person_test.rb
@@ -0,0 +1,39 @@
+require "integration_test_helper"
+
+class PersonTest < ActionDispatch::IntegrationTest
+  before do
+    name = "Rufus Scrimgeour"
+    slug = "rufus-scrimgeour"
+    base_path = "/government/people/#{slug}"
+
+    stub_person_page_content_item(base_path, name)
+    stub_person_page_search_query(slug)
+
+    visit base_path
+  end
+
+  it "displays the person page" do
+    assert_equal page.title, "Rufus Scrimgeour - GOV.UK"
+  end
+
+  def stub_person_page_content_item(base_path, name)
+    content_item = GovukSchemas::RandomExample.for_schema(frontend_schema: "person").merge(
+      "title" => name,
+      "base_path" => base_path,
+    )
+
+    stub_content_store_has_item(base_path, content_item)
+  end
+
+  def stub_person_page_search_query(slug)
+    stub_request(:get, "https://search.test.gov.uk/search.json").
+      with(query: {
+        count: 10,
+        fields: %w(content_store_document_type link public_timestamp title),
+        filter_people: slug,
+        order: "-public_timestamp",
+        reject_content_purpose_supergroup: "other",
+      }).
+      to_return(body: { results: [] }.to_json)
+  end
+end
