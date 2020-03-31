7b7b280b686bea59263b0d60aa5ac2d74dba73fa
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2020-03-31 13:15:03 +0100

    Add getter method for live_stream  - basic date and time parsing

--- app/presenters/coronavirus_landing_page_presenter.rb
+++ app/presenters/coronavirus_landing_page_presenter.rb
@@ -1,5 +1,5 @@
 class CoronavirusLandingPagePresenter
-  COMPONENTS = %w(stay_at_home guidance announcements_label announcements nhs_banner sections topic_section country_section notifications).freeze
+  COMPONENTS = %w(live_stream stay_at_home guidance announcements_label announcements nhs_banner sections topic_section country_section notifications).freeze
 
   def initialize(content_item)
     COMPONENTS.each do |component|
@@ -7,5 +7,40 @@ class CoronavirusLandingPagePresenter
         content_item["details"][component]
       end
     end
+  end
+
+  def parsed_stream_date
+    if live_stream_date.present?
+      ints = live_stream_date.split("/").map(&:to_i)
+      begin
+        DateTime.new(ints[2], ints[1], ints[0]).strftime("%d %B %Y").sub(/^0/, "")
+      rescue ArgumentError
+        ""
+      end
+    else
+      ""
+    end
+  end
+
+  def parsed_stream_time
+    if live_stream_time.present?
+      begin
+        DateTime.parse(live_stream_time).strftime("%I:%M %P").sub(/^0/, "")
+      rescue ArgumentError
+        ""
+      end
+    else
+      ""
+    end
+  end
+
+private
+
+  def live_stream_date
+    live_stream["date"]
+  end
+
+  def live_stream_time
+    live_stream["time"]
   end
 end
--- test/presenters/coronavirus_landing_page_presenter_test.rb
+++ test/presenters/coronavirus_landing_page_presenter_test.rb
@@ -4,8 +4,26 @@ require_relative "../../test/support/coronavirus_helper"
 describe CoronavirusLandingPagePresenter do
   it "provides getter methods for all component keys" do
     presenter = described_class.new(coronavirus_landing_page_content_item)
-    %i[stay_at_home guidance announcements_label announcements nhs_banner sections topic_section notifications].each do |method|
+    %i[live_stream stay_at_home guidance announcements_label announcements nhs_banner sections country_section topic_section notifications].each do |method|
       assert_respond_to(presenter, method)
     end
+  end
+
+  it "can parse date and time" do
+    presenter = described_class.new(coronavirus_landing_page_content_item)
+    assert_equal(presenter.parsed_stream_date, "31 March 2020")
+    assert_equal(presenter.parsed_stream_time, "5:00 pm")
+  end
+
+  it "displays and empty string if date and time are empty or invalid" do
+    bad_values = { "details" => {
+      "live_stream" => {
+        "date" => "2020/32/01",
+        "time" => "30:00",
+},
+      } }
+    presenter = described_class.new(coronavirus_landing_page_content_item.merge(bad_values))
+    assert_equal(presenter.parsed_stream_date, "")
+    assert_equal(presenter.parsed_stream_time, "")
   end
 end
