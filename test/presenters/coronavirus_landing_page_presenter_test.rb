2429cb2ac4c49315d5af1ade502302ff6fe2f8e9
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2020-04-21 14:34:39 +0100

    Adds a see all announcements link

--- test/presenters/coronavirus_landing_page_presenter_test.rb
+++ test/presenters/coronavirus_landing_page_presenter_test.rb
@@ -4,7 +4,7 @@ require_relative "../../test/support/coronavirus_helper"
 describe CoronavirusLandingPagePresenter do
   it "provides getter methods for all component keys" do
     presenter = described_class.new(coronavirus_landing_page_content_item)
-    %i[live_stream stay_at_home guidance announcements_label announcements nhs_banner sections topic_section country_section notifications].each do |method|
+    %i[live_stream stay_at_home guidance announcements_label announcements see_all_announcements_link nhs_banner sections topic_section country_section notifications].each do |method|
       assert_respond_to(presenter, method)
     end
   end
