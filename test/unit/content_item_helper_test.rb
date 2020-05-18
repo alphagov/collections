8153b40a9053cf129177514ed9d93a87d5e10009
Simon <sihugh@users.noreply.github.com>
2020-05-18 09:10:20 +0100

    Revert "Revert #1733 - Featured links automatically showing description"

---
+++ test/unit/content_item_helper_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+describe ContentItemHelper do
+  setup do
+    url = content_store_endpoint + "/content/blah/blah"
+    stub_request(:get, url).to_return(
+      status: 200,
+      body: {
+        description: "This is a description for a content item",
+      }.to_json,
+    )
+  end
+
+  describe "#content_item_description" do
+    it "returns description for content item" do
+      assert_equal "This is a description for a content item", content_item_description("/blah/blah")
+    end
+  end
+end
