ad907ca960dca9f41bdd11fe09214ed034a46717
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-05-20 10:28:25 +0100

    Remove redundant initializer printout
    This was causing a linting error and is unnecessary. We don't need
    to print the location of the component guide, as this is common
    knowledge.

--- config/initializers/govuk_publishing_components.rb
+++ config/initializers/govuk_publishing_components.rb
@@ -3,12 +3,4 @@ if defined?(GovukPublishingComponents)
     c.component_guide_title = "Collections Component Guide"
     c.application_print_stylesheet = "print"
   end
-
-  if Rails.env.development?
-    startup_message = "=> collections component guide available at: /component-guide"
-    color_blue = 36
-
-    # https://stackoverflow.com/questions/1489183/colorized-ruby-output
-    puts "\e[#{color_blue}m#{startup_message}\e[0m"
-  end
 end
