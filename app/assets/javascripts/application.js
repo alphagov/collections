1eede5cbf1fddbb2fa1de17064ba6dc0896aef67
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2020-06-09 09:29:47 +0100

    Update JS to use individual components

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,13 @@
 //= require govuk-frontend/govuk/vendor/polyfills/Function/prototype/bind
-//
+
+//= require govuk_publishing_components/lib
+//= require govuk_publishing_components/components/accordion
+//= require govuk_publishing_components/components/button
+//= require govuk_publishing_components/components/details
+//= require govuk_publishing_components/components/feedback
+//= require govuk_publishing_components/components/govspeak
+//= require govuk_publishing_components/components/step-by-step-nav
+
 //= require support
 //= require browse-columns
 //= require organisation-list-filter
@@ -8,7 +16,6 @@
 //= require modules/toggle-attribute
 //= require components/accordion
 //= require modules/coronavirus-landing-page
-//= require govuk_publishing_components/all_components
 
 $(document).on('ready', function(){
   var toggleAttribute = new GOVUK.Modules.ToggleAttribute();
