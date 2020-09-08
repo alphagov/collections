b703ca780cddbcd992b8bf08f138d3a560cc0c12
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2020-09-08 16:10:30 +0100

    Turn off Transition Checker AB Test 4
    - Turn of the AB test
    - Make the start now button on the transition landing page
    green, instead of blue, as that variant won.

--- test/controllers/transition_landing_page_controller_test.rb
+++ test/controllers/transition_landing_page_controller_test.rb
@@ -21,35 +21,5 @@ describe TransitionLandingPageController do
         assert_response :success
       end
     end
-
-    %w[A INVALID_VARIANT].each do |variant|
-      it "displays the default blue button for the #{variant} variant in the en locale" do
-        with_variant TransitionChecker4: "A" do
-          get :show
-          assert_select "div.landing-page__take-action-button"
-          assert_select "div.landing-page__take-action-button_variantB", count: 0
-        end
-      end
-    end
-
-    it "displays a red button for the B variant in the en locale" do
-      with_variant TransitionChecker4: "B" do
-        get :show
-        assert_select "div.landing-page__take-action-button_variantB"
-        assert_select "div.landing-page__take-action-button", count: 0
-      end
-    end
-
-    %w[A B INVALID_VARIANT].each do |variant|
-      it "displays the default blue button for the #{variant} variant in the cy locale" do
-        setup_ab_variant("TransitionChecker4", variant)
-
-        get :show, params: { locale: "cy" }
-
-        assert_response_not_modified_for_ab_test("TransitionChecker3")
-        assert_select "div.landing-page__take-action-button"
-        assert_select "div.landing-page__take-action-button_variantB", count: 0
-      end
-    end
   end
 end
