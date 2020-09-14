8bc721f1d441dcd07c6b7a997cf66799bc2e643a
owenatgov <owen.jones@digital.cabinet-office.gov.uk>
2020-09-14 10:48:02 +0100

    Update tests

--- features/step_definitions/curated_lists_steps.rb
+++ features/step_definitions/curated_lists_steps.rb
@@ -3,6 +3,6 @@ Given(/^there is curated content for a subtopic$/) do
 end
 
 Then(/^I see a curated list of content$/) do
-  assert page.has_selector?("h1", text: "Oil rigs")
-  assert page.has_selector?("h1", text: "Piping")
+  assert page.has_selector?("h2", text: "Oil rigs")
+  assert page.has_selector?("h2", text: "Piping")
 end
