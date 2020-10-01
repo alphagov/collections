430daa6ba06508e40fb2d7a8dea36b22dc8ca970
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2020-10-01 16:34:50 +0100

    Show a new Take Action title for users in the B variant:
    "Make sure you're ready"

--- test/controllers/transition_landing_page_controller_test.rb
+++ test/controllers/transition_landing_page_controller_test.rb
@@ -29,6 +29,22 @@ describe TransitionLandingPageController do
           assert_select "h1", text: "The transition period ends in December", count: 0
         end
       end
+
+      it "displays the default take action title for the #{variant} variant in the en locale" do
+        with_variant TransitionUrgency1: variant do
+          get :show
+          assert_select "h2", text: "Take action"
+          assert_select "h2", text: "Make sure you're ready", count: 0
+        end
+      end
+    end
+
+    it "displays the value of take_action_title_variant_B for the B variant in the en locale" do
+      with_variant TransitionUrgency1: "B" do
+        get :show
+        assert_select "h2", text: "Make sure you're ready"
+        assert_select "h2", text: "Take action", count: 0
+      end
     end
 
     it "displays the value of page_header_variant_B for the B variant in the en locale" do
@@ -40,7 +56,7 @@ describe TransitionLandingPageController do
     end
 
     %w[A B Z].each do |variant|
-      it "displays the default page header for the #{variant} variant in the cy locale" do
+      it "displays the control text for the #{variant} variant in the cy locale" do
         setup_ab_variant("TransitionUrgency1", variant)
 
         get :show, params: { locale: "cy" }
@@ -48,6 +64,9 @@ describe TransitionLandingPageController do
         assert_response_not_modified_for_ab_test("TransitionUrgency1")
         assert_select "h1", text: "Pontio’r DU"
         assert_select "h1", text: "The transition period ends in December", count: 0
+
+        assert_select "h2", text: "Cymrwch y camau a chofrestru ar gyfer negeseuon e-bost"
+        assert_select "h2", text: "Make sure you're ready", count: 0
       end
     end
   end
