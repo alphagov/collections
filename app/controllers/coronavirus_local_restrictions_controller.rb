67f7671ca7d0f6493ea0588e479828f644597cec
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2020-10-06 17:38:15 +0100

    Hardcode breadcrumbs
    - To fix NoMethodError error from _breadcrumbs.html.erb which expected a content_item.
    - Hardcoding for now, we can change this later if needed.

--- app/controllers/coronavirus_local_restrictions_controller.rb
+++ app/controllers/coronavirus_local_restrictions_controller.rb
@@ -1,4 +1,25 @@
 class CoronavirusLocalRestrictionsController < ApplicationController
-    def show; end
+  def show
+    render :show,
+           locals: {
+             breadcrumbs: breadcrumbs,
+           }
+  end
+
+private
+
+  # Breadcrumbs for this page are hardcoded because it doesn't yet have a
+  # content item with parents.
+  def breadcrumbs
+    [
+      {
+        title: "Home",
+        url: "/",
+      },
+      {
+        title: "Coronavirus (COVID-19)",
+        url: "/coronavirus",
+      },
+    ]
+  end
 end
-  
