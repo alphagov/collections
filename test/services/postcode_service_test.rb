6f7a856e394f06ae175770c9befa1f5b4c86e5a4
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-10-15 11:55:07 +0100

    Add a postcode sanitise method
    "Borrowed" from: https://github.com/alphagov/frontend/blob/master/lib/postcode_sanitizer.rb
    
    We've been seeing a lot of errors in sentry because sentry related to special characters that aren't
    being caught by the validation method. This follows the pattern set in frontend and uses the "uk_postcode"
    gem to remove all special characters.
    
    See:
    https://sentry.io/organizations/govuk/issues/1954296277/events/2614514fc38e42da958b9c48d6e8f42c/?project=202213

--- test/services/postcode_service_test.rb
+++ test/services/postcode_service_test.rb
@@ -17,4 +17,30 @@ describe PostcodeService do
       assert_not(described_class.new(postcode).valid?)
     end
   end
+
+  describe "#sanitize" do
+    it "strips trailing spaces from entered postcodes" do
+      assert_equal "WC2B 6NH", described_class.new("WC2B 6NH ").sanitize
+    end
+
+    it "transposes O/0 and I/1 if necessary" do
+      assert_equal "W1A 0AA", described_class.new("WIA OAA").sanitize
+    end
+
+    it "returns nil if the postcode parameter is nil or an empty string" do
+      assert_nil described_class.new("").sanitize
+    end
+
+    it "removes special characters from the start of the postcode" do
+      assert_equal "WC2B 6NH", described_class.new(".WC2B6NH").sanitize
+    end
+
+    it "converts the postcode to uppercase" do
+      assert_equal "WC2B 6NH", described_class.new("wc2b6nh").sanitize
+    end
+
+    it "removes non-breaking spaces from the postcode" do
+      assert_equal "WC1A 1AA", described_class.new("\u00A0WC1A1AA").sanitize
+    end
+  end
 end
