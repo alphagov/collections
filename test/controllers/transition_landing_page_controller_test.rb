94b9c60854df561c1b98a1e1caa05640f82fd85d
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2020-10-20 12:32:57 +0100

    Turn off transition urgency AB test 4
    - Checker engagement dropped for variant B, so we
    are reinstating the control which is displaying both
    the video and the announcements sections.

--- test/controllers/transition_landing_page_controller_test.rb
+++ test/controllers/transition_landing_page_controller_test.rb
@@ -20,38 +20,5 @@ describe TransitionLandingPageController do
         assert_response :success
       end
     end
-
-    describe "TransitionUrgency4 AB test" do
-      context "In the en locale" do
-        %w[A Z].each do |variant|
-          it "Variant #{variant} shows the default (video and announcements visible)" do
-            with_variant TransitionUrgency4: variant do
-              get :show
-              assert_template partial: "_video_section", count: 1
-              assert_template partial: "_comms", count: 1
-            end
-          end
-        end
-
-        it "Variant B hides the video and announcements" do
-          with_variant TransitionUrgency4: "B" do
-            get :show
-            assert_template partial: "_video_section", count: 0
-            assert_template partial: "_comms", count: 0
-          end
-        end
-      end
-
-      context "In the cy locale" do
-        %w[A B Z].each do |variant|
-          it "All variants shows the default (video and announcements visible)" do
-            setup_ab_variant("TransitionUrgency4", variant)
-            get :show, params: { locale: "cy" }
-            assert_template partial: "_video_section", count: 1
-            assert_template partial: "_comms", count: 1
-          end
-        end
-      end
-    end
   end
 end
