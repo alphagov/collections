12a9bce98f7e1de2f069edbd12de22f22310d4b7
maxgds <max.harden@digital.cabinet-office.gov.uk>
2020-10-21 13:53:17 +0100

    Move promoted news item on taxons page into document list

--- app/presenters/supergroups/news_and_communications.rb
+++ app/presenters/supergroups/news_and_communications.rb
@@ -16,6 +16,12 @@ module Supergroups
       format_document_data(items)
     end
 
+    def document_list_with_promoted(taxon_id)
+      items = tagged_content(taxon_id)
+
+      format_document_data(items)
+    end
+
     def promoted_content(taxon_id)
       items = tagged_content(taxon_id).shift(promoted_content_count)
       format_document_data(items, "FeaturedLinkClicked", true)
