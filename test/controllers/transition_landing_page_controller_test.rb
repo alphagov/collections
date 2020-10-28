85e99b82e4db8653661f02307dd2a70cc655ed9a
Michael Walker <mike@barrucadu.co.uk>
2020-10-28 17:10:26 +0000

    Add tests for accounts A/B test

--- test/controllers/transition_landing_page_controller_test.rb
+++ test/controllers/transition_landing_page_controller_test.rb
@@ -20,5 +20,34 @@ describe TransitionLandingPageController do
         assert_response :success
       end
     end
+
+    describe "accounts are enabled" do
+      before do
+        Rails.configuration.stubs(:feature_flag_govuk_accounts).returns(true)
+      end
+
+      %w[LoggedIn LoggedOut].each do |variant|
+        it "Variant #{variant} disables the search field" do
+          with_variant AccountExperiment: variant do
+            get :show
+            assert_equal "true", response.headers["X-Slimmer-Remove-Search"]
+          end
+        end
+      end
+
+      it "Variant LoggedIn requests the signed-in header" do
+        with_variant AccountExperiment: "LoggedIn" do
+          get :show
+          assert_equal "signed-in", response.headers["X-Slimmer-Show-Accounts"]
+        end
+      end
+
+      it "Variant LoggedOut requests the signed-out header" do
+        with_variant AccountExperiment: "LoggedOut" do
+          get :show
+          assert_equal "signed-out", response.headers["X-Slimmer-Show-Accounts"]
+        end
+      end
+    end
   end
 end
