a804843644f31a3e4f13ffadd6db742e21bb5ed5
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2020-10-29 11:46:51 +0000

    Stub everything for the short content test
    This fixes the test error that we were getting that we were making unexpected
    calls to web services. The test still fails because we want to return only
    1 result for news and comms.

--- test/integration/taxon_browsing_test.rb
+++ test/integration/taxon_browsing_test.rb
@@ -145,8 +145,19 @@ private
   end
 
   def and_the_taxon_has_short_tagged_content
-    stub_content_for_taxon(content_id, tagged_content)
+    stub_document_types_for_supergroup("guidance_and_regulation")
+    stub_most_popular_content_for_taxon(content_id, tagged_content_for_guidance_and_regulation, filter_content_store_document_type: "guidance_and_regulation")
+    stub_document_types_for_supergroup("services")
+    stub_most_popular_content_for_taxon(content_id, tagged_content_for_services, filter_content_store_document_type: "services")
     stub_document_types_for_supergroup("news_and_communications")
+    stub_most_recent_content_for_taxon(content_id, tagged_content_for_news_and_communications, filter_content_store_document_type: "news_and_communications")
+    stub_document_types_for_supergroup("policy_and_engagement")
+    stub_most_recent_content_for_taxon(content_id, tagged_content_for_policy_and_engagement, filter_content_store_document_type: "policy_and_engagement")
+    stub_document_types_for_supergroup("transparency")
+    stub_most_recent_content_for_taxon(content_id, tagged_content_for_transparency, filter_content_store_document_type: "transparency")
+    stub_document_types_for_supergroup("research_and_statistics")
+    stub_most_recent_content_for_taxon(content_id, tagged_content_for_research_and_statistics, filter_content_store_document_type: "research_and_statistics")
+    stub_organisations_for_taxon(content_id, tagged_organisations)
   end
 
   def when_i_visit_that_taxon
