f4a5a3eeaaf653c0a27f746d1a0ebaaf767a4da7
Kevin Dew <kevindew@me.com>
2020-12-08 23:05:03 +0000

    Handle postcode restrictions via GET requests
    This changes the HTTP process for looking up local restrictions by
    postcode. Previously there was a GET request to show the form and a POST
    request to see results, both with different actions. This changes the
    handling to be done with GET requests for both form and results and this
    done with a single action.
    
    There is a risk that this change could inconvenience any users who are
    in the process of checking their postcode at the time of this change. To
    handle these users there is a redirect set-up for any post requests.
    This can be removed an hour or so after this has been deployed.
    
    This also change simplifies the controller by switching to the
    PostcodeLocalRestrictionSearch model, this allows the conditionals in
    the controller to only be the different views. It also moves a bunch of
    the phrase handling into the view from the controller.

--- test/controllers/coronavirus_local_restrictions_controller_test.rb
+++ test/controllers/coronavirus_local_restrictions_controller_test.rb
@@ -10,58 +10,52 @@ describe CoronavirusLocalRestrictionsController do
   end
 
   describe "GET show" do
-    it "correctly renders the local restrictions page" do
+    it "renders the show template when not given a postcode" do
       get :show
 
       assert_response :success
       assert_template :show
     end
-  end
 
-  describe "POST results" do
-    it "renders the results page when given a real postcode" do
+    it "renders the show template when given an invalid postcode" do
+      get :show, params: { postcode: "not-a-postcode" }
+
+      assert_response :success
+      assert_template :show
+    end
+
+    it "renders the no_information template when given a postcode without information" do
+      stub_mapit_has_a_postcode_and_areas("E1 8QS", [], [])
+      get :show, params: { postcode: "E1 8QS" }
+
+      assert_response :success
+      assert_template :no_information
+    end
+
+    it "renders the results template for an area with information" do
       restriction = LocalRestriction.new("E01000123",
                                          { "name" => "Coruscant Planetary Council" })
       LocalRestriction.stubs(:find).returns(restriction)
 
       postcode = "E1 8QS"
-      areas = [
-        {
-          "gss" => restriction.gss_code,
-          "name" => restriction.area_name,
-          "type" => "LBO",
-        },
-      ]
-      stub_mapit_has_a_postcode_and_areas(postcode, [], areas)
-
-      post :results, params: { "postcode-lookup" => postcode }
+      stub_mapit_has_a_postcode_and_areas(postcode, [], [{
+        "gss" => restriction.gss_code,
+        "name" => restriction.area_name,
+        "type" => "LBO",
+        "country_name" => "England",
+      }])
+      get :show, params: { postcode: postcode }
 
       assert_response :success
       assert_template :results
     end
+  end
 
-    it "renders the local restriction page when given an incorrect postcode" do
-      postcode = "invalid postcode"
-      stub_mapit_does_not_have_a_postcode(postcode)
-      post :results, params: { "postcode-lookup" => postcode }
+  describe "POST legacy" do
+    it "redirects to the GET endpoint" do
+      post :legacy, params: { "postcode-lookup" => "E1 8QS" }
 
-      assert_response :success
-      assert_template :show
-    end
-
-    it "renders the local restriction page when the postcode is blank" do
-      postcode = ""
-      post :results, params: { "postcode-lookup" => postcode }
-
-      assert_response :success
-      assert_template :show
-    end
-
-    it "renders the local restriction page when the postcode param is not passed in" do
-      post :results
-
-      assert_response :success
-      assert_template :show
+      assert_redirected_to find_coronavirus_local_restrictions_path(postcode: "E1 8QS")
     end
   end
 end
