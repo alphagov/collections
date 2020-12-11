8d89a86d1591c212e104329b741821f43d2f3e90
Alex <alex-ju@users.noreply.github.com>
2020-12-11 15:06:19 +0000

    Further adjustments to the translation copy

--- test/controllers/dit_landing_page_controller_test.rb
+++ test/controllers/dit_landing_page_controller_test.rb
@@ -7,7 +7,7 @@ describe DitLandingPageController do
       en: "Trade with the UK from 1 January 2021 as a business based in the EU",
       es: "Cómo hacer negocios con el Reino Unido a partir del 1 de enero de 2021 en caso de ser una empresa con sede en la UE",
       fr: "Travailler avec le Royaume-Uni à partir du 1er janvier 2021 en tant qu'entreprise basée dans l'UE",
-      it: "Fai affari con il Regno Unito dal 1° gennaio 2021 in qualità di azienda con sede nell’UE",
+      it: "Se la tua azienda ha sede nell’UE, scopri le nuove regole per continuare a fare affari con il Regno Unito dal 1° gennaio 2021",
       nl: "Handel drijven met het Verenigd Koninkrijk vanuit een in Europa gevestigde onderneming vanaf 1 januari 2021",
       pl: "Handel z Wielką Brytanią od 1 stycznia 2021 roku – informacje dla firm z Unii Europejskiej",
     }
