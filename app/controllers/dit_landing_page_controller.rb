dd4f806243d70f55543ab803319a0a6309692797
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2020-12-14 12:31:01 +0000

    DIT landing page controller refactor
    - The /eubusiness content item contains available translations.
    So we no longer need to hard code the locales in the controller.

--- app/controllers/dit_landing_page_controller.rb
+++ app/controllers/dit_landing_page_controller.rb
@@ -1,31 +1,17 @@
 class DitLandingPageController < ApplicationController
   around_action :switch_locale
 
-  def show; end
-
-  helper_method :translation_links
-
-  def translation_links
-    available_translations.map do |translation|
-      {
-        locale: translation,
-        base_path: path(translation),
-        text: language_name(translation),
-        active: locale == translation,
-      }
-    end
+  def show
+    @presenter = presenter
   end
 
-  def path(language)
-    raw_path = request.path.split(".")[0]
-    language == :en ? raw_path : "#{raw_path}.#{language}"
+private
+
+  def content_item
+    ContentItem.find!(request.path)
   end
 
-  def language_name(language)
-    I18n.t("shared.language_names.#{language}")
-  end
-
-  def available_translations
-    %i[en de es fr it nl pl]
+  def presenter
+    DitLandingPagePresenter.new(content_item)
   end
 end
