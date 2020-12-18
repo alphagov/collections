7557eb83768bafb57bc7b2e3f8c767e56d80a9b7
Kevin Dew <kevindew@me.com>
2020-12-18 11:19:33 +0000

    Add method to find locations with MapitLocation
    This adds a static method that can query mapit to find locations for a
    postcode. This has been done to simplify the MapitLocation abstraction
    so it doesn't need to be split between this class and
    LocationLookupService.
    
    I've created exception classes for the two Mapit error situations that
    this application caters for: No location for the postcode; Postcode
    given was invalid. Other responses will raise an error. This somewhat
    works around GdsApiAdapters lacking a particular exception for a
    HTTPBadRequest which seems to be a strange omission (I'll try add this
    so I can refactor this code). As we don't do anything with other Mapit
    errors (which doesn't seem quite ideal) I didn't think to add a more
    generic MapitError exception.
    
    I couldn't see a particular reason why we converted the Mapit Response
    into a hash so I converted this to just work with the object that the
    GDS API Adapters responds with. To finish this off I simplified some of
    the method names and removed some of the tests for code that just
    delegates.

--- app/models/mapit_location.rb
+++ app/models/mapit_location.rb
@@ -1,24 +1,36 @@
 class MapitLocation
-  attr_reader :area
+  class LocationNotFound < RuntimeError; end
+  class LocationInvalid < RuntimeError; end
+
+  def self.locations_for_postcode(postcode)
+    mapit = if Rails.env.development?
+              GdsApi::Mapit.new("https://mapit.mysociety.org/")
+            else
+              GdsApi.mapit
+            end
+
+    areas = Rails.cache.fetch("mapit-location-#{postcode}", expires_in: 3.hours) do
+      mapit.location_for_postcode(postcode).areas
+    end
+    areas.filter_map { |area| new(area) if area.codes["gss"] }
+  rescue GdsApi::HTTPNotFound
+    raise LocationNotFound
+  rescue GdsApi::HTTPClientError => e
+    e.code == 400 ? raise(LocationInvalid) : raise
+  end
+
+  delegate :name, :type, to: :area
 
   def initialize(area)
     @area = area
   end
 
   def gss
-    area["codes"]["gss"]
-  end
-
-  def area_name
-    area["name"]
+    area.codes["gss"]
   end
 
   def country
-    area["country_name"]
-  end
-
-  def area_type
-    area["type"]
+    area.country_name
   end
 
   def england?
@@ -36,4 +48,8 @@ class MapitLocation
   def northern_ireland?
     country == "Northern Ireland"
   end
+
+private
+
+  attr_reader :area
 end
--- test/models/mapit_location_test.rb
+++ test/models/mapit_location_test.rb
@@ -1,62 +1,106 @@
 require "test_helper"
+require "gds_api/test_helpers/mapit"
 
 describe MapitLocation do
-  let(:mapit_area) do
-    {
-      "codes" => {
-        "ons" => "01",
-        "gss" => "E01000123",
-        "govuk_slug" => "test-one",
-      },
-      "name" => "Coruscant Planetary Council",
-      "type" => "LBO",
-      "country_name" => "England",
-    }
+  include GdsApi::TestHelpers::Mapit
+
+  describe ".locations_for_postcode" do
+    it "returns an array of MapitLocation object for areas with gss codes" do
+      postcode = "E1 8QS"
+      areas = [{ "gss" => "E01000123",
+                 "name" => "Coruscant Planetary Council",
+                 "type" => "LBO",
+                 "country_name" => "England" },
+               { "gss" => "E02000456",
+                 "name" => "Galactic Empire",
+                 "type" => "GLA",
+                 "country_name" => "England" },
+               { "name" => "Imperial Center",
+                 "type" => "OMF",
+                 "country_name" => "England" }]
+      stub_mapit_has_a_postcode_and_areas(postcode, [], areas)
+
+      locations = described_class.locations_for_postcode("E1 8QS")
+      assert 2, locations.length
+      assert ["Coruscant Planetary Council", "Galactic Empire"], locations.map(&:name)
+    end
+
+    it "returns an empty array when no areas have gss codes" do
+      postcode = "E1 8QS"
+      areas = [{ "name" => "Imperial Center",
+                 "type" => "OMF",
+                 "country_name" => "England" }]
+      stub_mapit_has_a_postcode_and_areas(postcode, [], areas)
+
+      assert_equal [], described_class.locations_for_postcode("E1 8QS")
+    end
+
+    it "raises a LocationNotFound error when Mapit doesn't have a postcode" do
+      stub_mapit_does_not_have_a_postcode("E1 8QS")
+
+      assert_raises described_class::LocationNotFound do
+        described_class.locations_for_postcode("E1 8QS")
+      end
+    end
+
+    it "raises a LocationInvalid error when Mapit returns a bad request response" do
+      stub_mapit_does_not_have_a_bad_postcode("E1 8QS")
+
+      assert_raises described_class::LocationInvalid do
+        described_class.locations_for_postcode("E1 8QS")
+      end
+    end
+
+    it "raises the GdsApi error when Mapit returns a different error response" do
+      stub_request(:get, /mapit/).to_return(status: 500)
+
+      assert_raises GdsApi::HTTPInternalServerError do
+        described_class.locations_for_postcode("E1 8QS")
+      end
+    end
   end
 
-  it "returns the gss code" do
-    assert_equal("E01000123", described_class.new(mapit_area).gss)
+  def mapit_area(**attributes)
+    OpenStruct.new(
+      codes: { "gss" => attributes.fetch(:gss, "E01000123") },
+      name: attributes.fetch(:name, "Coruscant Planetary Council"),
+      type: attributes.fetch(:type, "LBO"),
+      country_name: attributes.fetch(:country_name, "England"),
+    )
   end
 
-  it "returns the area name" do
-    assert_equal("Coruscant Planetary Council", described_class.new(mapit_area).area_name)
-  end
-
-  it "returns the country" do
-    assert_equal("England", described_class.new(mapit_area).country)
-  end
-
-  it "returns the area type" do
-    assert_equal("LBO", described_class.new(mapit_area).area_type)
+  describe "#gss" do
+    it "returns the area's gss code" do
+      instance = described_class.new(mapit_area(gss: "E01000456"))
+      assert_equal "E01000456", instance.gss
+    end
   end
 
   describe "#england?" do
     it "returns true if the country is England" do
-      assert(described_class.new(mapit_area).england?)
+      instance = described_class.new(mapit_area(country_name: "England"))
+      assert instance.england?
     end
   end
 
   describe "#scotland?" do
     it "returns true if the country is Scotland" do
-      mapit_area["country_name"] = "Scotland"
-
-      assert(described_class.new(mapit_area).scotland?)
+      instance = described_class.new(mapit_area(country_name: "Scotland"))
+      assert instance.scotland?
     end
   end
 
   describe "#wales?" do
     it "returns true if the country is Wales" do
-      mapit_area["country_name"] = "Wales"
-
-      assert(described_class.new(mapit_area).wales?)
+      instance = described_class.new(mapit_area(country_name: "Wales"))
+      assert instance.wales?
     end
   end
 
   describe "#northern_ireland?" do
     it "returns true if the country is Northern Ireland" do
-      mapit_area["country_name"] = "Northern Ireland"
-
-      assert(described_class.new(mapit_area).northern_ireland?)
+      instance = described_class.new(mapit_area(country_name: "Northern Ireland"))
+      assert instance.northern_ireland?
     end
   end
 end
