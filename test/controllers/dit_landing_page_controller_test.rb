14613abcb2ef3889a2c1550339adedeb53619390
Alex <alex-ju@users.noreply.github.com>
2021-01-04 14:41:50 +0000

    Update dit landing page content – Español

--- test/controllers/dit_landing_page_controller_test.rb
+++ test/controllers/dit_landing_page_controller_test.rb
@@ -10,7 +10,7 @@ describe DitLandingPageController do
   describe "GET show" do
     expected_content = {
       de: "Informationen für Unternehmen mit Sitz in der EU, die Handelsbeziehungen mit dem Vereinigten Königreich unterhalten",
-      es: "Cómo hacer negocios con el Reino Unido a partir del 1 de enero de 2021 en caso de ser una empresa con sede en la UE",
+      es: "Comerciar con el Reino Unido como empresa con sede en la UE",
       fr: "Travailler avec le Royaume-Uni à partir du 1er janvier 2021 en tant qu'entreprise basée dans l'UE",
       it: "Se la tua azienda ha sede nell’UE, scopri le nuove regole per continuare a fare affari con il Regno Unito dal 1° gennaio 2021",
       nl: "Handel drijven met het Verenigd Koninkrijk vanuit een in de EU gevestigde onderneming vanaf 1 januari 2021",
