1c46cb08eb563b9460451a0326342cda0d1410cc
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2021-02-10 14:51:38 +0000

    Remove redundant method from browse_test_helper

--- features/support/browse_test_helpers.rb
+++ features/support/browse_test_helpers.rb
@@ -2,10 +2,6 @@ require "gds_api/test_helpers/content_store"
 
 module BrowseTestHelpers
   include GdsApi::TestHelpers::ContentStore
-
-  def assert_can_see_linked_item(name)
-    assert page.has_selector?("a", text: name)
-  end
 end
 
 World(BrowseTestHelpers)
