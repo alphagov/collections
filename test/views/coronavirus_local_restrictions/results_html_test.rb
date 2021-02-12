bb4ea941556d585067f13935451af9b9ec1a85ad
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2021-02-12 13:29:04 +0000

    Refactor methods to render view
    - Refactor rendering views into one method
    - render_results_view was suggested/written by @kevindew in PR review... thanks!

--- test/views/coronavirus_local_restrictions/results_html_test.rb
+++ test/views/coronavirus_local_restrictions/results_html_test.rb
@@ -9,7 +9,7 @@ module CoronavirusLocalRestrictions
 
     describe "current restrictions" do
       test "rendering tier 4 results for a postcode in tier 4" do
-        render_tier_results(current_alert_level: 4)
+        render_results_view(local_restriction: { current_alert_level: 4 })
 
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.results.level_four.heading_pretext")
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.results.level_four.heading_tier_label")
@@ -17,7 +17,7 @@ module CoronavirusLocalRestrictions
       end
 
       test "rendering tier 3 results for a postcode in tier 3" do
-        render_tier_results(current_alert_level: 3)
+        render_results_view(local_restriction: { current_alert_level: 3 })
 
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.results.level_three.heading_pretext")
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.results.level_three.heading_tier_label")
@@ -25,7 +25,7 @@ module CoronavirusLocalRestrictions
       end
 
       test "rendering tier 2 results for a postcode in tier 2" do
-        render_tier_results(current_alert_level: 2)
+        render_results_view(local_restriction: { current_alert_level: 2 })
 
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.results.level_two.heading_pretext")
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.results.level_two.heading_tier_label")
@@ -33,7 +33,7 @@ module CoronavirusLocalRestrictions
       end
 
       test "rendering tier 1 results for a postcode in tier 1" do
-        render_tier_results(current_alert_level: 1)
+        render_results_view(local_restriction: { current_alert_level: 1 })
 
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.results.level_one.heading_pretext")
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.results.level_one.heading_tier_label")
@@ -60,25 +60,19 @@ module CoronavirusLocalRestrictions
 
     describe "devolved nations" do
       test "rendering results for a Welsh postcode" do
-        country_name = "Wales"
-
-        render_devolved_nation_guidance(country_name)
+        render_results_view(local_restriction: { country_name: "Wales" })
 
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.results.devolved_nations.wales.guidance.label")
       end
 
       test "rendering results for a Scottish postcode" do
-        country_name = "Scotland"
-
-        render_devolved_nation_guidance(country_name)
+        render_results_view(local_restriction: { country_name: "Scotland" })
 
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.results.devolved_nations.scotland.guidance.label")
       end
 
       test "rendering results for a Northern Irish postcode" do
-        country_name = "Northern Ireland"
-
-        render_devolved_nation_guidance(country_name)
+        render_results_view(local_restriction: { country_name: "Northern Ireland" })
 
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.results.devolved_nations.northern_ireland.guidance.label")
       end
@@ -86,19 +80,22 @@ module CoronavirusLocalRestrictions
 
     describe "future restrictions" do
       test "rendering restrictions changing from level one to level two" do
-        render_tier_results(current_alert_level: 1, future_alert_level: 2)
+        render_results_view(local_restriction: { current_alert_level: 1, future_alert_level: 2 })
+
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.results.level_one.changing_alert_level", area: area)
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.results.future.level_two.alert_level", area: area)
       end
 
       test "rendering restrictions changing from level two to level three" do
-        render_tier_results(current_alert_level: 2, future_alert_level: 3)
+        render_results_view(local_restriction: { current_alert_level: 2, future_alert_level: 3 })
+
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.results.level_two.changing_alert_level", area: area)
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.results.future.level_three.alert_level", area: area)
       end
 
       test "rendering restrictions changing from level three to level four" do
-        render_tier_results(current_alert_level: 3, future_alert_level: 4)
+        render_results_view(local_restriction: { current_alert_level: 3, future_alert_level: 4 })
+
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.results.level_three.changing_alert_level", area: area)
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.results.future.level_four.alert_level", area: area)
       end
@@ -106,63 +103,41 @@ module CoronavirusLocalRestrictions
 
     describe "out of date restrictions" do
       test "rendering an out of date warning on the tier one page" do
-        render_tier_results_out_of_date_warning(current_alert_level: 1)
+        render_results_view(local_restriction: { current_alert_level: 1 }, out_of_date: true)
 
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.out_of_date_warning")
       end
 
       test "rendering an out of date warning on the tier two page" do
-        render_tier_results_out_of_date_warning(current_alert_level: 2)
+        render_results_view(local_restriction: { current_alert_level: 2 }, out_of_date: true)
 
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.out_of_date_warning")
       end
 
       test "rendering an out of date warning on the tier three page" do
-        render_tier_results_out_of_date_warning(current_alert_level: 3)
+        render_results_view(local_restriction: { current_alert_level: 3 }, out_of_date: true)
 
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.out_of_date_warning")
       end
 
       test "rendering an out of date warning on the tier four page" do
-        render_tier_results_out_of_date_warning(current_alert_level: 4)
+        render_results_view(local_restriction: { current_alert_level: 4 }, out_of_date: true)
 
         assert_includes rendered, I18n.t("coronavirus_local_restrictions.out_of_date_warning")
       end
     end
 
-    def render_tier_results(current_alert_level: nil, future_alert_level: nil)
-      postcode = "E1 8QS"
-      @search = PostcodeLocalRestrictionSearch.new(postcode)
-      stub_local_restriction(postcode: postcode, name: area, current_alert_level: current_alert_level, future_alert_level: future_alert_level)
-
-      view.stubs(:out_of_date?).returns(false)
-
-      render template: "coronavirus_local_restrictions/results"
-    end
-
-    def render_devolved_nation_guidance(country_name)
-      postcode = "E1 8QS"
-      stub_local_restriction(postcode: postcode, country_name: country_name)
-
-      @search = PostcodeLocalRestrictionSearch.new(postcode)
-
-      view.stubs(:out_of_date?).returns(false)
-
-      render template: "coronavirus_local_restrictions/results"
-    end
-
-    def render_tier_results_out_of_date_warning(current_alert_level: nil, future_alert_level: nil)
-      postcode = "E1 8QS"
-      @search = PostcodeLocalRestrictionSearch.new(postcode)
-      stub_local_restriction(postcode: postcode, name: area, current_alert_level: current_alert_level, future_alert_level: future_alert_level)
-
-      view.stubs(:out_of_date?).returns(true)
+    def render_results_view(local_restriction: {}, out_of_date: false)
+      local_restriction = { postcode: "E1 8QS" }.merge(local_restriction)
+      @search = PostcodeLocalRestrictionSearch.new(local_restriction[:postcode])
+      stub_local_restriction(**local_restriction)
+      view.stubs(:out_of_date?).returns(out_of_date)
 
       render template: "coronavirus_local_restrictions/results"
     end
 
     def area
-      "Tattooine"
+      "Tatooine"
     end
   end
 end
