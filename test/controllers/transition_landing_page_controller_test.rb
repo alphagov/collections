8771aba93907d6d5cf04d90faf36454f02ef702e
Michael Walker <mike@barrucadu.co.uk>
2021-02-26 13:08:56 +0000

    Remove account A/B test
    We've now fully switched over to the custom header approach.

--- test/controllers/transition_landing_page_controller_test.rb
+++ test/controllers/transition_landing_page_controller_test.rb
@@ -2,7 +2,6 @@ require "test_helper"
 
 describe TransitionLandingPageController do
   include TaxonHelpers
-  include GovukAbTesting::MinitestHelpers
 
   describe "GET show" do
     before do
@@ -46,34 +45,6 @@ describe TransitionLandingPageController do
           request.headers["GOVUK-Account-Session"] = "foo"
           get :show
           assert_equal "signed-in", response.headers["X-Slimmer-Show-Accounts"]
-        end
-      end
-
-      context "with the LoggedIn A/B variant" do
-        it "requests the signed-in header" do
-          with_variant AccountExperiment: "LoggedIn" do
-            get :show
-            assert_equal "signed-in", response.headers["X-Slimmer-Show-Accounts"]
-          end
-        end
-      end
-
-      context "with the LoggedOut A/B variant" do
-        it "requests the signed-out header" do
-          with_variant AccountExperiment: "LoggedOut" do
-            get :show
-            assert_equal "signed-out", response.headers["X-Slimmer-Show-Accounts"]
-          end
-        end
-
-        context "the GOVUK-Account-Session header is set" do
-          it "requests the signed-in header" do
-            with_variant AccountExperiment: "LoggedOut" do
-              request.headers["GOVUK-Account-Session"] = "foo"
-              get :show
-              assert_equal "signed-in", response.headers["X-Slimmer-Show-Accounts"]
-            end
-          end
         end
       end
     end
