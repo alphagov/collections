afaab956f29c417e616697096c4139d27402e7e7
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2021-03-04 17:46:30 +0000

    feeds controller

--- spec/controllers/feeds_controller_spec.rb
+++ spec/controllers/feeds_controller_spec.rb
@@ -1,11 +1,6 @@
-require "test_helper"
-
-class FeedsControllerTest < ActionController::TestCase
-  include OrganisationFeedHelpers
-
-  test "routing handles paths with just format" do
-    assert_routing(
-      "/government/organisations/ministry-of-magic.atom",
+RSpec.describe FeedsController, type: :routing do
+  it "routing handles paths with just format" do
+    expect(get: "/government/organisations/ministry-of-magic.atom").to route_to(
       controller: "feeds",
       action: "organisation",
       organisation_name: "ministry-of-magic",
@@ -13,9 +8,8 @@ class FeedsControllerTest < ActionController::TestCase
     )
   end
 
-  test "routing handles paths with format and locale" do
-    assert_routing(
-      "/government/organisations/ministry-of-magic.cy.atom",
+  it "routing handles paths with format and locale" do
+    expect(get: "/government/organisations/ministry-of-magic.cy.atom").to route_to(
       controller: "feeds",
       action: "organisation",
       organisation_name: "ministry-of-magic",
@@ -23,24 +17,28 @@ class FeedsControllerTest < ActionController::TestCase
       locale: "cy",
     )
   end
+end
 
-  test "renders atom feeds" do
+RSpec.describe FeedsController, type: :controller do
+  include OrganisationFeedHelpers
+  render_views
+  it "renders atom feeds" do
     content_item = content_store_has_schema_example("organisation")
     stub_content_for_organisation_feed("ministry-of-magic", [])
 
     get :organisation, params: { organisation_name: organisation_slug(content_item), format: "atom" }
 
-    assert_response :success
-    assert_select "feed title", "Ministry of Magic - Activity on GOV.UK"
+    expect(response).to have_http_status(:success)
+    expect(response.body).to match(/<title>Ministry of Magic - Activity on GOV.UK<\/title>/)
   end
 
-  test "sets the Access-Control-Allow-Origin header for atom pages" do
+  it "sets the Access-Control-Allow-Origin header for atom pages" do
     content_item = content_store_has_schema_example("organisation")
     stub_content_for_organisation_feed("ministry-of-magic", [])
 
     get :organisation, params: { organisation_name: organisation_slug(content_item), format: "atom" }
 
-    assert_equal "*", response.headers["Access-Control-Allow-Origin"]
+    expect(response.headers["Access-Control-Allow-Origin"]).to eq("*")
   end
 
   def organisation_slug(content_item)
