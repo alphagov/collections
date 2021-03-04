60e9f5c0f87ada153e9985f7f1da41ceca7d3166
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2021-03-04 18:54:30 +0000

    coronavirus_local_restrictions_controller_spec

--- spec/controllers/coronavirus_local_restrictions_controller_spec.rb
+++ spec/controllers/coronavirus_local_restrictions_controller_spec.rb
@@ -1,50 +1,56 @@
-require "test_helper"
 require "gds_api/test_helpers/mapit"
 
-describe CoronavirusLocalRestrictionsController do
+RSpec.describe CoronavirusLocalRestrictionsController do
   include GdsApi::TestHelpers::Mapit
-  include GdsApi::TestHelpers::ContentStore
+  render_views
 
   before do
     stub_content_store_has_item("/find-coronavirus-local-restrictions", {})
-    CoronavirusLocalRestrictionsController.any_instance.stubs(:out_of_date?).returns(false)
+    allow_any_instance_of(CoronavirusLocalRestrictionsController)
+    .to receive(:out_of_date?)
+    .and_return(false)
   end
 
   describe "GET show" do
     it "returns a cached redirect when Rails is in production" do
-      Rails.env.stubs(:production?).returns(true)
+      allow(Rails.env)
+      .to receive(:production?)
+      .and_return(true)
+
       get :show
 
-      assert_equal "max-age=#{30.minutes}, public", response.headers["Cache-Control"]
-      assert_response :temporary_redirect
-      assert_redirected_to "/guidance/national-lockdown-stay-at-home"
+      expect(response.headers["Cache-Control"]).to eq("max-age=#{30.minutes}, public")
+      expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to("/guidance/national-lockdown-stay-at-home")
     end
 
     it "renders the show template when not given a postcode" do
       get :show
 
-      assert_response :success
+      expect(response).to have_http_status(:success)
       assert_template :show
     end
 
     it "caches responses publicly for 30 minutes by default" do
       get :show
 
-      assert_equal "max-age=#{30.minutes}, public", response.headers["Cache-Control"]
+      expect(response.headers["Cache-Control"]).to eq("max-age=#{30.minutes}, public")
     end
 
     it "caches responses for for 5 minutes when the data is being updated" do
-      CoronavirusLocalRestrictionsController.any_instance.stubs(:out_of_date?).returns(true)
+      allow_any_instance_of(CoronavirusLocalRestrictionsController)
+      .to receive(:out_of_date?)
+      .and_return(true)
 
       get :show
 
-      assert_equal "max-age=#{5.minutes}, public", response.headers["Cache-Control"]
+      expect(response.headers["Cache-Control"]).to eq("max-age=#{5.minutes}, public")
     end
 
     it "renders the show template when given an invalid postcode" do
       get :show, params: { postcode: "not-a-postcode" }
 
-      assert_response :success
+      expect(response).to have_http_status(:success)
       assert_template :show
     end
 
@@ -52,8 +58,8 @@ describe CoronavirusLocalRestrictionsController do
       stub_mapit_has_a_postcode_and_areas("E1 8QS", [], [])
       get :show, params: { postcode: "E1 8QS" }
 
-      assert_response :success
-      assert_template :no_information
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:no_information)
     end
 
     it "renders the no_information template when there is no restriction data for an area in local_restrictions.yml" do
@@ -69,21 +75,25 @@ describe CoronavirusLocalRestrictionsController do
         },
       ]
 
-      LocalRestriction.stubs(:find).with(gss).returns(nil)
+      allow(LocalRestriction)
+      .to receive(:find)
+      .with(gss)
+      .and_return(nil)
 
       stub_mapit_has_a_postcode_and_areas(postcode, [], areas)
 
       get :show, params: { postcode: "E1 8QS" }
 
-      assert_response :success
-      assert_template :no_information
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:no_information)
     end
 
     it "renders the results template for an area with information" do
       restriction = LocalRestriction.new("E01000123",
                                          { "name" => "Coruscant Planetary Council" })
-      LocalRestriction.stubs(:find).returns(restriction)
-
+      allow(LocalRestriction)
+      .to receive(:find)
+      .and_return(restriction)
       postcode = "E1 8QS"
       stub_mapit_has_a_postcode_and_areas(postcode, [], [{
         "gss" => restriction.gss_code,
@@ -93,8 +103,8 @@ describe CoronavirusLocalRestrictionsController do
       }])
       get :show, params: { postcode: postcode }
 
-      assert_response :success
-      assert_template :results
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:results)
     end
 
     it "reduces the cache time when an area has an upcoming future restriction" do
@@ -111,7 +121,10 @@ describe CoronavirusLocalRestrictionsController do
           ],
         },
       )
-      LocalRestriction.stubs(:find).returns(restriction)
+
+      allow(LocalRestriction)
+      .to receive(:find)
+      .and_return(restriction)
 
       postcode = "E1 8QS"
       stub_mapit_has_a_postcode_and_areas(postcode, [], [{
@@ -124,7 +137,7 @@ describe CoronavirusLocalRestrictionsController do
       travel_to(Time.zone.parse("2020-12-15 9:50")) do
         get :show, params: { postcode: postcode }
 
-        assert_equal "max-age=#{10.minutes}, public", response.headers["Cache-Control"]
+        expect(response.headers["Cache-Control"]).to eq("max-age=#{10.minutes}, public")
       end
     end
   end
