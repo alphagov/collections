962ce32a62ef5515d5063300375150d6463b2f18
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2021-03-08 11:12:40 +0000

    Move non-English and non-Welsh language names to their own locale
    We currently have non-English and non-Welsh language names in the
    English and Welsh locale files, which are used to display language names
    for translated content. This is very bad practice as it means we have to
    maintain two copies of the same translations.
    
    Here we update to include the language name in the correct language
    locale.

--- app/presenters/transition_landing_page_presenter.rb
+++ app/presenters/transition_landing_page_presenter.rb
@@ -42,7 +42,7 @@ class TransitionLandingPagePresenter
       {
         locale: link.locale,
         base_path: link.base_path,
-        text: I18n.t("shared.language_names.#{link.locale}"),
+        text: I18n.t("shared.language_name", locale: link.locale),
         active: active,
       }
     end
