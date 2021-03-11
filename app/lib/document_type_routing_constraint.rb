37e07da3076284b5768e181c5276c550648d3d8e
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2021-03-11 17:03:04 +0000

    refactor document_type_routing_constraint

--- app/lib/document_type_routing_constraint.rb
+++ app/lib/document_type_routing_constraint.rb
@@ -1,17 +1,16 @@
 class DocumentTypeRoutingConstraint
-  def initialize(document_type, document_type_inspector: DocumentTypeInspector)
+  def initialize(document_type)
     @document_type = document_type
-    @document_type_inspector = document_type_inspector
   end
 
   def matches?(request)
     @request = request
-    document_type == @document_type
+    request_document_type == @document_type
   end
 
 private
 
-  def document_type
+  def request_document_type
     @request.env[:__document_type] ||= begin
       document_type_inspector.document_type || set_error(document_type_inspector.error) || :no_match
     end
@@ -23,7 +22,7 @@ private
   end
 
   def document_type_inspector
-    @request.env[:__inspector] ||= @document_type_inspector.new(slug)
+    @request.env[:__inspector] ||= DocumentTypeInspector.new(slug)
   end
 
   def slug
--- spec/lib/document_type_routing_constraint_spec.rb
+++ spec/lib/document_type_routing_constraint_spec.rb
@@ -2,52 +2,49 @@ RSpec.describe DocumentTypeRoutingConstraint do
   describe "#matches?" do
     context "when the content_store returns a document" do
       it "should return true if document_type matches" do
-        instance = instance_double("DocumentTypeInspector")
-        subject = document_type_routing_constraint("foo", instance)
+        request = double("request", env: {})
+
+        doc_type = "foo"
+
+        subject = described_class.new(doc_type)
+        inspector = instance_double("DocumentTypeInspector", document_type: doc_type)
 
         allow(subject)
         .to receive(:document_type_inspector)
-        .and_return(instance)
-
-        allow(instance)
-        .to receive(:document_type)
-        .and_return("foo")
-
-        request = double("request", env: {})
+        .and_return(inspector)
 
         expect(subject.matches?(request)).to be(true)
       end
 
       it "should return false if document_type doesn't match" do
-        instance = instance_double("DocumentTypeInspector")
-        subject = document_type_routing_constraint("bar", instance)
+        request = double("request", env: {})
+
+        doc_type = "bar"
+        request_doc_type = "foo"
+
+        subject = described_class.new(doc_type)
+        inspector = instance_double("DocumentTypeInspector", document_type: request_doc_type)
 
         allow(subject)
         .to receive(:document_type_inspector)
-        .and_return(instance)
-
-        allow(instance)
-        .to receive(:document_type)
-        .and_return("foo")
-
-        request = double("request", env: {})
+        .and_return(inspector)
 
         expect(subject.matches?(request)).to be(false)
       end
     end
 
     context "when the content_store API call throws an error" do
-      let(:error) { GdsApi::HTTPNotFound.new(404) }
-
       it "should return false and set an error on the request object" do
-        instance = instance_double("DocumentTypeInspector", document_type: nil, error: error)
-        subject = document_type_routing_constraint("foo", instance)
+        request = double("request", env: {})
+
+        error = GdsApi::HTTPNotFound.new(404)
+
+        subject = described_class.new("foo")
+        inspector = instance_double("DocumentTypeInspector", document_type: nil, error: error)
 
         allow(subject)
         .to receive(:document_type_inspector)
-        .and_return(instance)
-
-        request = double("request", env: {})
+        .and_return(inspector)
 
         expect(subject.matches?(request)).to be(false)
         expect(request.env[:__api_error]).to eq(error)
@@ -56,19 +53,20 @@ RSpec.describe DocumentTypeRoutingConstraint do
   end
 
   context "instances are memoized and used across multiple requests" do
-    let(:document_type) { "static across requests" }
-
     it "should make new API calls each time" do
-      api_proxy = double("api_proxy")
-      inspector = double("inspector", document_type: document_type, error: nil)
       request1 = double("request", params: { slug: "slug" }, env: {})
       request2 = double("request", params: { slug: "slug" }, env: {})
-      subject = document_type_routing_constraint("foo", api_proxy)
 
-      expect(api_proxy)
+      doc_type = "foo"
+
+      api_proxy = double("api_proxy", document_type: doc_type)
+
+      subject = described_class.new(doc_type)
+
+      expect(DocumentTypeInspector)
       .to receive(:new)
       .twice
-      .and_return(inspector)
+      .and_return(api_proxy)
 
       subject.matches?(request1)
       subject.matches?(request2)
@@ -77,24 +75,24 @@ RSpec.describe DocumentTypeRoutingConstraint do
 
   context "a request will be passed to multiple instances" do
     it "should not make additional API calls" do
-      instance = instance_double("DocumentTypeInspector", document_type: "baz")
-      api_proxy = double("api_proxy", new: instance)
       request = double("request", params: { slug: "slug" }, env: {})
 
-      expect(api_proxy)
+      doc_type1 = "foo"
+      doc_type2 = "bar"
+      request_doc_type = "baz"
+
+      api_proxy = double("api_proxy", document_type: request_doc_type)
+
+      subject1 = described_class.new(doc_type1)
+      subject2 = described_class.new(doc_type2)
+
+      expect(DocumentTypeInspector)
       .to receive(:new)
       .once
-      .and_return(instance)
+      .and_return(api_proxy)
 
-      document_type_routing_constraint("foo", api_proxy).matches?(request)
-      document_type_routing_constraint("bar", api_proxy).matches?(request)
+      subject1.matches?(request)
+      subject2.matches?(request)
     end
-  end
-
-  def document_type_routing_constraint(doc_type, document_type_inspector)
-    described_class.new(
-      doc_type,
-      document_type_inspector: document_type_inspector,
-    )
   end
 end
