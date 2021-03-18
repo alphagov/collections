0e63894e3b2c566ae3315471717640bd22a3ba68
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2021-03-18 14:31:52 +0000

    Move presenter tests
    This is a precursor to converting them to rspec specs.

--- spec/presenters/world_wide_taxon_presenter_spec.rb
+++ spec/presenters/world_wide_taxon_presenter_spec.rb
@@ -9,22 +9,14 @@ describe WorldWideTaxonPresenter do
       taxon = mock
       taxon.stubs(grandchildren?: false, children?: true)
 
-      assert_equal(
-        WorldWideTaxonPresenter::ACCORDION,
-        WorldWideTaxonPresenter.new(taxon).rendering_type,
-        "Expected a taxon with children and no grandchildren to be rendered as an accordion page",
-      )
+      assert_equal(WorldWideTaxonPresenter::ACCORDION, WorldWideTaxonPresenter.new(taxon).rendering_type,    "Expected a taxon with children and no grandchildren to be rendered as an accordion page"      )
     end
 
     it "returns LEAF when the taxon has children and no grandchildren" do
       taxon = mock
       taxon.stubs(grandchildren?: false, children?: false)
 
-      assert_equal(
-        WorldWideTaxonPresenter::LEAF,
-        WorldWideTaxonPresenter.new(taxon).rendering_type,
-        "Expected a taxon without children and grandchildren to be rendered as a leaf page",
-      )
+      assert_equal(WorldWideTaxonPresenter::LEAF,         WorldWideTaxonPresenter.new(taxon).rendering_type,         "Expected a taxon without children and grandchildren to be rendered as a leaf page"       )
     end
   end
 
