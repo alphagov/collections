1284ea513864afbd26513591622615a29c9becf1
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2021-03-19 11:46:34 +0000

    Convert minister pages spec

--- spec/features/ministers_spec.rb
+++ spec/features/ministers_spec.rb
@@ -1,28 +1,28 @@
-require "integration_test_helper"
+require "integration_spec_helper"
 
-class MinistersTest < ActionDispatch::IntegrationTest
+RSpec.feature "Minister pages" do
   before do
     stub_content_store_has_item("/government/ministers", ministers_content_hash)
     visit "/government/ministers"
   end
 
-  it "returns 200 when visiting ministers page" do
+  scenario "returns 200 when visiting ministers page" do
     expect(page.status_code).to eq(200)
   end
 
-  it "renders webpage title" do
-    expect(page.has_title?("Ministers - GOV.UK")).to be
+  scenario "renders webpage title" do
+    expect(page).to have_title("Ministers - GOV.UK")
   end
 
-  it "renders page title" do
-    expect(page.has_css?(".gem-c-title__text", text: "Ministers")).to be
+  scenario "renders page title" do
+    expect(page).to have_selector(".gem-c-title__text", text: "Ministers")
   end
 
-  it "renders section headers" do
-    expect(page.has_css?(".gem-c-heading", text: "Cabinet ministers")).to be
-    expect(page.has_css?(".gem-c-heading", text: "Also attends Cabinet")).to be
-    expect(page.has_css?(".gem-c-heading", text: "Ministers by department")).to be
-    expect(page.has_css?(".gem-c-heading", text: "Whips")).to be
+  scenario "renders section headers" do
+    expect(page).to have_selector(".gem-c-heading", text: "Cabinet ministers")
+    expect(page).to have_selector(".gem-c-heading", text: "Also attends Cabinet")
+    expect(page).to have_selector(".gem-c-heading", text: "Ministers by department")
+    expect(page).to have_selector(".gem-c-heading", text: "Whips")
   end
 
 private
