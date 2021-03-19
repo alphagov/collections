10ed1d069dd086e3db5017977a9a44790c0520e7
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2021-03-19 20:47:34 +0000

    Request spec: - Convert organisations api route spec - Amend syntax error in redirection request spec.

--- spec/requests/organisations_api_route_spec.rb
+++ spec/requests/organisations_api_route_spec.rb
@@ -1,10 +1,8 @@
-require "integration_test_helper"
-
-class OrganisationApiRouteTest < ActionDispatch::IntegrationTest
+RSpec.describe "Organisations api routes" do
   it "should respond with '404 for a bad route'" do
     get "/api/organisations/bad/route", as: :json
 
-    assert_equal 404, response.status
-    assert_equal "404 error", response.body
+    expect(response).to have_http_status(:not_found)
+    expect(response.body).to eq("404 error")
   end
 end
