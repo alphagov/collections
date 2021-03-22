1ebf4f905fed94b395daf31497bcbc7af351a4ae
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2021-03-22 15:21:01 +0000

    Remove unused test helpers

--- test/support/analytics_helpers.rb
+++
@@ -1,5 +0,0 @@
-module AnalyticsHelpers
-  def page_has_meta_tag?(name, content)
-    page.has_selector?(:xpath, "/html/head/meta[@name='#{name}'][@content='#{content}']", visible: false)
-  end
-end
