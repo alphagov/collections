5d9a073f4623dece8e5ef6766127d6621a6f967a
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2021-03-24 15:43:15 +0000

    Convert coronavirus_local_restrictions show view spec to RSpec A small refactor was needed because view helper methods defined in controllers are not available to view tests. The CoronavirusLocalRestrictions controller implemented out_of_date? as both a view helper and an instance method to modify cache time. So I've moved the view helper form out into the ApplicationHelper module to allow for stubbing: https://relishapp.com/rspec/rspec-rails/v/5-0/docs/view-specs/view-spec#view-specs-can-stub-a-helper-method

--- spec/views/coronavirus_local_restrictions/show_html_spec.rb
+++ spec/views/coronavirus_local_restrictions/show_html_spec.rb
@@ -1,36 +1,32 @@
-require "test_helper"
-module CoronavirusLocalRestrictions
-  class ShowHtmlTest < ActionView::TestCase
-    include CoronavirusLocalRestrictionsHelpers
-    helper Rails.application.helpers
+RSpec.describe "coronavirus_local_restrictions/show.html.erb" do
+  include CoronavirusLocalRestrictionsHelpers
 
-    describe "errors" do
-      test "rendering error when invalid postcode is entered" do
-        postcode = "hello"
+  describe "errors" do
+    it "rendering error when invalid postcode is entered" do
+      postcode = "hello"
 
-        render_show_view(postcode)
+      render_show_view(postcode)
 
-        assert_includes rendered, I18n.t("coronavirus_local_restrictions.errors.invalid_postcode.input_error")
-      end
-
-      test "rendering error when postcode does not exist" do
-        postcode = "XM4 5HQ"
-
-        stub_mapit_does_not_have_a_postcode(postcode)
-
-        render_show_view(postcode)
-
-        assert_includes rendered, I18n.t("coronavirus_local_restrictions.errors.postcode_not_found.input_error")
-      end
+      expect(rendered).to match(I18n.t("coronavirus_local_restrictions.errors.invalid_postcode.input_error"))
     end
 
-    def render_show_view(postcode)
-      @search = PostcodeLocalRestrictionSearch.new(postcode)
-      @content_item = coronavirus_content_item
+    it "rendering error when postcode does not exist" do
+      postcode = "XM4 5HQ"
 
-      view.stubs(:out_of_date?).returns(false)
+      stub_mapit_does_not_have_a_postcode(postcode)
 
-      render template: "coronavirus_local_restrictions/show"
+      render_show_view(postcode)
+
+      expect(rendered).to match(I18n.t("coronavirus_local_restrictions.errors.postcode_not_found.input_error"))
     end
+  end
+
+  def render_show_view(postcode)
+    @search = PostcodeLocalRestrictionSearch.new(postcode)
+    @content_item = coronavirus_content_item
+    allow(view)
+    .to receive(:out_of_date?)
+    .and_return(false)
+    render
   end
 end
