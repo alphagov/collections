0ae41297ba3c0110db965f681beedfa2ede3ec9d
Alan Gabbianelli <alan.gabbianelli@digital.cabinet-office.gov.uk>
2021-06-02 14:49:03 +0100

    Use govuk_personalisation
    The `govuk_personalisation` gem provides this code so it can be deleted
    from here.
    
    Both `show_signed_in_header?` and `logged_in?` have the same
    implementation (`account_session_header.present?`) so I'm using the
    latter here now because it's provided by the gem.

--- app/controllers/concerns/account_concern.rb
+++ app/controllers/concerns/account_concern.rb
@@ -3,32 +3,14 @@
 module AccountConcern
   extend ActiveSupport::Concern
 
-  ACCOUNT_SESSION_REQUEST_HEADER_NAME = "HTTP_GOVUK_ACCOUNT_SESSION"
-  ACCOUNT_SESSION_RESPONSE_HEADER_NAME = "GOVUK-Account-Session"
-  ACCOUNT_SESSION_DEV_COOKIE_NAME = "govuk_account_session"
-
   included do
-    before_action :set_account_variant
-  end
+    include GovukPersonalisation::AccountConcern
 
-  def account_session_header
-    if request.headers[ACCOUNT_SESSION_REQUEST_HEADER_NAME]
-      request.headers[ACCOUNT_SESSION_REQUEST_HEADER_NAME]
-    elsif Rails.env.development?
-      cookies[ACCOUNT_SESSION_DEV_COOKIE_NAME]
+    before_action do
+      set_slimmer_headers(
+        remove_search: true,
+        show_accounts: logged_in? ? "signed-in" : "signed-out",
+      )
     end
-  end
-
-  def show_signed_in_header?
-    account_session_header.present?
-  end
-
-  def set_account_variant
-    response.headers["Vary"] = [response.headers["Vary"], ACCOUNT_SESSION_RESPONSE_HEADER_NAME].compact.join(", ")
-
-    set_slimmer_headers(
-      remove_search: true,
-      show_accounts: show_signed_in_header? ? "signed-in" : "signed-out",
-    )
   end
 end
