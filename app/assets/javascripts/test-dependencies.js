ad7f284b01508883af93c7dc622bf75e4b3efbcc
Ian James <ian.james@digital.cabinet-office.gov.uk>
2021-06-10 13:23:11 +0100

    Update test JavaScript dependencies
    Because certain scripts (such as the cookie helpers) now come from
    Static, we need to specifically include them in the tests that rely on
    them.

--- app/assets/javascripts/test-dependencies.js
+++ app/assets/javascripts/test-dependencies.js
@@ -1,3 +1,6 @@
 // This file contains dependencies that are only needed when running in a test
 // environment. In the dev and live environment these are provided by static.
 //= require govuk_publishing_components/dependencies
+//= require govuk_publishing_components/lib/cookie-functions
+//= require govuk_publishing_components/lib/header-navigation
+//= require govuk_publishing_components/lib/track-click
