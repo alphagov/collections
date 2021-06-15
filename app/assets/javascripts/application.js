42fdeb4ee2c260a3a1e4fd82debe71ad273f8541
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2021-06-15 11:56:00 +0100

    Add assets for radio buttons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,6 +26,7 @@
 //= require govuk_publishing_components/components/details
 //= require govuk_publishing_components/components/feedback
 //= require govuk_publishing_components/components/govspeak
+//= require govuk_publishing_components/components/radio
 //= require govuk_publishing_components/components/step-by-step-nav
 //= require govuk_publishing_components/components/error-summary
 //= require govuk_publishing_components/analytics/explicit-cross-domain-links
