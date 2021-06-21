8f01c6d97db8bd7fa45e710e1e8c56bb175c6396
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2021-06-21 12:57:11 +0100

    Add helper to get the timeline for specific nations

--- app/helpers/coronavirus_timeline_nations_helper.rb
+++ app/helpers/coronavirus_timeline_nations_helper.rb
@@ -2,4 +2,8 @@ module CoronavirusTimelineNationsHelper
   def show_timeline_nations?(timeline_list)
     timeline_list.any? { |item| item["national_applicability"] }
   end
+
+  def timeline_for_nation(timeline_list, nation)
+    timeline_list.select { |item| item["national_applicability"].include?(nation) }
+  end
 end
--- spec/helpers/coronavirus_timeline_nations_helper_spec.rb
+++ spec/helpers/coronavirus_timeline_nations_helper_spec.rb
@@ -16,4 +16,37 @@ RSpec.describe CoronavirusTimelineNationsHelper do
       expect(helper.show_timeline_nations?(timeline_list)).to be true
     end
   end
+
+  describe "#timeline_for_nation" do
+    it "only returns the entries for requested country" do
+      timeline_list = [
+        {
+          "heading" => "England applicability",
+          "national_applicability" => %w[england],
+          "paragraph" => "Timeline entry only applies to England.",
+        },
+        {
+          "heading" => "Wales applicability",
+          "national_applicability" => %w[wales],
+          "paragraph" => "Timeline entry only applies to Wales.",
+        },
+      ]
+
+      expect(helper.timeline_for_nation(timeline_list, "wales").size).to eq(1)
+      expect(helper.timeline_for_nation(timeline_list, "wales").first).to match(hash_including("heading" => "Wales applicability"))
+    end
+
+    it "returns entries shared by multiple countries" do
+      timeline_list = [
+        {
+          "heading" => "England and Wales applicability",
+          "national_applicability" => %w[england wales],
+          "paragraph" => "Timeline entry applies to England and Wales.",
+        },
+      ]
+
+      expect(helper.timeline_for_nation(timeline_list, "wales").size).to eq(1)
+      expect(helper.timeline_for_nation(timeline_list, "wales").first).to match(hash_including("heading" => "England and Wales applicability"))
+    end
+  end
 end
