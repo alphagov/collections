7b0e1f28a3d622da15191ac0105776869ba2b5d4
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2021-07-05 20:53:55 +0100

    Add AbTests::SlimmerTemplateSelectable
    To reduce repeated code in each of the controllers. This module adds logic for when users are in the B group, we request the Explore Super Menu Header from Static via Slimmer.

---
+++ app/controllers/ab_tests/slimmer_template_selectable.rb
@@ -0,0 +1,17 @@
+module AbTests::SlimmerTemplateSelectable
+  def set_gem_layout
+    if explore_menu_testable?
+      slimmer_template "gem_layout_explore_header"
+    else
+      slimmer_template "gem_layout"
+    end
+  end
+
+  def set_gem_layout_full_width
+    if explore_menu_testable?
+      slimmer_template "gem_layout_full_width_explore_header"
+    else
+      slimmer_template "gem_layout_full_width"
+    end
+  end
+end
