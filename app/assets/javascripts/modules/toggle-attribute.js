46f7f236528f4cfd6fad7c587390ec689866be13
JonathanHallam <jonathan.hallam@digital.cabinet-office.gov.uk>
2021-07-23 14:07:21 +0100

    wip

--- app/assets/javascripts/modules/toggle-attribute.js
+++ app/assets/javascripts/modules/toggle-attribute.js
@@ -6,15 +6,23 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
 
   Modules.ToggleAttribute = function () {
     this.start = function ($element) {
-      $element.find('[data-toggle-attribute]').click(function (event) {
-        var clicked = event.target
-        var toggleAttribute = clicked.getAttribute('data-toggle-attribute')
-        var current = clicked.getAttribute(toggleAttribute)
-        var closedText = clicked.getAttribute('data-when-closed-text')
-        var openText = clicked.getAttribute('data-when-open-text')
+      if ($element[0]){
 
-        clicked.setAttribute(toggleAttribute, current === closedText ? openText : closedText)
-      })
+        console.log($element.find('[data-toggle-attribute]')[0])
+        console.log($element[0].querySelectorAll('[data-toggle-attribute]'))
+
+        for (let i = 0; i < $element[0].querySelectorAll('[data-toggle-attribute]').length; i++) {
+          $element[0].querySelectorAll('[data-toggle-attribute]')[i].click(function (event) {
+            var clicked = event.target
+            var toggleAttribute = clicked.getAttribute('data-toggle-attribute')
+            var current = clicked.getAttribute(toggleAttribute)
+            var closedText = clicked.getAttribute('data-when-closed-text')
+            var openText = clicked.getAttribute('data-when-open-text')
+
+            clicked.setAttribute(toggleAttribute, current === closedText ? openText : closedText)
+          })
+        }
+      }
     }
   }
 })(window.GOVUK.Modules)
