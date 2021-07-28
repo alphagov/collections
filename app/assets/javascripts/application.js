6f55616c6374dd9cf02d60f4a4f907d362dc39f8
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2021-07-28 17:17:32 +0100

    Move FilterOrganisations into modules folder

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,7 +33,7 @@
 
 //= require support
 //= require browse-columns
-//= require organisation-list-filter
+//= require modules/organisation-list-filter
 //= require modules/toggle-attribute
 //= require modules/coronavirus-landing-page
 //= require modules/track-links
