26dca639b20cd5f380946ec99d49e56df8718f0c
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2022-01-21 10:52:52 +0000

    Add locale validation to test suite

---
+++ spec/lib/locales_validation_spec.rb
@@ -0,0 +1,6 @@
+RSpec.describe "locales files" do
+  it "should meet all locale validation requirements" do
+    checker = RailsTranslationManager::LocaleChecker.new("config/locales/*/*.yml")
+    expect(checker.validate_locales).to be_truthy
+  end
+end
