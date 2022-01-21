134b326ccecc2215d7ed92e1648f1eeecdf79a82
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2022-01-21 11:40:43 +0000

    Exclude a false positive
    'other' is the name of an organisation type, so it doesn't feel safe just to rename it. Best to skip it, as `organisations.type` is not a set of singular/plural forms.

--- spec/lib/locales_validation_spec.rb
+++ spec/lib/locales_validation_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe "locales files" do
   it "should meet all locale validation requirements" do
-    checker = RailsTranslationManager::LocaleChecker.new("config/locales/*/*.yml")
+    checker = RailsTranslationManager::LocaleChecker.new("config/locales/*/*.yml", %w[organisations.type])
     expect(checker.validate_locales).to be_truthy
   end
 end
