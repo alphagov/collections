95e91b11fe0296b5b41b7c26c71334115b7224d1
Ian James <ian.james@digital.cabinet-office.gov.uk>
2022-03-03 15:56:49 +0000

    Add Topic level 0 & 1 templates and layout toggle
     - Append `?b=true` to the URL to turn the B (new) page layout on

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -6,40 +6,50 @@ class BrowseController < ApplicationController
     @dimension26 = 1
     @dimension27 = page.top_level_browse_pages.count || 0
     setup_content_item_and_navigation_helpers(page)
+    slimmer_template "gem_layout_full_width" if is_variant_b?
 
-    render :index,
-           locals: {
-             page: page,
-           }
+    template = is_variant_b? ? :new_index : :index
+
+    render(template, locals: {
+      page: page,
+    })
   end
 
   def show
-    page =
-      MainstreamBrowsePage.find("/browse/#{params[:top_level_slug]}")
+    page = MainstreamBrowsePage.find("/browse/#{params[:top_level_slug]}")
     setup_content_item_and_navigation_helpers(page)
+    slimmer_template "gem_layout_full_width" if is_variant_b?
 
     respond_to do |f|
       f.html do
         show_html(page)
       end
+
       f.json do
-        render json: {
+        render(json: {
           content_id: page.content_id,
           navigation_page_type: "First Level Browse",
           breadcrumbs: breadcrumb_content,
           html: second_level_browse_pages_partial(page),
-        }
+        })
       end
     end
   end
 
 private
 
+  # NOTE: This is just to fake an A/B test - replace with proper A/B test code.
+  # Add a query string with b=true to the URL to force variant B.
+  def is_variant_b?
+    params["b"].present?
+  end
+
   def show_html(page)
-    render :show,
-           locals: {
-             page: page,
-           }
+    template = is_variant_b? ? :new_show : :show
+
+    render(template, locals: {
+      page: page,
+    })
   end
 
   def second_level_browse_pages_partial(page)
