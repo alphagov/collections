7630ab4eda6c34a5d5159990b89efbaa538d869d
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2022-05-10 17:03:55 +0100

    Update the list of keys that should be skipped
    This spec is running the same rake tasks. It needs it's own
    list of excluded keys. See:
    
    https://github.com/alphagov/rails_translation_manager/blob/e6d46f03cf1d9a75f852784aaab385ed48de1147/lib/rails_translation_manager/locale_checker.rb#L15

--- spec/lib/locales_validation_spec.rb
+++ spec/lib/locales_validation_spec.rb
@@ -1,6 +1,12 @@
 RSpec.describe "locales files" do
   it "should meet all locale validation requirements" do
-    checker = RailsTranslationManager::LocaleChecker.new("config/locales/*/*.yml", %w[organisations.type])
+    skip_validation = %w[
+      organisations.content_item.schema_name
+      roles.heading
+      organisations.type
+      organisations.works_with_statement
+    ]
+    checker = RailsTranslationManager::LocaleChecker.new("config/locales/*/*.yml", skip_validation)
     expect(checker.validate_locales).to be_truthy
   end
 end
