2a9a4c4c1fbf1ad742d3c0662dacd31bcc110574
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2022-05-16 07:35:12 +0100

    Update component asset imports
    - removing assets for components not needed as components have been removed (e.g. radios on the covid landing page)
    - removing assets for components that are already included in static and were therefore duplicated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,13 +2,10 @@
 
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/accordion
-//= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/details
-//= require govuk_publishing_components/components/feedback
 //= require govuk_publishing_components/components/govspeak
-//= require govuk_publishing_components/components/radio
+//= require govuk_publishing_components/components/metadata
 //= require govuk_publishing_components/components/step-by-step-nav
-//= require govuk_publishing_components/components/error-summary
 
 //= require support
 //= require browse-columns
