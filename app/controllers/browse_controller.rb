e3b77b024650c5cf849c1761d797e80799575640
Hanna <hannalaakso@users.noreply.github.com>
2022-07-26 11:35:23 +0100

    Switch between A and B variants
    Add logic to switch between A and B variants. The variants use the same templates, the only change is how the content items on the Level 2 curated pages are presented (accordions or lists). Make the curated lists the A (default) template.
    
    Switching the variant based on the query string is only for testing purposes, we will implement the proper logic in a later PR.

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,29 +1,17 @@
 class BrowseController < ApplicationController
+  slimmer_template "gem_layout_full_width"
+
   def index
     page = MainstreamBrowsePage.find("/browse")
     @dimension26 = 1
     @dimension27 = page.top_level_browse_pages.count || 0
     setup_content_item_and_navigation_helpers(page)
-    index_html(page)
+    render :index, locals: { page: page }
   end
 
   def show
     page = MainstreamBrowsePage.find("/browse/#{params[:top_level_slug]}")
     setup_content_item_and_navigation_helpers(page)
-    show_html(page)
-  end
-
-private
-
-  def show_html(page)
-    template = :show
-    slimmer_template "gem_layout_full_width"
-    render template, locals: { page: page }
-  end
-
-  def index_html(page)
-    template = :index
-    slimmer_template "gem_layout_full_width"
-    render template, locals: { page: page }
+    render :show, locals: { page: page }
   end
 end
