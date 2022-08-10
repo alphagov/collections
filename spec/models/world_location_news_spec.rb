7633c779b5bfc5adcc8185b7ef43b1c83f589bdc
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2022-08-10 08:43:33 +0100

    Add featured documents to world location news page
    This page is currently rendered by Whitehall. This is part of the work
    to bring the rendering into Collections.

--- spec/models/world_location_news_spec.rb
+++ spec/models/world_location_news_spec.rb
@@ -11,4 +11,36 @@ RSpec.describe WorldLocationNews do
   it "should have a description" do
     expect(world_location_news.description).to eq("Find out about the relations between the UK and Mock Country")
   end
+
+  it "should map the ordered featured documents with truncated description where this exceeds 160 chars" do
+    expect(world_location_news.ordered_featured_documents.first).to eq(
+      {
+        href: "https://www.gov.uk/somewhere",
+        image_src: "https://www.gov.uk/someimage.png",
+        image_alt: "Alt text for the image",
+        heading_text: "A document related to this location",
+        description: "Very interesting document content. However this goes over the 160 character limit so when displayed this should be truncated in order to display the content...",
+        context: {
+          date: Date.parse("2022-07-28"),
+          text: "News",
+        },
+      },
+    )
+  end
+
+  it "should map the ordered featured documents with offsite links that have no date" do
+    expect(world_location_news.ordered_featured_documents.second).to eq(
+      {
+        href: "https://www.gov.uk/somewhere-without-an-update-date",
+        image_src: "https://www.gov.uk/someimage2.png",
+        image_alt: "Alt text for the second image",
+        heading_text: "A document related to this location with no updated date",
+        description: "Very interesting document content.",
+        context: {
+          date: nil,
+          text: "Blog",
+        },
+      },
+    )
+  end
 end
