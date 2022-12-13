001be6879ab602c0983e4368f929a271c4353aa1
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2022-12-13 21:18:00 +0000

    Rename generic signup_url methods
    As we are about to add a second recruitment survey to the helper,
    make the naming more explicit.

--- app/helpers/research_panel_banner_helper.rb
+++ app/helpers/research_panel_banner_helper.rb
@@ -1,19 +1,19 @@
 module ResearchPanelBannerHelper
-  SIGNUP_URL = "https://signup.take-part-in-research.service.gov.uk/?utm_campaign=GOV.UK&utm_source=govukhp&utm_medium=gov.uk&t=GDS&id=456".freeze
-  RECRUITMENT_PAGES = {
-    "/browse/benefits" => SIGNUP_URL,
-    "/browse/births-deaths-marriages/register-offices" => SIGNUP_URL,
-    "/browse/disabilities" => SIGNUP_URL,
-    "/browse/disabilities/work" => SIGNUP_URL,
-    "/browse/driving/driving-licences" => SIGNUP_URL,
+  UR_COMMUNITY_SIGNUP_URL = "https://signup.take-part-in-research.service.gov.uk/?utm_campaign=GOV.UK&utm_source=govukhp&utm_medium=gov.uk&t=GDS&id=456".freeze
+  SURVEY_URL_MAPPINGS = {
+    "/browse/benefits" => UR_COMMUNITY_SIGNUP_URL,
+    "/browse/births-deaths-marriages/register-offices" => UR_COMMUNITY_SIGNUP_URL,
+    "/browse/disabilities" => UR_COMMUNITY_SIGNUP_URL,
+    "/browse/disabilities/work" => UR_COMMUNITY_SIGNUP_URL,
+    "/browse/driving/driving-licences" => UR_COMMUNITY_SIGNUP_URL,
   }.freeze
 
   def recruitment_survey_url
-    signup_url
+    ur_community_signup_url
   end
 
-  def signup_url
-    key = RECRUITMENT_PAGES.keys.find { |topic| base_path.eql?(topic) }
-    RECRUITMENT_PAGES[key]
+  def ur_community_signup_url
+    key = SURVEY_URL_MAPPINGS.keys.find { |topic| base_path.eql?(topic) }
+    SURVEY_URL_MAPPINGS[key]
   end
 end
