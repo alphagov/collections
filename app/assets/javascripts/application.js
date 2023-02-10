182e6059f04705fc1d9853dd89c68502a5bb7741
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2023-02-10 12:11:18 +0000

    WIP

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,7 @@
 //= require govuk_publishing_components/components/details
 //= require govuk_publishing_components/components/govspeak
 //= require govuk_publishing_components/components/image-card
+//= require govuk_publishing_components/components/intervention
 //= require govuk_publishing_components/components/metadata
 //= require govuk_publishing_components/components/step-by-step-nav
 
