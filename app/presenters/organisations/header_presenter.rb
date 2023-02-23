7e15b7b9359fd384c63144e595b205ffce8b627a
Jonathon <jonathon.shire@digital.cabinet-office.gov.uk>
2023-02-23 11:48:47 +0000

    Remove High Speed Two services and information
    This page is deprecated

--- app/presenters/organisations/header_presenter.rb
+++ app/presenters/organisations/header_presenter.rb
@@ -114,7 +114,6 @@ module Organisations
         department-for-education
         department-for-environment-food-rural-affairs
         driver-and-vehicle-standards-agency
-        high-speed-two-limited
         hm-revenue-customs
         marine-management-organisation
         maritime-and-coastguard-agency
