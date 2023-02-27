3a55a6b5a930bda01ee08e942a0a656f90d4d361
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2023-02-27 18:53:32 +0000

    Add cost of living banner to state pension browse page

---
+++ app/helpers/cost_of_living_banner_helper.rb
@@ -0,0 +1,11 @@
+module CostOfLivingBannerHelper
+  COST_OF_LIVING_SURVEY_URL = "https://surveys.publishing.service.gov.uk/s/XS2YWV/".freeze
+
+  SURVEY_URL_MAPPINGS = {
+    "/browse/working/state-pension" => COST_OF_LIVING_SURVEY_URL,
+  }.freeze
+
+  def survey_url
+    SURVEY_URL_MAPPINGS[base_path]
+  end
+end
