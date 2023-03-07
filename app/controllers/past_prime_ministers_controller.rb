0405936f658ed0332b365626f75ac97ad8ab463d
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2023-03-07 13:02:17 +0000

    Introduce the basics of the past prime ministers index page
    This introduces the relevant controller methods, routes and hard-coded view.

--- app/controllers/past_prime_ministers_controller.rb
+++ app/controllers/past_prime_ministers_controller.rb
@@ -3,4 +3,8 @@ class PastPrimeMinistersController < ApplicationController
     @past_prime_minister = PastPrimeMinister.find!(request.path)
     setup_content_item_and_navigation_helpers(@past_prime_minister)
   end
+
+  def index
+    @content_item = {}
+  end
 end
--- spec/controllers/past_prime_ministers_controller_spec.rb
+++ spec/controllers/past_prime_ministers_controller_spec.rb
@@ -1,23 +1,23 @@
 RSpec.describe PastPrimeMinistersController do
-  let(:base_path) { "/government/history/past-prime-ministers" }
-  let(:past_pm_id) { "a-pm" }
-  let(:content_item) do
-    {
-      base_path: "#{base_path}/#{past_pm_id}",
-      title: "a past PM",
-      details: {
-        born: "1900",
-        died: "2000",
-        interesting_facts: "a fact",
-        major_acts: "an act",
-        political_party: "A party",
-        dates_in_office: [{ end_year: 2005, start_year: 2000 }],
-      },
-      links: { ordered_related_items: [] },
-    }
-  end
-
   describe "GET show" do
+    let(:base_path) { "/government/history/past-prime-ministers" }
+    let(:past_pm_id) { "a-pm" }
+    let(:content_item) do
+      {
+        base_path: "#{base_path}/#{past_pm_id}",
+        title: "a past PM",
+        details: {
+          born: "1900",
+          died: "2000",
+          interesting_facts: "a fact",
+          major_acts: "an act",
+          political_party: "A party",
+          dates_in_office: [{ end_year: 2005, start_year: 2000 }],
+        },
+        links: { ordered_related_items: [] },
+      }
+    end
+
     it "has a success response for an existent pm" do
       stub_content_store_has_item("#{base_path}/#{past_pm_id}", content_item)
 
@@ -32,6 +32,15 @@ RSpec.describe PastPrimeMinistersController do
       get :show, params: { id: }
 
       expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe "GET index" do
+    let(:base_path) { "government/history/past-prime-ministers" }
+
+    it "has a success response" do
+      get :index
+      expect(response).to have_http_status(:success)
     end
   end
 end
