4d64ef0bd03e68404e8d70ec3b86fcc7d788a028
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2023-03-13 16:00:55 +0000

    Add method to historical figures index model
    This will allow us to present the centuries data on the page.

--- app/models/historic_appointments_index.rb
+++ app/models/historic_appointments_index.rb
@@ -9,6 +9,11 @@ class HistoricAppointmentsIndex
     @content_item = content_item
   end
 
+  def centuries_data
+    @content_item.dig("body", "centuries")
+                 .transform_keys { |key| I18n.t("historic_appointments_index.headings.#{key}") }
+  end
+
   def self.find!(base_path)
     content_item = ContentItem.find!(base_path)
     new(content_item)
