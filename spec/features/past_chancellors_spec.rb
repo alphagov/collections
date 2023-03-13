e34c519d057be38b4c23d3a8e65c8d9000d997d7
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2023-03-13 13:43:57 +0000

    Add better tests for past foreign secretaries and past chancellors
    This adds assertions based on the actual expected contents of the page, beyond headings and titles

--- spec/features/past_chancellors_spec.rb
+++ spec/features/past_chancellors_spec.rb
@@ -11,24 +11,44 @@ RSpec.feature "Past Chancellors pages" do
 
   before do
     stub_content_store_has_item(base_path, content_item)
+    visit base_path
   end
 
   it "sets the page title" do
-    visit base_path
     expect(page).to have_title("#{content_item[:title]} - GOV.UK")
   end
 
   it "sets renders the title on the page" do
-    visit base_path
     expect(page).to have_text(content_item[:title])
   end
 
   it "includes headers for each century" do
-    visit base_path
     expect(page).to have_text("21st century")
     expect(page).to have_text("20th century")
     expect(page).to have_text("19th century")
     expect(page).to have_text("18th century")
     expect(page).to have_text("16th & 17th centuries")
+  end
+
+  it "renders past foreign secretaries in the relevant section" do
+    within("div#historical-people-21st-century") do
+      expect(page).to have_text("Kwasi Kwarteng")
+    end
+
+    within("div#historical-people-20th-century") do
+      expect(page).to have_text("Kenneth Clarke")
+    end
+
+    within("div#historical-people-19th-century") do
+      expect(page).to have_text("Sir Michael Hicks")
+    end
+
+    within("div#historical-people-18th-century") do
+      expect(page).to have_text("Lord John Cavendish")
+    end
+
+    within('div#historical-people-16th-\&-17th-centuries') do
+      expect(page).to have_text("Charles Montagu")
+    end
   end
 end
