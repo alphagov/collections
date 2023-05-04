13b659c98a58216995ac4e798b22656cfdb4159e
James Mead <james.mead@digital.cabinet-office.gov.uk>
2023-05-04 12:31:07 +0100

    Add routes for legacy organisation-related redirects
    Trello: https://trello.com/c/sajxUZnZ
    
    This will be used in conjunction with this whitehall PR [1] which
    changes the main route for an organisation content item to be a "prefix"
    route rather than a "exact" route. This change would mean that the
    router would send the relevant URLs to collections instead of
    whitehall-frontend. This PR adds the necessary routes which are closely
    based on those currently in whitehall [2] so that collections can do the
    redirects.
    
    [1]: https://github.com/alphagov/whitehall/pull/7621
    [2]: https://github.com/alphagov/whitehall/blob/9afe523ed2f3eca7771c5510056b4bbb2cfa29a1/config/routes.rb#L66-L73

--- spec/requests/redirection_spec.rb
+++ spec/requests/redirection_spec.rb
@@ -20,4 +20,52 @@ RSpec.describe "Redirection of encoded urls" do
     expect(response).to have_http_status(:redirect)
     expect("http://www.example.com/government/people/cornelius-fudge?associates=higgs%26mclaggen%2Fshacklebolt%26tonks").to eq(response.header["Location"])
   end
+
+  it "redirects organisation chiefs-of-staff path to the organisation path" do
+    get "/government/organisations/organisation-1/chiefs-of-staff"
+
+    expect(response).to redirect_to("/government/organisations/organisation-1")
+  end
+
+  it "redirects organisation consultations path to the organisation path" do
+    get "/government/organisations/organisation-1/consultations"
+
+    expect(response).to redirect_to("/government/organisations/organisation-1")
+  end
+
+  it "redirects organisation groups path to the organisation path" do
+    get "/government/organisations/organisation-1/groups"
+
+    expect(response).to redirect_to("/government/organisations/organisation-1")
+  end
+
+  it "redirects organisation group path to the organisation path" do
+    get "/government/organisations/organisation-1/groups/group-1"
+
+    expect(response).to redirect_to("/government/organisations/organisation-1")
+  end
+
+  it "redirects organisation series index path to the publications path" do
+    get "/government/organisations/organisation-1/series"
+
+    expect(response).to redirect_to("/government/publications")
+  end
+
+  it "redirects organisation localised series index path to the publications path" do
+    get "/government/organisations/organisation-1/series.cy"
+
+    expect(response).to redirect_to("/government/publications")
+  end
+
+  it "redirects organisation series path to the corresponding collections path" do
+    get "/government/organisations/organisation-1/series/series-1"
+
+    expect(response).to redirect_to("/government/collections/series-1")
+  end
+
+  it "redirects organisation localised series path to the correspondong collections path" do
+    get "/government/organisations/organisation-1/series/series-1.cy"
+
+    expect(response).to redirect_to("/government/collections/series-1")
+  end
 end
