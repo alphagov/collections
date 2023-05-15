0d68687e463170f4209f56345ce908ab79838023
Catalina Ilie <catalina.ilie@digital.cabinet-office.gov.uk>
2023-05-15 11:02:24 +0100

    Update survey link for COL user research banner

--- app/helpers/user_research_banner_helper.rb
+++ app/helpers/user_research_banner_helper.rb
@@ -1,5 +1,5 @@
 module UserResearchBannerHelper
-  COST_OF_LIVING_SURVEY_URL = "https://gdsuserresearch.optimalworkshop.com/treejack/ct80d1d6".freeze
+  COST_OF_LIVING_SURVEY_URL = "https://gdsuserresearch.optimalworkshop.com/treejack/f49b8c01521bf45bd0a519fe02f5f913".freeze
 
   SURVEY_URL_MAPPINGS = {
     "/browse/working/state-pension" => COST_OF_LIVING_SURVEY_URL,
