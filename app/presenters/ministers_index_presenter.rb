e5a7cae820e006de388aa3f1e135f4dcd60d6ed7
Cristina <579522+CristinaRO@users.noreply.github.com>
2023-05-16 20:28:09 +0100

    Simplify the structure

--- app/presenters/ministers_index_presenter.rb
+++ app/presenters/ministers_index_presenter.rb
@@ -1,34 +1,34 @@
 class MinistersIndexPresenter
   def initialize(ministers_index)
-    @ministers_index = ministers_index
+    @ministers_index = ministers_index.content_item
   end
 
   def lead_paragraph
-    @ministers_index.content_item.details.fetch("body", nil)
+    @ministers_index.details.fetch("body", nil)
   end
 
   def is_during_reshuffle?
-    @ministers_index.content_item.details.fetch("reshuffle", nil)
+    @ministers_index.details.fetch("reshuffle", nil)
   end
 
   def reshuffle_messaging
-    @ministers_index.content_item.details.dig("reshuffle", "message")
+    @ministers_index.details.dig("reshuffle", "message")
   end
 
   def cabinet_ministers
-    @ministers_index.content_item.content_item_data.dig("links", "ordered_cabinet_ministers").map do |minister_data|
+    @ministers_index.content_item_data.dig("links", "ordered_cabinet_ministers").map do |minister_data|
       Minister.new(minister_data)
     end
   end
 
   def also_attends_cabinet
-    @ministers_index.content_item.content_item_data.dig("links", "ordered_also_attends_cabinet").map do |minister_data|
+    @ministers_index.content_item_data.dig("links", "ordered_also_attends_cabinet").map do |minister_data|
       Minister.new(minister_data)
     end
   end
 
   def by_organisation
-    @ministers_index.content_item.content_item_data.dig("links", "ordered_ministerial_departments").map do |department_data|
+    @ministers_index.content_item_data.dig("links", "ordered_ministerial_departments").map do |department_data|
       Department.new(
         url: department_data.fetch("web_url"),
         title: department_data.fetch("title"),
@@ -46,7 +46,7 @@ class MinistersIndexPresenter
 
   def whips
     ordered_whip_organisations.each do |whip_org|
-      whip_org.ministers = @ministers_index.content_item.content_item_data.dig("links", whip_org.item_key).map do |minister_data|
+      whip_org.ministers = @ministers_index.content_item_data.dig("links", whip_org.item_key).map do |minister_data|
         Minister.new(minister_data, whip_only: true)
       end
     end
