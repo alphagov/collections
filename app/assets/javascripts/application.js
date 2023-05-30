4beda5d71da7d471f0adc6fd46e2825732710f3e
Patrick Cartlidge <patrickcartlidge@gmail.com>
2023-05-30 12:34:27 +0100

    remove the polyfills

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,3 @@
-//= require govuk-frontend/govuk/vendor/polyfills/Function/prototype/bind
-
-//= require govuk_publishing_components/lib
-
-//= require support
 //= require modules/organisation-list-filter
 //= require modules/toggle-attribute
 //= require modules/coronavirus-landing-page
