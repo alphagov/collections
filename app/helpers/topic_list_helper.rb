d47af75db65b1a1a7ea4fe9c20ed5d27f7aae0ae
James Chan <james.chan@digital.cabinet-office.gov.uk>
2023-06-20 14:57:36 +0100

    Add ga4 link tracking to topic lists on topic pages

--- app/helpers/topic_list_helper.rb
+++ app/helpers/topic_list_helper.rb
@@ -17,11 +17,17 @@ module TopicListHelper
     }
   end
 
-  def topic_list_params(list, tracking_attributes: nil, list_index: nil, category: nil)
+  def topic_list_params(list, tracking_attributes: nil, list_index: nil, category: nil, list_count: nil, list_title: nil)
     tracking_attributes ||= topic_list_tracking_attributes(list.count, list_index, category)
+    ga4_data = {}
+    ga4_data[:index] = {}
+    ga4_data[:index][:index_section] = list_index + 1 if list_index
+    ga4_data[:index][:index_section_count] = list_count if list_count
+    ga4_data[:section] = list_title if list_title
 
     {
       items: topic_list_items(list, tracking_attributes),
+      ga4_data:,
     }
   end
 
