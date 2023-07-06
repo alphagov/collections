619ecda845f9e4ad8c90f58c9a653d4811be0871
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2023-07-06 18:26:19 +0100

    Add more examples to the Development page

--- app/controllers/development_controller.rb
+++ app/controllers/development_controller.rb
@@ -4,18 +4,25 @@ class DevelopmentController < ApplicationController
   def index
     @schema_names = %w[
       coronavirus_landing_page
+      embassies
+      embassy
       finder
+      government_feed
       historic_appointment
       mainstream_browse_page
       ministerial_role
       organisation
+      organisation_feed
+      past_prime_ministers
       person
       services_and_information
       step_by_step_nav
       taxon
       topic
       topical_event
+      world_embassies
       world_location_news
+      world_wide_taxon
     ]
 
     @paths = YAML.load_file(Rails.root.join("config/govuk_examples.yml"))
