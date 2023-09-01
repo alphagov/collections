0ddb6c0b46d0a16fa36a634ec1a87868ebd2a080
Jon Kirwan <87758239+jon-kirwan@users.noreply.github.com>
2023-09-01 12:12:57 +0100

    Add `watch` task

--- startup.sh
+++ startup.sh
@@ -9,7 +9,7 @@ if [[ $1 == "--live" ]] ; then
   PLEK_SERVICE_CONTENT_STORE_URI=${PLEK_SERVICE_CONTENT_STORE_URI-https://www.gov.uk/api} \
   PLEK_SERVICE_STATIC_URI=${PLEK_SERVICE_STATIC_URI-https://assets.publishing.service.gov.uk} \
   PLEK_SERVICE_SEARCH_API_URI=${PLEK_SERVICE_SEARCH_API_URI-https://www.gov.uk/api} \
-  bundle exec rails s -p 3070
+  ./bin/dev
 else
   echo "ERROR: other startup modes are not supported"
   echo ""
