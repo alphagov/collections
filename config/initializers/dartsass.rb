c898cee2cf17df91bf566c16ae02493ecb415143
Jonathon <jonathon.shire@digital.cabinet-office.gov.uk>
2024-02-26 09:58:21 +0000

    Remove topic and subtopic views

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,6 +1,5 @@
 APP_STYLESHEETS = {
   "components/_taxon-list.scss" => "components/_taxon-list.css",
-  "components/_topic-list.scss" => "components/_topic-list.css",
   "views/_browse.scss" => "views/_browse.css",
   "views/_bunting.scss" => "views/_bunting.css",
   "views/_covid.scss" => "views/_covid.css",
@@ -10,7 +9,6 @@ APP_STYLESHEETS = {
   "views/_organisations.scss" => "views/_organisations.css",
   "views/_taxons.scss" => "views/_taxons.css",
   "views/_topical_events.scss" => "views/_topical_events.css",
-  "views/_topics.scss" => "views/_topics.css",
   "views/_world_index.scss" => "views/_world_index.css",
 }.freeze
 
