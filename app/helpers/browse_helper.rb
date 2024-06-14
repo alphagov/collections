0b48f3414fea63a0872efb4e3e6a58a2122a7725
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2024-06-14 16:21:47 +0100

    Add popular tasks from search to browse pages

--- app/helpers/browse_helper.rb
+++ app/helpers/browse_helper.rb
@@ -1,37 +1,10 @@
 module BrowseHelper
-  def display_popular_links_for_slug?(slug)
-    popular_links_data(slug).present?
-  end
-
-  def control_key_is_present?(slug)
-    I18n.exists?("#{slug}.control", scope: "browse.popular_links")
-  end
-
-  def control_data(slug)
-    if control_key_is_present?(slug)
-      I18n.t("browse.popular_links.#{slug}.control")
-    else
-      []
-    end
-  end
-
-  def popular_links_data(slug)
-    if popular_tasks_variant_a_page?
-      I18n.t("browse.popular_links.#{slug}.variant_a")
-    elsif popular_tasks_variant_b_page?
-      I18n.t("browse.popular_links.#{slug}.variant_b")
-    else
-      control_data(slug)
-    end
-  end
-
-  def popular_links_for_slug(slug)
-    links = popular_links_data(slug)
-    count = links.length
-    links.map.with_index(1) do |link, index|
+  def with_tracking_data(popular_content)
+    count = popular_content.length
+    popular_content.map.with_index(1) do |page, index|
       {
-        text: link[:title],
-        href: link[:url],
+        text: page[:title],
+        href: page[:link],
         data_attributes: {
           module: "ga4-link-tracker",
           ga4_track_links_only: "",
@@ -40,7 +13,7 @@ module BrowseHelper
             type: "action",
             index_link: index,
             index_total: count,
-            text: link[:title],
+            text: page[:title],
             section: "Popular tasks",
           },
         },
