794a1c2124ab20d7d64764eff68cfedc3931d5b7
Rebecca Pearce <17481621+beccapearce@users.noreply.github.com>
2024-09-06 09:42:56 +0100

    Render popular tasks in view
    - Updated the view to render the results fetched from BigQuery
    - Simple unordered list displaying the search term as a link
    - Just making sure that the data can be passed from BigQuery to the view
    
    NB Still need to add the title into the link rendering

--- app/helpers/browse_helper.rb
+++ app/helpers/browse_helper.rb
@@ -1,27 +1,22 @@
 module BrowseHelper
-  def display_popular_links_for_slug?(slug)
-    I18n.exists?(slug.to_s, scope: "browse.popular_links")
+  def slug(path = base_path)
+    path.sub(%r{.*(?=/browse/)}, "")
+  end
+
+  def display_popular_tasks_for_slug?(slug)
+    %w[benefits business].include?(slug)
+  end
+
+  def select_browse_page(browse_page = "/browse/benefits")
+    browse_page = slug(browse_page)
+    popular_task_data = PopularTasks.new.fetch_data
+    popular_task_data.select { |link| link[:browse_page] == "/browse/#{browse_page}" }
   end
 
   def popular_links_for_slug(slug)
-    links = I18n.t(slug.to_s, scope: "browse.popular_links")
-    count = links.length
-    links.map.with_index(1) do |link, index|
-      {
-        text: link[:title],
-        href: link[:url],
-        data_attributes: {
-          module: "ga4-link-tracker",
-          ga4_track_links_only: "",
-          ga4_link: {
-            event_name: "navigation",
-            type: "action",
-            index_link: index,
-            index_total: count,
-            text: link[:title],
-          },
-        },
-      }
-    end
+    links = select_browse_page(slug)
+    return [] unless links
+
+    links
   end
 end
