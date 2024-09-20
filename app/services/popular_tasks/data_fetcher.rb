31e5c2ea5b12c82bc87a2a916aa541f50625cfdc
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2024-09-20 23:03:16 +0100

    Create a reusable cached_query method
    Small refactor, so that we can re-use this method to fetch data
    without having an expiry (maybe!)

--- app/services/popular_tasks/data_fetcher.rb
+++ app/services/popular_tasks/data_fetcher.rb
@@ -29,10 +29,11 @@ module PopularTasks
 
     def fresh_popular_task_data(page, date)
       todays_popular_tasks_query = sql_query(page, date)
-
-      Rails.cache.fetch("popular_tasks_for_#{page}", expires_in: 12.hours) do
-        client.query(todays_popular_tasks_query).all
-      end
+      cached_query(
+        key: "popular_tasks_for_#{page}",
+        query: todays_popular_tasks_query,
+        expires_in: 12.hours,
+      )
     rescue SomeBigQueryErrorCodes
       to_be_implemented_report_error
       []
@@ -44,6 +45,12 @@ module PopularTasks
 
     def yet_to_be_implemented_backup_popular_task_data(page)
       Rails.cache.read("backup_popular_tasks_for_#{page}")
+    end
+
+    def cached_query(key:, query:, expires_in: nil)
+      Rails.cache.fetch(key, expires_in:) do
+        client.query(query).all
+      end
     end
   end
 end
