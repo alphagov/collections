81f7fc3b655eb0ef6a2bd9ecee0099ca610733cd
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2024-09-20 22:51:03 +0100

    We should have some error handling
    https://cloud.google.com/bigquery/docs/error-messages#errortable
    
    If the cache expires and we can't fetch fresh data we should fall back
    to a backup that is rebuilt via a nightly job.

--- app/services/popular_tasks/data_fetcher.rb
+++ app/services/popular_tasks/data_fetcher.rb
@@ -27,12 +27,23 @@ module PopularTasks
       SQL
     end
 
-    def popular_task_data(page, date)
+    def fresh_popular_task_data(page, date)
       todays_popular_tasks_query = sql_query(page, date)
 
       Rails.cache.fetch("popular_tasks_for_#{page}", expires_in: 12.hours) do
         client.query(todays_popular_tasks_query).all
       end
+    rescue SomeBigQueryErrorCodes
+      to_be_implemented_report_error
+      []
+    end
+
+    def popular_task_data(page, date)
+      fresh_popular_task_data(page, date) || yet_to_be_implemented_backup_popular_task_data(page)
+    end
+
+    def yet_to_be_implemented_backup_popular_task_data(page)
+      Rails.cache.read("backup_popular_tasks_for_#{page}")
     end
   end
 end
