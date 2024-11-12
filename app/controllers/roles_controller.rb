3cb6f421ad6ab5bdf0d2a6bd8eeae4291323cd35
Mike Patrick <mike.patrick2@digital.cabinet-office.gov.uk>
2024-11-12 15:30:04 +0000

    Check for GraphQL feature flag in Roles controller
    Co-authored-by: Ynda <yndajas@gmail.com>

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -2,9 +2,15 @@ class RolesController < ApplicationController
   around_action :switch_locale
 
   def show
-    @role = Role.find!(request.path)
+    if Features.graphql_feature_enabled? || params.include?(:graphql)
+      @role = Graphql::Role.find!(request.path)
+      content_item_data = @role.content_item
+    else
+      @role = Role.find!(request.path)
+      content_item_data = @role.content_item.content_item_data
+    end
+
     setup_content_item_and_navigation_helpers(@role)
-    content_item_data = @role.content_item.content_item_data
     render :show, locals: { role: RolePresenter.new(content_item_data) }
   end
 end
--- spec/features/role_spec.rb
+++ spec/features/role_spec.rb
@@ -3,84 +3,171 @@ require "integration_spec_helper"
 RSpec.feature "Role page" do
   include SearchApiHelpers
 
-  before do
-    stub_content_store_has_item(
-      "/government/ministers/prime-minister",
-      role_content_item_data,
-    )
-    stub_search(body: { results: [] })
+  context "with the GraphQL feature flag" do
+    before do
+      enable_graphql_feature_flag
+      stub_publishing_api_graphql_query(
+        Graphql::RoleQuery.new("/government/ministers/prime-minister").query,
+        role_edition_data,
+      )
+      stub_search(body: { results: [] })
 
-    visit "/government/ministers/prime-minister"
-  end
+      visit "/government/ministers/prime-minister"
+    end
 
-  let(:role_content_item_data) do
-    GovukSchemas::Example.find("role", example_name: "prime_minister")
-  end
+    let(:role_edition_data) do
+      fetch_graphql_fixture("prime_minister")
+    end
 
-  it "renders the page successfully" do
-    expect(page).to have_selector("h1", text: "Prime Minister")
-  end
+    it "renders the page successfully" do
+      expect(page).to have_selector("h1", text: "Prime Minister")
+    end
 
-  context "when there's a current role holder" do
-    let(:role_content_item_data) do
-      GovukSchemas::Example.find("role", example_name: "prime_minister")
-        .tap do |example|
-          none_current = example["links"]["role_appointments"]
-            .all? { |ra| ra["details"]["current"] == false }
+    context "when there's a current role holder" do
+      let(:role_edition_data) do
+        fetch_graphql_fixture("prime_minister")
+          .tap do |fixture|
+            any_current = fixture["data"]["edition"]["links"]["roleAppointments"]
+              .any? { |ra| ra["details"]["current"] == true }
 
-          if none_current
-            example["links"]["role_appointments"][0]["details"]["current"] = true
+            unless any_current
+              links = fixture["data"]["edition"]["links"]
+              links["roleAppointments"][0]["details"]["current"] = true
+            end
           end
-        end
+      end
+
+      it "renders the page successfully" do
+        expect(page.status_code).to eq(200)
+      end
     end
 
-    it "renders the page successfully" do
-      expect(page.status_code).to eq(200)
-    end
-  end
+    context "when there isn't a current role holder" do
+      let(:role_edition_data) do
+        fetch_graphql_fixture("prime_minister")
+          .tap do |fixture|
+            current = fixture["data"]["edition"]["links"]["roleAppointments"]
+              .find { |ra| ra["details"]["current"] == true }
 
-  context "when there isn't a current role holder" do
-    let(:role_content_item_data) do
-      GovukSchemas::Example.find("role", example_name: "prime_minister")
-        .tap do |example|
-          current = example["links"]["role_appointments"]
-            .find { |ra| ra["details"]["current"] == true }
-
-          if current
-            current["details"]["current"] = false
-            current["details"]["ended_on"] = Time.zone.now
+            if current
+              current["details"]["current"] = false
+              current["details"]["endedOn"] = Time.zone.now
+            end
           end
-        end
+      end
+
+      it "renders the page successfully" do
+        expect(page.status_code).to eq(200)
+      end
     end
 
-    it "renders the page successfully" do
-      expect(page.status_code).to eq(200)
+    context "when the role 'supports historical accounts'" do
+      let(:role_edition_data) do
+        fetch_graphql_fixture("prime_minister")
+          .tap do |fixture|
+            fixture["data"]["edition"]["details"]["supportsHistoricalAccounts"] = true
+          end
+      end
+
+      it "renders the page successfully" do
+        expect(page.status_code).to eq(200)
+      end
+    end
+
+    context "when the role doesn't 'support historical accounts'" do
+      let(:role_edition_data) do
+        fetch_graphql_fixture("prime_minister")
+          .tap do |fixture|
+            fixture["data"]["edition"]["details"]["supportsHistoricalAccounts"] = false
+          end
+      end
+
+      it "renders the page successfully" do
+        expect(page.status_code).to eq(200)
+      end
     end
   end
 
-  context "when the role 'supports historical accounts'" do
+  context "without the GraphQL feature flag" do
+    before do
+      stub_content_store_has_item(
+        "/government/ministers/prime-minister",
+        role_content_item_data,
+      )
+      stub_search(body: { results: [] })
+
+      visit "/government/ministers/prime-minister"
+    end
+
     let(:role_content_item_data) do
       GovukSchemas::Example.find("role", example_name: "prime_minister")
-        .tap do |example|
-          example["details"]["supports_historical_accounts"] = true
-        end
     end
 
     it "renders the page successfully" do
-      expect(page.status_code).to eq(200)
-    end
-  end
-
-  context "when the role doesn't 'support historical accounts'" do
-    let(:role_content_item_data) do
-      GovukSchemas::Example.find("role", example_name: "prime_minister")
-        .tap do |example|
-          example["details"]["supports_historical_accounts"] = false
-        end
+      expect(page).to have_selector("h1", text: "Prime Minister")
     end
 
-    it "renders the page successfully" do
-      expect(page.status_code).to eq(200)
+    context "when there's a current role holder" do
+      let(:role_content_item_data) do
+        GovukSchemas::Example.find("role", example_name: "prime_minister")
+          .tap do |example|
+            any_current = example["links"]["role_appointments"]
+              .any? { |ra| ra["details"]["current"] == true }
+
+            unless any_current
+              example["links"]["role_appointments"][0]["details"]["current"] = true
+            end
+          end
+      end
+
+      it "renders the page successfully" do
+        expect(page.status_code).to eq(200)
+      end
+    end
+
+    context "when there isn't a current role holder" do
+      let(:role_content_item_data) do
+        GovukSchemas::Example.find("role", example_name: "prime_minister")
+          .tap do |example|
+            current = example["links"]["role_appointments"]
+              .find { |ra| ra["details"]["current"] == true }
+
+            if current
+              current["details"]["current"] = false
+              current["details"]["ended_on"] = Time.zone.now
+            end
+          end
+      end
+
+      it "renders the page successfully" do
+        expect(page.status_code).to eq(200)
+      end
+    end
+
+    context "when the role 'supports historical accounts'" do
+      let(:role_content_item_data) do
+        GovukSchemas::Example.find("role", example_name: "prime_minister")
+          .tap do |example|
+            example["details"]["supports_historical_accounts"] = true
+          end
+      end
+
+      it "renders the page successfully" do
+        expect(page.status_code).to eq(200)
+      end
+    end
+
+    context "when the role doesn't 'support historical accounts'" do
+      let(:role_content_item_data) do
+        GovukSchemas::Example.find("role", example_name: "prime_minister")
+          .tap do |example|
+            example["details"]["supports_historical_accounts"] = false
+          end
+      end
+
+      it "renders the page successfully" do
+        expect(page.status_code).to eq(200)
+      end
     end
   end
 end
