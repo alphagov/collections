53fc2ac1e05b962cf8810373ff7a328f46f2a3ca
Mike Patrick <mike.patrick2@digital.cabinet-office.gov.uk>
2024-11-22 16:32:18 +0000

    Introduce Graphql::MinistersIndex model
    This follows the same pattern as we used with the World Index page and
    the Role page. This model is an alternative to the existing
    MinistersIndex model, using Publishing API's GraphQL endpoint as its
    data source instead of Content Store.

---
+++ app/models/graphql/ministers_index.rb
@@ -0,0 +1,17 @@
+class Graphql::MinistersIndex
+  attr_reader :content_item
+
+  def initialize(content_item)
+    @content_item = content_item
+  end
+
+  def self.find!(base_path)
+    query = Graphql::MinistersIndexQuery.new(base_path).query
+
+    edition = Services.publishing_api.graphql_query(query)
+      .dig("data", "edition")
+      .deep_transform_keys(&:underscore)
+
+    new(edition)
+  end
+end
