88bd373a79229db8fe8cd793665aedfb0113f5e2
Martin Jones <martin.jones@digital.cabinet-office.gov.uk>
2025-07-10 12:03:16 +0100

    Test org update

--- app/presenters/organisations/index_presenter.rb
+++ app/presenters/organisations/index_presenter.rb
@@ -40,7 +40,9 @@ module Organisations
 
     def all_organisations
       {
-        number_10: @organisations.number_10,
+        number_10: ordered_executive_offices(
+          @organisations.number_10
+        ), #number_10 is any executive office
         ministerial_departments: reject_joining_organisations(
           @organisations.ministerial_departments,
         ),
@@ -70,6 +72,14 @@ module Organisations
       non_joining_works_with_categorised_links(organisation, @organisations.by_href)
         .to_a
         .sort_by { |type, _departments| LISTING_ORDER.index(type) || 999 }
+    end
+
+    def ordered_executive_offices(organisations)
+      sorted = organisations.sort_by do |organisation|
+        organisation[:slug] == "prime-ministers-office-10-downing-street" ? [1, organisation[:slug]] : [1, organisation[:slug]]
+      end
+
+      sorted
     end
 
     def works_with_statement(organisation)
