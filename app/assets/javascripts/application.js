ecd1e8eda043c1a4e3903811d6017ad7ea833ec8
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-11-18 12:38:34 +0000

    Add required JS and Styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,19 @@
+//= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/lib
+
 //= require govuk_publishing_components/components/accordion
+//= require govuk_publishing_components/components/button
+//= require govuk_publishing_components/components/cookie-banner
+//= require govuk_publishing_components/components/cross-service-header
+//= require govuk_publishing_components/components/feedback
 //= require govuk_publishing_components/components/global-banner
 //= require govuk_publishing_components/components/govspeak
 //= require govuk_publishing_components/components/image-card
+//= require govuk_publishing_components/components/layout-super-navigation-header
 //= require govuk_publishing_components/components/metadata
+//= require govuk_publishing_components/components/search-with-autocomplete
+//= require govuk_publishing_components/components/service-navigation
+//= require govuk_publishing_components/components/skip-link
 //= require govuk_publishing_components/components/step-by-step-nav
 
 //= require govuk_web_banners/dependencies
