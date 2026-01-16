72842549fd8e07ca5ac66e8e53ffd94593922440
Martin Jones <martin.jones@digital.cabinet-office.gov.uk>
2026-01-16 14:23:09 +0000

    Import app-promo-banner JS and CSS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,7 @@
 //= require govuk_publishing_components/lib
 
 //= require govuk_publishing_components/components/accordion
+//= require govuk_publishing_components/components/app-promo-banner
 //= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/cookie-banner
 //= require govuk_publishing_components/components/cross-service-header
