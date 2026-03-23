0dffb4cbf1793e12c8c64797392ed4a2a0292345
Callum Knights <callum.knights@digital.cabinet-office.gov.uk>
2026-03-23 16:47:15 +0000

    Role controller action refactor
    This commit changes roles to load content using the new govuk_content_item_loader gem,
    which delegates the graphql vs content_store decision to the gem.
    
    Also removes now redundant testing that has been introduced over the course of the
    graphql work.

--- spec/controllers/roles_controller_spec.rb
+++ spec/controllers/roles_controller_spec.rb
@@ -6,82 +6,21 @@ RSpec.describe RolesController do
   let(:role) { base_path.split("/").last }
 
   describe "GET show" do
-    context "the request is for content store" do
-      before do
-        stub_content_store_has_item(base_path, prime_minister)
-        stub_content_store_does_not_have_item("/government/ministers/she-ra")
-        stub_minister_announcements(role)
-      end
-
-      it "when the content item exists" do
-        get :show, params: { name: role, graphql: false }
-        expect(response).to have_http_status(:success)
-        expect(response).to render_template(:show)
-      end
-
-      it "when there is no content item" do
-        get :show, params: { name: "she-ra", graphql: false }
-        expect(response).to have_http_status(:not_found)
-      end
+    before do
+      stub_conditional_loader_returns_content_item_for_path(base_path, prime_minister)
+      stub_conditional_loader_does_not_return_content_item_for_path("/government/ministers/she-ra")
+      stub_minister_announcements(role)
     end
 
-    context "when the request is for GraphQL" do
-      context "and the GraphQL query is successful" do
-        before do
-          stub_publishing_api_graphql_has_item(base_path, prime_minister)
-        end
+    it "when the content item exists" do
+      get :show, params: { name: role }
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show)
+    end
 
-        it "the request is successful" do
-          get :show, params: { name: role, graphql: true }
-
-          expect(request.env["govuk.prometheus_labels"]).to include({
-            "graphql_status_code" => 200,
-            "graphql_api_timeout" => false,
-          })
-          expect(response).to have_http_status(:success)
-          expect(response).to render_template(:show)
-        end
-      end
-
-      context "and publishing-api returns an error status code" do
-        before do
-          stub_any_publishing_api_call_to_return_not_found
-          stub_content_store_has_item(base_path, prime_minister)
-        end
-
-        it "falls back to loading from Content Store" do
-          get :show, params: { name: role, graphql: true }
-
-          expect(response).to have_http_status(:success)
-        end
-
-        it "pushes the status codes to prometheus" do
-          get :show, params: { name: role, graphql: true }
-
-          expect(request.env["govuk.prometheus_labels"]["graphql_status_code"]).to eq(404)
-        end
-      end
-
-      context "and GDS API Adapters times-out the request" do
-        before do
-          allow(Services.publishing_api).to receive(:graphql_live_content_item)
-            .and_raise(GdsApi::TimedOutException)
-
-          stub_content_store_has_item(base_path, prime_minister)
-        end
-
-        it "falls back to loading from Content Store" do
-          get :show, params: { name: role, graphql: true }
-
-          expect(response).to have_http_status(:success)
-        end
-
-        it "pushes the errors to prometheus" do
-          get :show, params: { name: role, graphql: true }
-
-          expect(request.env["govuk.prometheus_labels"]["graphql_api_timeout"]).to be(true)
-        end
-      end
+    it "when there is no content item" do
+      get :show, params: { name: "she-ra" }
+      expect(response).to have_http_status(:not_found)
     end
   end
 end
