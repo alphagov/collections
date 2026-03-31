59ce5505305a97236577d99f9ee88d26205f76f4
Callum Knights <callum.knights@digital.cabinet-office.gov.uk>
2026-03-31 17:15:53 +0100

    Add new govuk_graphql_traffic_rates Initializer
    This is how the gem handles ENV variables
    
    Also removes now redundant test, as it's intruding on the
    gems responsibilities

--- config/initializers/graphql_traffic_rates.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.graphql_traffic_rates = ENV
-  .select { |key, _| key.starts_with?("GRAPHQL_RATE_") }
-  .transform_keys { |key| key.sub(/^GRAPHQL_RATE_/, "").downcase }
-  .transform_values(&:to_f)
