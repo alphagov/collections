cfb91e9c95bbff3e575e3971547ff0b7b5bf079b
Catalina Ilie <catalina.ilie@digital.cabinet-office.gov.uk>
2026-04-30 16:00:12 +0100

    Retun translated search results when documents have a translated version

--- app/helpers/organisation_helper.rb
+++ app/helpers/organisation_helper.rb
@@ -8,11 +8,32 @@ module OrganisationHelper
     I18n.t("organisations.type.#{organisation_type}", default: I18n.t("organisations.type.other"))
   end
 
+  def translated_search_result(result)
+    return result if I18n.locale == :en
+
+    content_item = Services.content_store.content_item(result["link"])
+
+    return result if content_item.parsed_content.dig("links", "available_translations").nil?
+
+    content_item.parsed_content["links"]["available_translations"].each do |t|
+      if t["locale"] == I18n.locale.to_s
+        return {
+          "title" => t["title"],
+          "link" => t["base_path"],
+          "content_store_document_type" => t["document_type"],
+          "public_timestamp" => t["public_updated_at"],
+        }
+      end
+    end
+
+    result
+  end
+
   def search_results_to_documents(search_results, organisation, include_metadata: true)
     {
       brand: (organisation.brand if organisation.is_live?),
       items: search_results.map do |result|
-        Organisations::DocumentPresenter.new(result, include_metadata:).present
+        Organisations::DocumentPresenter.new(translated_search_result(result), include_metadata:).present
       end,
     }
   end
